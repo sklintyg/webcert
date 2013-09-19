4e2c06bd0bd5b4bf182ed9807b3d4e7fe7bde684
Marcus Cedergren <marcus.cedergren@callistaenterprise.se>
2013-09-19 15:16:02 +0200

    Added persistence module with initial Q&A Entity Pojos

---
+++ persistence/src/main/java/se/inera/webcert/persistence/Komplettering.java
@@ -0,0 +1,27 @@
+package se.inera.webcert.persistence;
+
+/**
+ * @author marced
+ * 
+ */
+public class Komplettering {
+    private String falt;
+    private String text;
+
+    public String getFalt() {
+        return falt;
+    }
+
+    public void setFalt(String falt) {
+        this.falt = falt;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+}
---
+++ persistence/src/main/java/se/inera/webcert/persistence/Vardperson.java
@@ -0,0 +1,122 @@
+package se.inera.webcert.persistence;
+
+public class Vardperson {
+    private String hsaId;
+    private String namn;
+    private String forskrivarKod;
+    private String enhetsId;
+    private String arbetsplatsKod;
+    private String enhetsNamn;
+    private String postadress;
+    private String postNummer;
+    private String postort;
+    private String telefonnummer;
+    private String epost;
+    private String vardgivarId;
+    private String vardgivarNamn;
+
+    public String getHsaId() {
+        return hsaId;
+    }
+
+    public void setHsaId(String hsaId) {
+        this.hsaId = hsaId;
+    }
+
+    public String getNamn() {
+        return namn;
+    }
+
+    public void setNamn(String namn) {
+        this.namn = namn;
+    }
+
+    public String getForskrivarKod() {
+        return forskrivarKod;
+    }
+
+    public void setForskrivarKod(String forskrivarKod) {
+        this.forskrivarKod = forskrivarKod;
+    }
+
+    public String getEnhetsId() {
+        return enhetsId;
+    }
+
+    public void setEnhetsId(String enhetsId) {
+        this.enhetsId = enhetsId;
+    }
+
+    public String getArbetsplatsKod() {
+        return arbetsplatsKod;
+    }
+
+    public void setArbetsplatsKod(String arbetsplatsKod) {
+        this.arbetsplatsKod = arbetsplatsKod;
+    }
+
+    public String getEnhetsNamn() {
+        return enhetsNamn;
+    }
+
+    public void setEnhetsNamn(String enhetsNamn) {
+        this.enhetsNamn = enhetsNamn;
+    }
+
+    public String getPostadress() {
+        return postadress;
+    }
+
+    public void setPostadress(String postadress) {
+        this.postadress = postadress;
+    }
+
+    public String getPostNummer() {
+        return postNummer;
+    }
+
+    public void setPostNummer(String postNummer) {
+        this.postNummer = postNummer;
+    }
+
+    public String getPostort() {
+        return postort;
+    }
+
+    public void setPostort(String postort) {
+        this.postort = postort;
+    }
+
+    public String getTelefonnummer() {
+        return telefonnummer;
+    }
+
+    public void setTelefonnummer(String telefonnummer) {
+        this.telefonnummer = telefonnummer;
+    }
+
+    public String getEpost() {
+        return epost;
+    }
+
+    public void setEpost(String epost) {
+        this.epost = epost;
+    }
+
+    public String getVardgivarId() {
+        return vardgivarId;
+    }
+
+    public void setVardgivarId(String vardgivarId) {
+        this.vardgivarId = vardgivarId;
+    }
+
+    public String getVardgivarNamn() {
+        return vardgivarNamn;
+    }
+
+    public void setVardgivarNamn(String vardgivarNamn) {
+        this.vardgivarNamn = vardgivarNamn;
+    }
+
+}
