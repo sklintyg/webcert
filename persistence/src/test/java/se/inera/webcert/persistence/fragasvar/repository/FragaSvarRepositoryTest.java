17c8495895b90c436460a13ee923da8d2cba2052
Marcus Cedergren <marcus.cedergren@callistaenterprise.se>
2013-09-25 15:04:28 +0200

    Added custom FragaSvar Repository find methods to find all fragasva entities by their enhetsId and intygsId

--- persistence/src/test/java/se/inera/webcert/persistence/fragasvar/repository/FragaSvarRepositoryTest.java
+++ persistence/src/test/java/se/inera/webcert/persistence/fragasvar/repository/FragaSvarRepositoryTest.java
@@ -3,6 +3,7 @@ package se.inera.webcert.persistence.fragasvar.repository;
 import static org.junit.Assert.assertEquals;
 
 import java.util.Arrays;
+import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -11,7 +12,6 @@ import org.joda.time.LocalDateTime;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -20,28 +20,32 @@ import org.springframework.transaction.annotation.Transactional;
 import se.inera.webcert.persistence.fragasvar.model.Amne;
 import se.inera.webcert.persistence.fragasvar.model.FragaSvar;
 import se.inera.webcert.persistence.fragasvar.model.IntygsReferens;
+import se.inera.webcert.persistence.fragasvar.model.Vardperson;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {"classpath:repository-context.xml"})
+@ContextConfiguration(locations = { "classpath:repository-context.xml" })
 @ActiveProfiles("dev")
 @Transactional
 public class FragaSvarRepositoryTest {
-    
+
     @Autowired
-    private CrudRepository<FragaSvar, Long> fragasvarRepository;
-    
+    private FragaSvarRepository fragasvarRepository;
+
     @PersistenceContext
     private EntityManager em;
-    
+
     private LocalDateTime FRAGE_SIGN_DATE = new LocalDateTime("2013-03-01T11:11:11");
     private LocalDateTime FRAGE_SENT_DATE = new LocalDateTime("2013-03-01T12:00:00");
-    private IntygsReferens INTYGS_REFERENS = new IntygsReferens("abc123", "fk", "Sven Persson", "19121212-1212", FRAGE_SENT_DATE);
-   
+    private IntygsReferens INTYGS_REFERENS = new IntygsReferens("abc123", "fk", "Sven Persson", "19121212-1212",
+            FRAGE_SENT_DATE);
+    private static String ENHET_1_ID = "ENHET_1_ID";
+    private static String ENHET_2_ID = "ENHET_2_ID";
+    private static String ENHET_3_ID = "ENHET_3_ID";
+    private static String ENHET_4_ID = "ENHET_4_ID";
+
     @Test
     public void testFindOne() {
-        FragaSvar saved = buildFragaSvarFraga();
-       
-        
+        FragaSvar saved = buildFragaSvarFraga(ENHET_1_ID);
         fragasvarRepository.save(saved);
         FragaSvar read = fragasvarRepository.findOne(saved.getInternReferens());
         assertEquals(read.getInternReferens(), saved.getInternReferens());
@@ -52,29 +56,52 @@ public class FragaSvarRepositoryTest {
         assertEquals(read.getFrageStallare(), saved.getFrageStallare());
         assertEquals(read.getFrageText(), saved.getFrageText());
         assertEquals(read.getIntygsReferens(), saved.getIntygsReferens());
-        
     }
 
-    private FragaSvar buildFragaSvarFraga() {
+    @Test
+    public void testFindByEnhetsId() {
+
+        fragasvarRepository.save(buildFragaSvarFraga(ENHET_1_ID));
+        fragasvarRepository.save(buildFragaSvarFraga(ENHET_2_ID));
+        fragasvarRepository.save(buildFragaSvarFraga(ENHET_3_ID));
+        fragasvarRepository.save(buildFragaSvarFraga(ENHET_3_ID));
+        fragasvarRepository.save(buildFragaSvarFraga(ENHET_4_ID));
+
+        List<FragaSvar> result = fragasvarRepository.findByEnhetsId(Arrays.asList(ENHET_1_ID, ENHET_3_ID));
+        assertEquals(3, result.size());
+
+    }
+
+    @Test
+    public void testFindByIntygsReferens() {
+        FragaSvar saved = buildFragaSvarFraga(ENHET_1_ID);
+        saved.setIntygsReferens(new IntygsReferens("non-existing-intygs-id", "fk", "Sven Persson", "19121212-1212",
+                FRAGE_SENT_DATE));
+        fragasvarRepository.save(saved);
+        fragasvarRepository.save(buildFragaSvarFraga(ENHET_3_ID));
+        fragasvarRepository.save(buildFragaSvarFraga(ENHET_4_ID));
+
+        List<FragaSvar> result = fragasvarRepository.findByIntygsReferensIntygsId(INTYGS_REFERENS.getIntygsId());
+        assertEquals(2, result.size());
+
+    }
+
+    private FragaSvar buildFragaSvarFraga(String enhetsId) {
         FragaSvar f = new FragaSvar();
-        f.setExternaKontakter(Arrays.asList("KONTAKT1","KONTAKT2","KONTAKT3"));
+        f.setExternaKontakter(Arrays.asList("KONTAKT1", "KONTAKT2", "KONTAKT3"));
         f.setAmne(Amne.AVSTAMNINGSMOTE);
         f.setExternReferens("externReferens");
         f.setFrageSigneringsDatum(FRAGE_SIGN_DATE);
         f.setFrageSkickadDatum(FRAGE_SENT_DATE);
         f.setFrageStallare("Olle");
+        Vardperson vardperson = new Vardperson();
+        vardperson.setEnhetsId(enhetsId);
+        vardperson.setEnhetsnamn(enhetsId + "-namnet");
+        f.setVardperson(vardperson);
         f.setFrageText("Detta var ju otydligt formulerat!");
         f.setIntygsReferens(INTYGS_REFERENS);
-        
+
         return f;
     }
-       /*
-    @Test
-    public void testFind2() {
-
-        FragaSvar read = fragasvarRepository.findOne(saved.getInternReferens());
-
-
-    }  */
 
 }
