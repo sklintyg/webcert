64c9c0be15f6758382fa9a24408fa685f293bb11
Benny Samuelsson <benny.samuelsson@usify.se>
2013-09-26 14:01:05 +0200

    Implemented listing of units on qa page

--- webcert/src/main/webapp/js/app/dashboard/directives.js
+++ webcert/src/main/webapp/js/app/dashboard/directives.js
@@ -6,15 +6,22 @@ angular.module('wcDashBoardApp').directive("wcCareUnitClinicSelector", ['$rootSc
         restrict: "A",
         transclude: false,
         replace: true,
-        template: '<div>\n' +
-        '    <div class="control-group">\n' +
+        template: 
+    	    '<table class="span12 table unit-table">'+
+        		'<tr ng-repeat="vardenhet in vardenheter">'+
+        			'<td><span class="qa-unit" ng-class="{selected : valdVardenhet == vardenhet}">{{vardenhet.namn}}</span></td>'+
+        			'<!--<td><span class="qa-circle qa-circle-active" title="Ohanterade frågor och svar">2</span></td>-->'+
+        		'</tr>'+
+        	'</table>',
+/*        	'    <div>\n' +
+        '    	 <div class="control-group">\n' +
         '        <div class="btn-group">\n' +
         '            <select ng-model="valdVardenhet" ng-options="vardenhet.namn for vardenhet in vardenheter" ng-change="updateVardenhetsval()"></select>\n' +
         '        </div>\n' +
         '        <div class="btn-group">\n' +
         '            <select ng-disabled="mottagningar.length==0" ng-model="valdMottagning" ng-options="mottagning.namn for mottagning in mottagningar" ng-change="updateMottagningsval()"></select>\n' +
         '        </div>\n' +
-        '    </div>\n',
+        '    </div>\n',*/
         controller: function ($scope) {
             // init
             $scope.vardenheter = $rootScope.MODULE_CONFIG.USERCONTEXT.vardgivare.vardenheter;
