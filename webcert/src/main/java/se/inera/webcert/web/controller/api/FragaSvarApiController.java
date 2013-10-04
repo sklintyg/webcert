b59fdbd3cc83c131ede307680b57dbfd8c4cd8dc
Marcus Cedergren <marcus.cedergren@callistaenterprise.se>
2013-10-04 16:32:35 +0200

    saving answer to a fk fraga

--- webcert/src/main/java/se/inera/webcert/web/controller/api/FragaSvarApiController.java
+++ webcert/src/main/java/se/inera/webcert/web/controller/api/FragaSvarApiController.java
@@ -1,16 +1,21 @@
 package se.inera.webcert.web.controller.api;
 
 
+import java.util.List;
+
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import java.util.List;
+import javax.ws.rs.core.Response;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+
 import se.inera.webcert.persistence.fragasvar.model.FragaSvar;
 import se.inera.webcert.security.WebCertUser;
 import se.inera.webcert.service.FragaSvarService;
@@ -31,16 +36,20 @@ public class FragaSvarApiController {
     @Autowired
     private WebCertUserService webCertUserService;
     
-    @RequestMapping(value = "/test", method = RequestMethod.GET)
-    public String test() {
-        LOG.debug("api.test");
-        return "test";
-    }
     
     @GET
     @Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
     public List<FragaSvar> list() {
         WebCertUser user = webCertUserService.getWebCertUser();
         return fragaSvarService.getFragaSvar(user.getVardEnheter());
+    }
+    
+    @PUT
+    @Path("/{enhets-id}/{fragasvar-id}/answer" )
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+    public Response answer(@PathParam( "enhets-id" ) final String enhetsId, @PathParam( "fragasvar-id" ) final Long frageSvarId, FragaSvar fragaSvar) {
+        FragaSvar fragaSvarResponse =  fragaSvarService.saveSvar(fragaSvar);
+        return Response.ok(fragaSvarResponse).build();
     }
 }
