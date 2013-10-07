7f1e6270a28409cdcd3668d03ffa6b248ca731bc
Benny Samuelsson <benny.samuelsson@usify.se>
2013-10-07 11:09:49 +0200

    Moved module specific text from common messages to module messages.js

--- webcert/src/main/webapp/js/common/wc-common-message-resources.js
+++ webcert/src/main/webapp/js/common/wc-common-message-resources.js
@@ -3,65 +3,14 @@
 var commonMessageResources = {
     "sv" : {
 
-	    "common.ok":"OK",
-	    "common.cancel":"Avbryt",
-	    "common.goback":"Tillbaka",
-	    //wc-common-directives-resources
-	    "nav.label.loggedinas" : "Inloggad som:",
+		    "common.ok":"OK",
+		    "common.cancel":"Avbryt",
+		    "common.goback":"Tillbaka",
+	
+		    //wc-common-directives-resources
+		    "nav.label.loggedinas" : "Inloggad som:",
 
-	    	//modules resources
-	    	"view.label.field" : "Fält",
-        "view.label.blank" : "- ej ifyllt",
-        "view.label.nedsattning" : "Jag bedömer att arbetsförmåga är",
-        "view.label.patientname" : "Patientens namn",
-        "view.label.issued" : "Utfärdat",
-        "view.label.civicnumber" : "Personnummer",
-        "view.label.issuer" : "Utfärdare",
-        "view.label.period" : "Period",
-        "view.label.unit" : "Enhet",
-        "view.label.smittskydd" : "Avstängning enligt smittskyddslagen på grund av smitta",
-        "view.label.yes" : "Ja",
-        "view.label.no" : "Nej",
-        "view.label.partialyes" : "Ja, delvis",
-        "view.label.diagnosis" : "Diagnos/diagnoser för sjukdom som orsakar nedsatt arbetsförmåga",
-        "view.label.diagnosiscode" : "Diagnoskod enligt ICD-10 (huvuddiagnos): ",
-        "view.label.progressofdisease" : "Aktuellt sjukdomförlopp",
-        "view.label.disabilities" : "Funktionsnedsättning - observationer, undersökningsfynd och utredningsresultat",
-        "view.label.basedon" : "Intyget baseras på",
-        "view.label.comment" : "Kommentar:",
-        "view.label.limitation" : "Aktivitetsbegränsning relaterat till diagnos och funktionsnedsättning",
-        "view.label.recommendations" : "Rekommendationer",
-        "view.label.plannedtreatment" : "Planerad eller pågående behandling eller åtgärd",
-        "view.label.plannedtreatment.healthcare" : "Inom sjukvården:",
-        "view.label.plannedtreatment.other" : "Annan åtgärd:",
-        "view.label.workrehab" : "Är arbetslivsinriktad rehabilitering aktuell?",
-        "view.label.unjudgeable" : "Går inte att bedöma",
-        "view.label.patientworkcapacity" : "Patientens arbetsförmåga bedöms i förhållande till",
-        "view.label.patientworkcapacity.currentwork" : "Nuvarande arbete: ",
-        "view.label.patientworkcapacity.unemployed" : "Arbetslöshet - att utföra sådant arbete som är normalt förekommande på arbetsmarknaden",
-        "view.label.patientworkcapacity.parentalleave" : "Föräldraledighet med föräldrapenning - att vårda sitt barn",
-        "view.label.patientworkcapacityjudgement" : "Patientens arbetsförmåga bedöms nedsatt längre tid än den som det försäkringsmedicinska beslutsstödet anger, därför att:",
-        "view.label.prognosis" : "Prognos - kommer patienten att få tillbaka sin arbetsförmåga i nuvarande arbete? (Gäller inte arbetslösa)",
-        "view.label.othertransport" : "Kan resor till och från arbetet med annat färdsätt än normalt göra det möjligt för patienten att återgå i arbete?",
-        "view.label.fkcontact" : "Kontakt önskas med Försäkringskassan",
-        "view.label.workcodes" : "Förskrivarkod och arbetsplatskod",
-        "view.label.recommendations.contact.jobcenter" : "Kontakt med Arbetsförmedlingen",
-        "view.label.recommendations.contact.healthdepartment" : "Kontakt med företagshälsovården",
-        "view.label.recommendations.contact.other" : "Övrigt: ",
-        "view.label.reference.to.field13" : "Se under Övriga upplysningar och förtydliganden",
-        "view.label.otherinformation" : "Övriga upplysningar och förtydliganden",
-
-        "view.label.confirmedby" : "Ovanstående uppgifter och bedömningar har bekräftas",
-        "view.label.date" : "Datum",
-        "view.label.contactinfo" : "Namnförtydligande, mottagningens adress och telefon",
-        "vardkontakt.undersokning" : "Min undersökning av patienten den ",
-        "vardkontakt.telefonkontakt" : "Min telefonkontakt med patienten den ",
-        "referens.journal" : "Journaluppgifter, den ",
-        "referens.annat" : "Annat, den ",
-
-        "view.message.certificateloading" : "Hämtar intyg...",
-        
-        // Fraga/svar resource - used both by 
+        // Fraga/svar resource - used both by webcert and module 
         "qa.status.pending_internal_action" : "Kräver svar",
         "qa.status.pending_external_action" : "Inväntar svar",
         "qa.status.answered" : "Besvarat",
