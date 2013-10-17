00cda615ba94e1b4262710ae995efb058024c153
Marcus Cedergren <marcus.cedergren@callistaenterprise.se>
2013-10-17 11:04:21 +0200

    Added error result messages to admiv view unhandled Q&A, and message dialog when setVidarebefordrad fails

--- webcert/src/main/webapp/js/app/dashboard/UnhandledQAController.js
+++ webcert/src/main/webapp/js/app/dashboard/UnhandledQAController.js
@@ -2,102 +2,123 @@
  *  UnhandledQACtrl - Controller for logic related to listing questions and answers 
  * 
  */
-angular.module('wcDashBoardApp').controller('UnhandledQACtrl',
-        [ '$scope', '$window', '$log', '$timeout', '$filter', 'dashBoardService', function UnhandledCertCtrl($scope, $window, $log, $timeout, $filter, dashBoardService) {
-            // init state
-            $scope.widgetState = {
-                doneLoading : false,
-                hasError : false
-            }
-
-            $scope.isCollapsed = true;
-
-            $scope.qaList = {};
-            $scope.activeUnit = "";
-
-            // load all fragasvar for all units in usercontext
-            dashBoardService.getQA(function(data) {
-                $scope.widgetState.doneLoading = true;
-                if (data != null) {
-                    $scope.qaList = data;
-                    $log.debug("got Data!");
-
-                    // set waiting messages
-                    angular.forEach($scope.qaList, function(qa, key) {
-
-                        if (qa.status == "ANSWERED" || qa.amne == "MAKULERING" || qa.amne == "PAMINNELSE") {
-                            qa.vantarpa = "markhandled";
-                        } else if (qa.status == "CLOSED") {
-                            qa.vantarpa = "handled";
-                        } else if (qa.amne == "KOMPLETTERING_AV_LAKARINTYG") {
-                            qa.vantarpa = "komplettering";
-                        } else {
-
-                            if (qa.status == "PENDING_INTERNAL_ACTION") {
-                                qa.vantarpa = "svarfranvarden";
-                            } else if (qa.status == "PENDING_EXTERNAL_ACTION") {
-                                qa.vantarpa = "svarfranfk";
-                            } else {
-                                qa.vantarpa = "";
-                                $log.debug("warning: undefined status");
+angular
+        .module('wcDashBoardApp')
+        .controller(
+                'UnhandledQACtrl',
+                [
+                        '$scope',
+                        '$window',
+                        '$log',
+                        '$timeout',
+                        '$filter',
+                        'dashBoardService',
+                        function UnhandledCertCtrl($scope, $window, $log, $timeout, $filter, dashBoardService) {
+                            // init state
+                            $scope.widgetState = {
+                                doneLoading : false,
+                                activeErrorMessageKey : null
                             }
-                        }
-                    });
 
-                } else {
-                    $scope.widgetState.hasError = true;
-                }
-            });
+                            $scope.isCollapsed = true;
 
-            $scope.onVidareBefordradChange = function(qa) {
-                qa.updateInProgress = true;
-                $timeout(function() { // wrap in timeout to simulate latency -
-                    dashBoardService.setVidareBefordradState(qa.internReferens, qa.vidarebefordrad, function(result) {
-                        qa.updateInProgress = false;
-                        if (result != null) {
-                            $log.debug("onVidareBefordradChange result - checked state is now " + result.vidarebefordrad);
-                            qa.vidarebefordrad = result.vidarebefordrad;
-                        } else {
-                            $scope.widgetState.hasError = true;
-                        }
-                    });
-                }, 1000);
-            }
+                            $scope.qaList = {};
+                            $scope.activeUnit = "";
 
-            $scope.setActiveUnit = function(unit) {
-                $log.debug("ActiveUnit is now:" + unit);
-                $scope.activeUnit = unit;
-            }
+                            // load all fragasvar for all units in usercontext
+                            dashBoardService.getQA(function(data) {
+                                $scope.widgetState.doneLoading = true;
+                                if (data != null) {
+                                    $scope.widgetState.activeErrorMessageKey = null;
+                                    $scope.qaList = data;
 
-            // Calculate how many entities we have for a specific enhetsId
-            $scope.getItemCountForUnitId = function(unit) {
-                if (!$scope.widgetState.doneLoading) {
-                    return "?";
-                }
-                var count = $filter('QAEnhetsIdFilter')($scope.qaList, unit.id).length;
+                                    // set waiting messages
+                                    angular.forEach($scope.qaList, function(qa, key) {
 
-                return count;
-            }
+                                        if (qa.status == "ANSWERED" || qa.amne == "MAKULERING" || qa.amne == "PAMINNELSE") {
+                                            qa.vantarpa = "markhandled";
+                                        } else if (qa.status == "CLOSED") {
+                                            qa.vantarpa = "handled";
+                                        } else if (qa.amne == "KOMPLETTERING_AV_LAKARINTYG") {
+                                            qa.vantarpa = "komplettering";
+                                        } else {
 
-            $scope.openIntyg = function(intygsReferens) {
-                $log.debug("open intyg " + intygsReferens.intygsId);
-                $window.location.href = "/m/" + intygsReferens.intygsTyp.toLowerCase() + "/webcert/intyg/" + intygsReferens.intygsId;
-            }
+                                            if (qa.status == "PENDING_INTERNAL_ACTION") {
+                                                qa.vantarpa = "svarfranvarden";
+                                            } else if (qa.status == "PENDING_EXTERNAL_ACTION") {
+                                                qa.vantarpa = "svarfranfk";
+                                            } else {
+                                                qa.vantarpa = "";
+                                                $log.debug("warning: undefined status");
+                                            }
+                                        }
+                                    });
 
-            $scope.mailDialogOpen = false;
-            $scope.qaToMail = {};
-            $scope.dialogOpts = {
-                backdropFade : true,
-                dialogFade : true
-            };
+                                } else {
+                                    $scope.widgetState.activeErrorMessageKey = "error.unansweredcerts.couldnotbeloaded";
+                                }
+                            });
 
-            $scope.openDialog = function(mail) {
-                $scope.qaToMail = mail;
-                $scope.mailDialogOpen = true;
-            }
+                            $scope.onVidareBefordradChange = function(qa) {
+                                qa.updateInProgress = true;
+                                $timeout(
+                                        function() { // wrap in timeout to
+                                                        // simulate
+                                            // latency -
+                                            dashBoardService
+                                                    .setVidareBefordradState(
+                                                            qa.internReferens,
+                                                            qa.vidarebefordrad,
+                                                            function(result) {
+                                                                qa.updateInProgress = false;
 
-            $scope.closeDialog = function() {
-                $scope.mailDialogOpen = false;
-            }
+                                                                if (result != null) {
+                                                                    qa.vidarebefordrad = result.vidarebefordrad;
+                                                                } else {
+                                                                    qa.vidarebefordrad = !qa.vidarebefordrad;
+                                                                    dashBoardService
+                                                                            .showErrorMessageDialog("Kunde inte markera/avmarkera frågan som vidarebefordrad. Försök gärna igen för att se om felet är tillfälligt. Annars kan du kontakta supporten");
+                                                                }
+                                                            });
+                                        }, 1000);
+                            }
 
-        } ]);
+                            $scope.setActiveUnit = function(unit) {
+                                $log.debug("ActiveUnit is now:" + unit);
+                                $scope.activeUnit = unit;
+                            }
+
+                            // Calculate how many entities we have for a
+                            // specific
+                            // enhetsId
+                            $scope.getItemCountForUnitId = function(unit) {
+                                if (!$scope.widgetState.doneLoading) {
+                                    return "?";
+                                }
+                                var count = $filter('QAEnhetsIdFilter')($scope.qaList, unit.id).length;
+
+                                return count;
+                            }
+
+                            $scope.openIntyg = function(intygsReferens) {
+                                $log.debug("open intyg " + intygsReferens.intygsId);
+                                $window.location.href = "/m/" + intygsReferens.intygsTyp.toLowerCase() + "/webcert/intyg/" + intygsReferens.intygsId;
+                            }
+
+                            $scope.mailDialogOpen = false;
+                            $scope.qaToMail = {};
+                            $scope.dialogOpts = {
+                                backdropFade : true,
+                                dialogFade : true
+                            };
+
+                            $scope.openDialog = function(mail) {
+                                $scope.qaToMail = mail;
+                                $scope.mailDialogOpen = true;
+                            }
+
+                            $scope.closeDialog = function() {
+                                $scope.mailDialogOpen = false;
+                            }
+
+                        } ]);
