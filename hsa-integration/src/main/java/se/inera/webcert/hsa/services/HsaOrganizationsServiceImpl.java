1d9e2777544dcbb4019da08fcf0344fbc5683f0c
Andreas Kaltenbach <andreas.kaltenbach@googlemail.com>
2013-10-17 12:44:01 +0200

    fetching sub-enheter and mottagningar from HSA

--- hsa-integration/src/main/java/se/inera/webcert/hsa/services/HsaOrganizationsServiceImpl.java
+++ hsa-integration/src/main/java/se/inera/webcert/hsa/services/HsaOrganizationsServiceImpl.java
@@ -1,25 +1,32 @@
 package se.inera.webcert.hsa.services;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
-import java.util.Map;
 
 import com.google.common.base.Function;
 import com.google.common.base.Predicate;
-import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableListMultimap;
 import com.google.common.collect.Iterables;
-import com.google.common.collect.Maps;
 import com.google.common.collect.Multimaps;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import se.inera.ifv.hsawsresponder.v3.AttributeListType;
+import se.inera.ifv.hsawsresponder.v3.AttributeValuePairType;
+import se.inera.ifv.hsawsresponder.v3.CareUnitType;
+import se.inera.ifv.hsawsresponder.v3.ExactType;
+import se.inera.ifv.hsawsresponder.v3.GetCareUnitListResponseType;
+import se.inera.ifv.hsawsresponder.v3.GetHsaUnitResponseType;
 import se.inera.ifv.hsawsresponder.v3.GetMiuForPersonResponseType;
 import se.inera.ifv.hsawsresponder.v3.GetMiuForPersonType;
+import se.inera.ifv.hsawsresponder.v3.HsawsSimpleLookupResponseType;
+import se.inera.ifv.hsawsresponder.v3.HsawsSimpleLookupType;
+import se.inera.ifv.hsawsresponder.v3.LookupHsaObjectType;
 import se.inera.ifv.hsawsresponder.v3.MiuInformationType;
+import se.inera.ifv.hsawsresponder.v3.SearchOperatorExact;
 import se.inera.ifv.webcert.spi.authorization.impl.HSAWebServiceCalls;
+import se.inera.webcert.hsa.model.Mottagning;
 import se.inera.webcert.hsa.model.Vardenhet;
 import se.inera.webcert.hsa.model.Vardgivare;
 
@@ -46,21 +53,22 @@ public class HsaOrganizationsServiceImpl implements HsaOrganizationsService {
         GetMiuForPersonResponseType response = client.callMiuRights(parameters);
 
         // filter by syfte. Only 'Vård och behandling' assignments are relevant for WebCert.
-        Iterable<MiuInformationType> filteredMius = Iterables.filter(response.getMiuInformation(), new Predicate<MiuInformationType>() {
-            @Override
-            public boolean apply(MiuInformationType miuInformationType) {
-                return VARD_OCH_BEHANDLING.equalsIgnoreCase(miuInformationType.getMiuPurpose());
-            }
-        });
+        Iterable<MiuInformationType> filteredMius = Iterables.filter(response.getMiuInformation(),
+                new Predicate<MiuInformationType>() {
+                    @Override
+                    public boolean apply(MiuInformationType miuInformationType) {
+                        return VARD_OCH_BEHANDLING.equalsIgnoreCase(miuInformationType.getMiuPurpose());
+                    }
+                });
 
         // group medarbetaruppdrag by vardgivare ID
-        ImmutableListMultimap<String, MiuInformationType> vardgivareIdToMiuInformation =
-                Multimaps.index(filteredMius, new Function<MiuInformationType, String>() {
-            @Override
-            public String apply(MiuInformationType miuInformation) {
-                return miuInformation.getCareGiver();
-            }
-        });
+        ImmutableListMultimap<String, MiuInformationType> vardgivareIdToMiuInformation = Multimaps.index(filteredMius,
+                new Function<MiuInformationType, String>() {
+                    @Override
+                    public String apply(MiuInformationType miuInformation) {
+                        return miuInformation.getCareGiver();
+                    }
+                });
 
         for (String vardgivareId : vardgivareIdToMiuInformation.keySet()) {
             vardgivareList.add(convert(vardgivareIdToMiuInformation.get(vardgivareId)));
@@ -68,56 +76,107 @@ public class HsaOrganizationsServiceImpl implements HsaOrganizationsService {
         return vardgivareList;
     }
 
+    private List<Vardenhet> fetchAllEnheter(Vardgivare vardgivare, MiuInformationType miuInformationType) {
+
+        List<Vardenhet> vardenheter = new ArrayList<>();
+
+        String enhetDn = fetchDistinguishedName(miuInformationType);
+
+        List<CareUnitType> careUnits = fetchSubEnheter(vardgivare, enhetDn);
+
+        // TODO - check care unit start and end date!!!
+
+        for (CareUnitType careUnit : careUnits) {
+            Vardenhet vardenhet = new Vardenhet(careUnit.getHsaIdentity(), careUnit.getCareUnitName());
+            attachMottagningar(vardenhet);
+            vardenheter.add(vardenhet);
+        }
+
+        return vardenheter;
+    }
+
+    private List<CareUnitType> fetchSubEnheter(Vardgivare vardgivare, String enhetDn) {
+        LookupHsaObjectType parameters = new LookupHsaObjectType();
+        parameters.setHsaIdentity(vardgivare.getId());
+        parameters.setSearchBase(enhetDn);
+        GetCareUnitListResponseType response = client.callGetCareUnitList(parameters);
+        return response.getCareUnits().getCareUnit();
+    }
+
+    private String fetchDistinguishedName(MiuInformationType miuInformationType) {
+        HsawsSimpleLookupType lookupType = new HsawsSimpleLookupType();
+        ExactType exactType = new ExactType();
+        exactType.setSearchAttribute("hsaIdentity");
+        exactType.setSearchOperator(SearchOperatorExact.EXACT);
+        exactType.setValue(miuInformationType.getCareUnitHsaIdentity());
+
+        AttributeListType attributeList = new AttributeListType();
+        attributeList.getAttribute().add("hsaIdentity");
+        lookupType.setAttributes(attributeList);
+        lookupType.setLookup(exactType);
+
+        HsawsSimpleLookupResponseType lookupResponse = client.callHsawsSimpleLookup(lookupType);
+
+        return lookupResponse.getResponseValues().get(0).getDN();
+    }
+
+    private void attachMottagningar(Vardenhet vardenhet) {
+        List<String> mottagningsIds = fetchMottagningsHsaId(vardenhet);
+        for (String mottagningsId : mottagningsIds) {
+            Mottagning mottagning = fetchMottagning(mottagningsId);
+            vardenhet.getMottagningar().add(mottagning);
+        }
+    }
+
+    private Mottagning fetchMottagning(String mottagningsHsaId) {
+        GetHsaUnitResponseType response = client.callGetHsaunit(mottagningsHsaId);
+        return new Mottagning(response.getHsaIdentity(), response.getName());
+    }
 
 
+    private List<String> fetchMottagningsHsaId(Vardenhet vardenhet) {
+        List<String> ids = new ArrayList<>();
 
+        HsawsSimpleLookupType lookupType = new HsawsSimpleLookupType();
 
+        ExactType exactType = new ExactType();
+        exactType.setSearchAttribute("hsaIdentity");
+        exactType.setSearchOperator(SearchOperatorExact.EXACT);
+        exactType.setValue(vardenhet.getId());
 
+        AttributeListType attributeList = new AttributeListType();
+        attributeList.getAttribute().add("hsaHealthCareUnitMember");
+        lookupType.setAttributes(attributeList);
 
-        /*
-        for (MiuInformationType miu : response.getMiuInformation()) {
-            // Check that MiuPurpose is "Vård och behandling"
-            if (miu.getMiuPurpose().equalsIgnoreCase("Vård och behandling")) {
+        // TODO - improve - search base can be narrowed down
+        // lookupType.setSearchBase("c=SE");
+        lookupType.setLookup(exactType);
 
-                // Add data as a new CareUnit and CareGiver
+        HsawsSimpleLookupResponseType lookupResponse = client.callHsawsSimpleLookup(lookupType);
 
-                String vardgivareId = miu.getCareGiver();
+        if (lookupResponse.getResponseValues().isEmpty()) {
+            return ids;
+        }
 
-                Vardgivare vardgivare = new Vardgivare(vardgivareId, miu.getCareGiverName());
-
-                int index = vardgivareList.indexOf(vardgivare);
-                if (index != -1) {
-                    vardgivare = vardgivareList.get()
-
-                }
-                if (vardgivareList.contains(new Vardgivare(vardgivareId)))
-
-                Vardgivare vardgivare = new Vardgivare(miu.getCareGiver(), miu.getCareGiverName());
-
-
-                if (vardgivareMap.containsKey(vardgivare)) {
-                    vardgivare.
-                }
-
-                Vardenhet enhet = new Vardenhet();
-                enhet.setId(miu.getCareUnitHsaIdentity());
-                enhet.setNamn(miu.getCareUnitName());
-                vardgivare.getVardenheter().add(enhet);
-
-                vardgivareMap.put(vardgivareId, vardgivare);
+        List<AttributeValuePairType> attributes = lookupResponse.getResponseValues().get(0).getResponse();
+        for (AttributeValuePairType attribute : attributes) {
+            if ("hsaHealthCareUnitMember".equals(attribute.getAttribute())) {
+                ids.addAll(attribute.getValue());
             }
         }
-        //return vardgivareMap.values();
-
-         */
-
+        
+        return ids;
+    }
 
     private Vardgivare convert(List<MiuInformationType> miuInformationTypes) {
 
-        Vardgivare vardgivare = new Vardgivare(miuInformationTypes.get(0).getCareGiver(), miuInformationTypes.get(0).getCareGiverName());
+        Vardgivare vardgivare = new Vardgivare(miuInformationTypes.get(0).getCareGiver(), miuInformationTypes.get(0)
+                .getCareGiverName());
 
         for (MiuInformationType miuInformationType : miuInformationTypes) {
-            vardgivare.getVardenheter().add(new Vardenhet(miuInformationType.getCareUnitHsaIdentity(), miuInformationType.getCareUnitName()));
+
+            List<Vardenhet> enheter = fetchAllEnheter(vardgivare, miuInformationType);
+            vardgivare.getVardenheter().addAll(enheter);
         }
         return vardgivare;
     }
