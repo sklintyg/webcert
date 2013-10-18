07700ce2299671d53e011ff1ad646d98540175b4
Marcus Cedergren <marcus.cedergren@callistaenterprise.se>
2013-10-18 10:28:24 +0200

    added error handling and cfx exception mapping

--- webcert/src/main/java/se/inera/webcert/service/exception/WebCertServiceErrorCodeEnum.java
+++ webcert/src/main/java/se/inera/webcert/service/exception/WebCertServiceErrorCodeEnum.java
@@ -1,8 +1,10 @@
 package se.inera.webcert.service.exception;
 
 public enum WebCertServiceErrorCodeEnum {
-    UNKNOWN_INTERNAL_PROBLEM,
-    INTERNAL_PROBLEM, 
-    AUTHORIZATION_PROBLEM,
-    EXTERNAL_SYSTEM_PROBLEM,  
+
+    INTERNAL_PROBLEM, // Generic tech problem
+    INVALID_STATE, // Operation not allowed at this state, probably because of concurrency issues
+    AUTHORIZATION_PROBLEM, // User is not authorized for the operation
+    EXTERNAL_SYSTEM_PROBLEM, // Other system in unavailable, gave technical error response
+    UNKNOWN_INTERNAL_PROBLEM // All others
 }
