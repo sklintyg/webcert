ae83150fe109178197b39746faa374cbbebe8613
Olof Klason <olof.klason@callistaenterprise.se>
2013-11-15 13:26:10 +0100

    SauceLabs-driver för Chrome, IE och Safari

--- specifications/src/main/resources/GebConfig.groovy
+++ specifications/src/main/resources/GebConfig.groovy
@@ -6,7 +6,7 @@ import org.openqa.selenium.remote.DesiredCapabilities
 import org.openqa.selenium.remote.RemoteWebDriver
 import org.openqa.selenium.Platform
 
-driver = { new FirefoxDriver() } // use firefox by default
+//driver = { new FirefoxDriver() } // use firefox by default
 //driver = { new ChromeDriver() }
 //driver = { new HtmlUnitDriver() }
 //baseUrl = "http://localhost:8088"
@@ -14,12 +14,45 @@ waiting {
     timeout = 2 // default wait is two seconds
 }
 environments {
-     saucelabs {
+	saucelabschrome {
+		// Login to saucelabs.com. Name: olofklason  Pwd: MittSauceLabs1
+		driver = {
+			DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+			capabilities.setCapability("version", "31");
+			capabilities.setCapability("platform", Platform.WIN8);
+			new RemoteWebDriver(
+						  new URL("http://olofklason:da0ae2dc-0522-4c76-972c-26e4e900ecfe@ondemand.saucelabs.com:80/wd/hub"),
+						  capabilities);
+		}
+	}
+	saucelabsfirefox {
         // Login to saucelabs.com. Name: olofklason  Pwd: MittSauceLabs1
         driver = {
             DesiredCapabilities capabilities = DesiredCapabilities.firefox();
             capabilities.setCapability("version", "23");
             capabilities.setCapability("platform", Platform.WIN8);
+            new RemoteWebDriver(
+                          new URL("http://olofklason:da0ae2dc-0522-4c76-972c-26e4e900ecfe@ondemand.saucelabs.com:80/wd/hub"),
+                          capabilities);
+        }
+    }
+	saucelabsie {
+		// Login to saucelabs.com. Name: olofklason  Pwd: MittSauceLabs1
+		driver = {
+			DesiredCapabilities capabilities = DesiredCapabilities.internetExplore();
+			capabilities.setCapability("version", "10");
+			capabilities.setCapability("platform", Platform.WIN8);
+			new RemoteWebDriver(
+						  new URL("http://olofklason:da0ae2dc-0522-4c76-972c-26e4e900ecfe@ondemand.saucelabs.com:80/wd/hub"),
+						  capabilities);
+		}
+	}
+    saucelabssafari {
+        // Login to saucelabs.com. Name: olofklason  Pwd: MittSauceLabs1
+        driver = {
+            DesiredCapabilities capabilities = DesiredCapabilities.safari();
+            capabilities.setCapability("version", "5");
+            capabilities.setCapability("platform", "OS X 10.6");
             new RemoteWebDriver(
                           new URL("http://olofklason:da0ae2dc-0522-4c76-972c-26e4e900ecfe@ondemand.saucelabs.com:80/wd/hub"),
                           capabilities);
