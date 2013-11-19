424e852ab5f72998e13db86efdbe4542ff41a13d
Marcus Cedergren <marcus.cedergren@callistaenterprise.se>
2013-11-19 09:10:03 +0100

    removed random test stats and return 0 for unimplemented stat numbers

--- webcert/src/main/java/se/inera/webcert/web/controller/moduleapi/StatModuleApiController.java
+++ webcert/src/main/java/se/inera/webcert/web/controller/moduleapi/StatModuleApiController.java
@@ -2,7 +2,6 @@ package se.inera.webcert.web.controller.moduleapi;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -33,16 +32,16 @@ public class StatModuleApiController {
     @GET
     @Path("/")
     @Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
-    public Response intyg(@PathParam("intygId") String intygId) {
+    public Response getStats() {
 
         // TODO: get actual number for from service(s)
-        StatEntry userStat = new StatEntry((int) (Math.random() * 5), 0);
+        StatEntry userStat = new StatEntry(0, 0);
 
         WebCertUser user = webCertUserService.getWebCertUser();
 
         long unhandledQuestions = fragaSvarService.getUnhandledFragaSvarForUnitsCount(user.getVardenheterIds());
         // TODO: get actual unsigned certs for unit numbers from service(s) when its implemented
-        StatEntry unitStat = new StatEntry((int) (Math.random() * 10), unhandledQuestions);
+        StatEntry unitStat = new StatEntry(0, unhandledQuestions);
         StatRequestResponse stats = new StatRequestResponse(unitStat, userStat);
         return Response.ok(stats).build();
     }
