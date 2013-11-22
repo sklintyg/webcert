ec4a40474aa340d7337fb44f1a0037f059daa617
nikpet <niklas.pettersson@r2m.se>
2013-11-22 16:09:10 +0100

    Added persistence entity and repository for migrated Medcert certificates. Updated the Spring context for persistence.

---
+++ persistence/src/main/java/se/inera/webcert/persistence/legacy/repository/MigreratMedcertIntygRepository.java
@@ -0,0 +1,9 @@
+package se.inera.webcert.persistence.legacy.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import se.inera.webcert.persistence.legacy.model.MigreratMedcertIntyg;
+
+public interface MigreratMedcertIntygRepository extends CrudRepository<MigreratMedcertIntyg, String> {
+
+}
