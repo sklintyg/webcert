5d2e2de834905837f82ff7caf0a6e06aad3217ca
Marcus Cedergren <marcus.cedergren@callistaenterprise.se>
2013-12-03 16:38:23 +0100

    made intygstjanststub return not found result when requesting a certificate it doesnt have

--- intygstjanst-stub/src/main/java/se/inera/webcert/intygstjanststub/GetCertificateForCareResponderStub.java
+++ intygstjanst-stub/src/main/java/se/inera/webcert/intygstjanststub/GetCertificateForCareResponderStub.java
@@ -1,14 +1,17 @@
 package se.inera.webcert.intygstjanststub;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
 import se.inera.certificate.clinicalprocess.healthcond.certificate.getcertificateforcare.v1.GetCertificateForCareRequestType;
 import se.inera.certificate.clinicalprocess.healthcond.certificate.getcertificateforcare.v1.GetCertificateForCareResponderInterface;
 import se.inera.certificate.clinicalprocess.healthcond.certificate.getcertificateforcare.v1.GetCertificateForCareResponseType;
+import se.inera.certificate.clinicalprocess.healthcond.certificate.v1.ResultCodeType;
+import se.inera.certificate.clinicalprocess.healthcond.certificate.v1.ResultType;
 
 /**
- * Stub class for mocking Intygstjanstens {@link GetCertificateForCareResponderStub} WS interface
- * Uses a simple in memory store for complete  {@link GetCertificateForCareResponseType} responses
- *
+ * Stub class for mocking Intygstjanstens {@link GetCertificateForCareResponderStub} WS interface Uses a simple in
+ * memory store for complete {@link GetCertificateForCareResponseType} responses
+ * 
  * @author marced
  */
 public class GetCertificateForCareResponderStub implements GetCertificateForCareResponderInterface {
@@ -16,12 +19,26 @@ public class GetCertificateForCareResponderStub implements GetCertificateForCare
     @Autowired
     private IntygStore intygStore;
 
-
     @Override
-    public GetCertificateForCareResponseType getCertificateForCare(String logicalAddress, GetCertificateForCareRequestType request) {
+    public GetCertificateForCareResponseType getCertificateForCare(String logicalAddress,
+            GetCertificateForCareRequestType request) {
 
-        return intygStore.getAllIntyg().get(request.getCertificateId());
+         GetCertificateForCareResponseType intygResponse = intygStore.getAllIntyg().get(request.getCertificateId());
+         if (intygResponse!=null) {
+             return intygResponse;
+         } else {
+             return buildNotFoundResponse();
+         }
     }
 
+    private GetCertificateForCareResponseType buildNotFoundResponse() {
+        GetCertificateForCareResponseType response = new GetCertificateForCareResponseType();
+        ResultType resultType = new ResultType();
+        resultType.setResultCode(ResultCodeType.VALIDATION_ERROR);
+        resultType.setResultText("Intyg not found in stub");
+        response.setResult(resultType);
+        return response;
+        
+    }
 
 }
