a0010c9000c1dff6de795b806b78ee97b294b708
Andreas Kaltenbach <andreas.kaltenbach@googlemail.com>
2013-12-05 15:34:31 +0100

    improving LogServiceImplTest -adding assertions to check the resulting log message -removing unnecessary mocks -removing unused variables

--- webcert/src/test/java/se/inera/webcert/service/LogServiceImplTest.java
+++ webcert/src/test/java/se/inera/webcert/service/LogServiceImplTest.java
@@ -1,32 +1,32 @@
 package se.inera.webcert.service;
 
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Ignore;
+import javax.jms.Session;
+import javax.xml.bind.JAXBContext;
+import javax.xml.transform.stream.StreamSource;
+
+import static org.joda.time.LocalDateTime.now;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.only;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.jms.core.MessageCreator;
-import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import se.inera.certificate.clinicalprocess.healthcond.certificate.getcertificateforcare.v1.GetCertificateForCareResponseType;
-import se.inera.certificate.clinicalprocess.healthcond.certificate.v1.*;
-
 import se.inera.log.messages.IntygReadMessage;
 import se.inera.webcert.hsa.model.WebCertUser;
 import se.inera.webcert.web.service.WebCertUserService;
 import se.inera.webcert.web.service.WebCertUserServiceImpl;
-
-import javax.jms.*;
-
-import static org.mockito.Mockito.*;
-import static org.mockito.Mockito.verify;
 
 /**
  * Created by pehr on 13/11/13.
@@ -37,69 +37,61 @@ public class LogServiceImplTest {
     @Mock
     private JmsTemplate template = mock(JmsTemplate.class);
 
-
     @Mock
     protected WebCertUserService webCertUserService = new WebCertUserServiceImpl();
 
     @InjectMocks
     LogServiceImpl logService = new LogServiceImpl();
 
-
-
     @Test
-    public void serviceSendsDocumentAndIdForCreate() throws JMSException {
-        ArgumentCaptor<MessageCreator> captor = ArgumentCaptor.forClass(MessageCreator.class);
+    public void serviceSendsDocumentAndIdForCreate() throws Exception {
+        logService.systemId = "webcert";
 
-        //Certificate certificate = mock(Certificate.class);
-        //when(certificate.getDocument()).thenReturn("The document");
-        //when(certificate.getId()).thenReturn("The id");
+        ArgumentCaptor<MessageCreator> messageCreatorCaptor = ArgumentCaptor.forClass(MessageCreator.class);
 
-        when(webCertUserService.getWebCertUser()).thenReturn(createWcUser()) ;
+        when(webCertUserService.getWebCertUser()).thenReturn(createWcUser());
 
-        GetCertificateForCareResponseType intyg = mock(GetCertificateForCareResponseType.class);
-        UtlatandeType utlatande = mock(UtlatandeType.class);
-        HosPersonalType persType = mock(HosPersonalType.class);
-        EnhetType enhetTyp = mock(EnhetType.class);
-        VardgivareType vgType = mock(VardgivareType.class);
-        HsaId enhetHsaId = mock(HsaId.class);
-        HsaId vgHsaId = mock(HsaId.class);
+        GetCertificateForCareResponseType certificate = certificate();
 
+        logService.logReadOfIntyg(certificate);
 
+        verify(template, only()).send(messageCreatorCaptor.capture());
 
-        when(vgHsaId.getExtension()).thenReturn("VARDGIVARE ID");
-        when(vgType.getVardgivareId()).thenReturn(vgHsaId);
-        when(enhetTyp.getVardgivare()).thenReturn(vgType);
+        MessageCreator messageCreator = messageCreatorCaptor.getValue();
 
-        when(enhetHsaId.getExtension()).thenReturn("ENHETS ID");
-        when(enhetTyp.getEnhetsId()).thenReturn(enhetHsaId);
-        when(persType.getEnhet()).thenReturn(enhetTyp);
-        when(utlatande.getSkapadAv()).thenReturn(persType);
-        when(intyg.getCertificate()).thenReturn(utlatande);
-
-        IntygReadMessage logthis = mock(IntygReadMessage.class);
-
-        ObjectMessage message = mock(ObjectMessage.class);
         Session session = mock(Session.class);
-        //when(session.createObjectMessage(any(IntygReadMessage.class))).thenReturn(message);
+        ArgumentCaptor<IntygReadMessage> intygReadMessageCaptor = ArgumentCaptor.forClass(IntygReadMessage.class);
+        when(session.createObjectMessage(intygReadMessageCaptor.capture())).thenReturn(null);
 
+        messageCreator.createMessage(session);
 
-        logService.logReadOfIntyg(intyg);
+        IntygReadMessage intygReadMessage = intygReadMessageCaptor.getValue();
 
-        verify(template, only()).send(captor.capture());
-        captor.getValue().createMessage(session);
+        assertNotNull(intygReadMessage.getLogId());
+        assertEquals("Läsa", intygReadMessage.getActivityType());
+        assertEquals("Vård och behandling", intygReadMessage.getPurpose());
+        assertEquals("intyg", intygReadMessage.getResourceType());
 
-        IntygReadMessage msg = (IntygReadMessage)message.getObject();
+        assertEquals("HSAID", intygReadMessage.getUserId());
+
+        assertEquals("ENHETS ID", intygReadMessage.getEnhetId());
+        assertEquals("VARDGIVARE ID", intygReadMessage.getVardgivareId());
+
+        assertTrue(intygReadMessage.getTimestamp().minusSeconds(10).isBefore(now()));
+        assertTrue(intygReadMessage.getTimestamp().plusSeconds(10).isAfter(now()));
+
+        assertEquals("webcert", intygReadMessage.getSystemId());
     }
 
+    private GetCertificateForCareResponseType certificate() throws Exception {
+        return JAXBContext.newInstance(GetCertificateForCareResponseType.class).createUnmarshaller().unmarshal(
+                new StreamSource(new ClassPathResource("LogServiceTest/certificate.xml").getInputStream()),
+                GetCertificateForCareResponseType.class).getValue();
+    }
 
-
-
-    protected WebCertUser createWcUser(){
+    private WebCertUser createWcUser() {
         WebCertUser wcu = new WebCertUser();
         wcu.setHsaId("HSAID");
-        wcu.setNamn("TESTUSER TESTSSSON");
-
-
         return wcu;
     }
 }
