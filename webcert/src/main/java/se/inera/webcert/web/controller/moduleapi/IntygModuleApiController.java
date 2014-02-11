3c589797f9020907071ce6b6418f50cfe689732e
nikpet <niklas.pettersson@r2m.se>
2014-02-11 17:19:31 +0100

    Added RUD-methods for draft certificates to the module REST service

--- webcert/src/main/java/se/inera/webcert/web/controller/moduleapi/IntygModuleApiController.java
+++ webcert/src/main/java/se/inera/webcert/web/controller/moduleapi/IntygModuleApiController.java
@@ -3,6 +3,9 @@ package se.inera.webcert.web.controller.moduleapi;
 import static javax.ws.rs.core.Response.Status.INTERNAL_SERVER_ERROR;
 import static javax.ws.rs.core.Response.Status.OK;
 
+import java.io.UnsupportedEncodingException;
+import java.util.List;
+
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
@@ -10,6 +13,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Request;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
@@ -24,10 +28,16 @@ import se.inera.certificate.integration.rest.ModuleRestApiFactory;
 import se.inera.certificate.integration.rest.dto.CertificateContentHolder;
 import se.inera.webcert.hsa.model.WebCertUser;
 import se.inera.webcert.persistence.intyg.model.Intyg;
+import se.inera.webcert.persistence.intyg.model.IntygsStatus;
 import se.inera.webcert.persistence.intyg.model.VardpersonReferens;
 import se.inera.webcert.persistence.intyg.repository.IntygRepository;
 import se.inera.webcert.service.IntygService;
+import se.inera.webcert.service.draft.IntygDraftService;
+import se.inera.webcert.service.draft.dto.DraftValidation;
+import se.inera.webcert.service.draft.dto.DraftValidationMessage;
+import se.inera.webcert.web.controller.moduleapi.dto.DraftValidationStatus;
 import se.inera.webcert.web.controller.moduleapi.dto.IntygDraftHolder;
+import se.inera.webcert.web.controller.moduleapi.dto.SaveDraftResponse;
 import se.inera.webcert.web.service.WebCertUserService;
 
 /**
@@ -35,15 +45,20 @@ import se.inera.webcert.web.service.WebCertUserService;
  */
 public class IntygModuleApiController {
 
+    private static final String UTF_8 = "UTF-8";
+
     private static final Logger LOG = LoggerFactory.getLogger(IntygModuleApiController.class);
 
-    private static final String UTF_8 = ";charset=utf-8";
+    private static final String UTF_8_CHARSET = ";charset=utf-8";
     
     private static final String CONTENT_DISPOSITION = "Content-Disposition";
 
     @Autowired
     private IntygService intygService;
 
+    @Autowired
+    private IntygDraftService draftService;
+    
     @Autowired
     private IntygRepository intygRepository;
     
@@ -61,7 +76,7 @@ public class IntygModuleApiController {
      */
     @GET
     @Path("/draft/{intygId}")
-    @Produces(MediaType.APPLICATION_JSON + UTF_8)
+    @Produces(MediaType.APPLICATION_JSON + UTF_8_CHARSET)
     public Response getDraft(@PathParam("intygId") String intygId) {
         
         LOG.debug("Retrieving Intyg with id {}", intygId);
@@ -91,8 +106,8 @@ public class IntygModuleApiController {
     @PUT
     @Path("/draft/{intygId}")
     @Transactional
-    public Response saveDraft(@PathParam("intygId") String intygId, IntygDraftHolder draftHolder) {
-        
+    public Response saveDraft(@PathParam("intygId") String intygId, byte[] bytes) {
+                        
         LOG.debug("Saving Intyg with id {}", intygId);
         
         Intyg intyg = intygRepository.findOne(intygId);
@@ -102,15 +117,50 @@ public class IntygModuleApiController {
             return Response.status(Status.NOT_FOUND).build();
         }
         
-        intyg.setModel(draftHolder.getContent());
-        intyg.setStatus(draftHolder.getStatus());
+        String draftAsJson = fromBytesToString(bytes);
+        
+        String intygType = intyg.getIntygsTyp();
+        
+        DraftValidation draftValidation = draftService.validateDraft(intygId, intygType, draftAsJson);
+        
+        IntygsStatus intygStatus = (draftValidation.isDraftValid()) ? IntygsStatus.DRAFT_COMPLETE : IntygsStatus.DRAFT_INCOMPLETE;
+                
+        intyg.setModel(draftAsJson);
+        intyg.setStatus(intygStatus);
         
         VardpersonReferens vardPersonRef = createVardpersonReferens();
         intyg.setSenastSparadAv(vardPersonRef);
         
         intygRepository.save(intyg);
         
-        return Response.ok().build();
+        SaveDraftResponse responseEntity = buildSaveDraftResponse(draftValidation);
+        
+        return Response.ok().entity(responseEntity).build();
+    }
+    
+    private SaveDraftResponse buildSaveDraftResponse(DraftValidation draftValidation) {
+        
+        if (draftValidation.isDraftValid()) {
+            return new SaveDraftResponse(DraftValidationStatus.COMPLETE);
+        }
+        
+        SaveDraftResponse responseEntity = new SaveDraftResponse(DraftValidationStatus.INCOMPLETE);
+        
+        List<DraftValidationMessage> validationMessages = draftValidation.getMessages();
+        
+        for (DraftValidationMessage validationMessage : validationMessages) {
+            responseEntity.addMessage(validationMessage.getField(), validationMessage.getMessage());
+        }
+        
+        return responseEntity;
+    }
+    
+    private String fromBytesToString(byte[] bytes) {
+        try {
+            return new String(bytes, UTF_8);
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException("Could not convert the payload from bytes to String!");
+        }
     }
     
     /**
@@ -150,7 +200,7 @@ public class IntygModuleApiController {
     @Deprecated
     @GET
     @Path("/{intygId}")
-    @Produces(MediaType.APPLICATION_JSON + UTF_8)
+    @Produces(MediaType.APPLICATION_JSON + UTF_8_CHARSET)
     public Response getSignedIntyg(@PathParam("intygId") String intygId) {
         
         CertificateContentHolder fetchIntygData = intygService.fetchIntygData(intygId);
@@ -160,13 +210,11 @@ public class IntygModuleApiController {
 
     /**
      * Return the certificate identified by the given id as PDF.
-     * Note! Candidate for removal since we do not really know if this is used anywhere.
      * 
      * @param id
      *            - the globally unique id of a certificate.
      * @return The certificate in PDF format
      */
-    @Deprecated
     @GET
     @Path("/{intygId}/pdf")
     @Produces("application/pdf")
---
+++ webcert/src/main/java/se/inera/webcert/web/controller/moduleapi/dto/SaveDraftValidationMessage.java
@@ -0,0 +1,35 @@
+package se.inera.webcert.web.controller.moduleapi.dto;
+
+public class SaveDraftValidationMessage {
+
+    private String field;
+
+    private String message;
+
+    public SaveDraftValidationMessage() {
+
+    }
+    
+    public SaveDraftValidationMessage(String field, String message) {
+        super();
+        this.field = field;
+        this.message = message;
+    }
+
+    public String getField() {
+        return field;
+    }
+
+    public void setField(String field) {
+        this.field = field;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+}
