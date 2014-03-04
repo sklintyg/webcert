5f9de4e8a5fe99ce73980a6b3683b9dc886b46ca
Erik Forsgren <erik.forsgren@r2m.se>
2014-03-04 09:18:56 +0100

    Added message for Ts-diabetes to wc-dashboard

--- webcert/src/main/webapp/js/app/dashboard/messages.js
+++ webcert/src/main/webapp/js/app/dashboard/messages.js
@@ -24,6 +24,7 @@ var webcertResources = {
         "certificatetypes.fk7263.typename" : "Läkarintyg FK 7263",
         "certificatetypes.rli.typename" : "Intyg vid avbeställd resa",
         "certificatetypes.ts-bas.typename" : "Transportstyrelsens läkarintyg, bas",
+        "certificatetypes.ts-diabetes.typename" : "Transportstyrelsens läkarintyg, diabetes",
         
         //about texts
         "about.cookies" : "<h3>Kakor (Cookies)</h3><p>Så kallade kakor (cookies) används för att underlätta för besökaren på webbplatsen. En kaka är en textfil som lagras på din dator och som innehåller information. Denna webbplats använder så kallade sessionskakor. Sessionskakor lagras temporärt i din dators minne under tiden du är inne på en webbsida. Sessionskakor försvinner när du stänger din webbläsare. Ingen personlig information om dig sparas vid användning av sessionskakor. Om du inte accepterar användandet av kakor kan du stänga av det via din webbläsares säkerhetsinställningar. Du kan även ställa in webbläsaren så att du får en varning varje gång webbplatsen försöker sätta en kaka på din dator. </p><p><strong>Observera!</strong> Om du stänger av kakor i din webbläsare kan du inte logga in i Mina Intyg.</p><p>Allmän information om kakor (cookies) och lagen om elektronisk kommunikation finns på Post- och telestyrelsens webbplats.</p> <p><a href='http://www.pts.se/sv/Bransch/Regler/Lagar/Lag-om-elektronisk-kommunikation/Cookies-kakor/' target='_blank'>Mer om kakor (cookies) på Post- och telestyrelsens webbplats</a></p>",
