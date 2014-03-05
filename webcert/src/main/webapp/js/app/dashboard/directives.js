6123bca1a50cf3d702e5d03a882bf2c90ffc161f
Benny Samuelsson <benny.samuelsson@usify.se>
2014-03-05 14:57:55 +0100

    WEBCERT-333 Uppdatera grafisk layout för byt enhet enligt ny design

--- webcert/src/main/webapp/js/app/dashboard/directives.js
+++ webcert/src/main/webapp/js/app/dashboard/directives.js
@@ -9,25 +9,15 @@ directives.directive('wcCareUnitClinicSelector', ['$rootScope', '$cookieStore', 
             restrict : "A",
             transclude : false,
             replace : true,
-            template : '<table class="span12 table unit-table">' +
-                '<tr ng-repeat="unit in units">' +
-                '<td><button id="select-active-unit-{{unit.id}}" type="button" ng-click="selectUnit(unit)" class="qa-unit" ng-class="{selected : selectedUnit == unit}">{{unit.namn}}<span class="qa-circle" ng-class="{\'qa-circle-active\': getItemCountForUnitId(unit)>0}" title="Ohanterade frågor och svar">{{getItemCountForUnitId(unit)}}</span></button></td>' +
-                '</tr>' +
+            template :
+                '<table class="span12 table unit-table">' +
+                  '<tr ng-repeat="unit in units">' +
+                    '<td><button id="select-active-unit-{{unit.id}}" type="button" ng-click="selectUnit(unit)" class="qa-unit" ng-class="{selected : selectedUnit == unit}">{{unit.namn}}<span class="qa-circle" ng-class="{\'qa-circle-active\': getItemCountForUnitId(unit)>0}" title="Ohanterade frågor och svar">{{getItemCountForUnitId(unit)}}</span></button></td>' +
+                  '</tr>' +
                 '</table>',
             controller : function ($scope) {
 
-/*                var valdVardenhet = angular.copy(User.getValdVardenhet());
-
-                $scope.units = []; // aggregated units to present for vardenhet/mottagning choice
-                $scope.units.push(valdVardenhet);
-
-                angular.forEach(valdVardenhet.mottagningar, function (mottagning, key) {
-                    mottagning.namn = valdVardenhet.namn + ' - ' + mottagning.namn;
-                    this.push(mottagning);
-                }, $scope.units);
-*/
-              $scope.units = User.getVardenhetFilterList(User.getValdVardenhet());
-
+                $scope.units = User.getVardenhetFilterList(User.getValdVardenhet());
                 $scope.selectedUnit = null;
 
                 $scope.selectUnit = function (unit) {
@@ -41,7 +31,6 @@ directives.directive('wcCareUnitClinicSelector', ['$rootScope', '$cookieStore', 
                 } else if ($scope.units.length > 1 && $cookieStore.get("enhetsId")) {
                     $scope.selectUnit(selectUnitById($scope.units, $cookieStore.get("enhetsId")));
                 }
-
 
                 // Local function getting the first care unit's hsa id in the data struct.
                 function selectFirstUnit (units) {
