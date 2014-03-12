a6a65d7a7e503447d5d31c2ff70d0ec2e82583d2
nikpet <niklas.pettersson@r2m.se>
2014-03-12 14:10:36 +0100

    WEBCERT-359: Improved RestExceptionHandler and made sure it was included in all JAX-RS servers.

--- webcert/src/main/java/se/inera/webcert/web/controller/WebcertRestExceptionHandler.java
+++ webcert/src/main/java/se/inera/webcert/web/controller/WebcertRestExceptionHandler.java
@@ -1,4 +1,4 @@
-package se.inera.webcert.web.controller.moduleapi;
+package se.inera.webcert.web.controller;
 
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
@@ -11,20 +11,29 @@ import se.inera.webcert.service.exception.WebCertServiceErrorCodeEnum;
 import se.inera.webcert.service.exception.WebCertServiceException;
 import se.inera.webcert.web.controller.moduleapi.dto.ModuleRestExceptionResponse;
 
-public class ModuleRestExceptionHandler implements ExceptionMapper<RuntimeException> {
-    private static final Logger LOG = LoggerFactory.getLogger(ModuleRestExceptionHandler.class);
+/**
+ * Exception handler for REST services. Runtime exceptions thrown as {@link WebCertServiceException} WebCertServiceException are
+ * logged as warnings. All other runtime exceptions thrown are treated as errors and logged as such.
+ * 
+ * @author nikpet
+ *
+ */
+public class WebcertRestExceptionHandler implements ExceptionMapper<RuntimeException> {
+    
+    private static final Logger LOG = LoggerFactory.getLogger(WebcertRestExceptionHandler.class);
     
     @Override
     public Response toResponse(RuntimeException e) {
         ModuleRestExceptionResponse moduleResponse;
         //If this is an exception thown by our code, we have a more specific error code
         if (e instanceof WebCertServiceException) {
+            WebCertServiceException ex = (WebCertServiceException) e;
             //Exceptions thrown by us should be logged as warnings as they are thrown deliberately.
-            LOG.warn("WebCertServiceException",e);
-            moduleResponse = new ModuleRestExceptionResponse(((WebCertServiceException) e).getErrorCode(), e.getMessage());
+            LOG.warn("Internal exception occured! Internal error code: {} Error message: {}", ex.getErrorCode(), e.getMessage());
+            moduleResponse = new ModuleRestExceptionResponse(ex.getErrorCode(), e.getMessage());
         } else {
             //All other exceptions are logged as errors as they are not thrown deliberately.
-            LOG.error("Unhandled RuntimeException",e);
+            LOG.error("Unhandled RuntimeException occured!",e);            
             moduleResponse = new ModuleRestExceptionResponse(WebCertServiceErrorCodeEnum.UNKNOWN_INTERNAL_PROBLEM, e.getMessage());    
         }
         
