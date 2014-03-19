1e176e7dab02e3cdea702b24dc8e8e4fd63ce4da
Benny Samuelsson <benny.samuelsson@usify.se>
2014-03-19 16:26:03 +0100

    Added tooltip placement attribute to wcfield so placement can be configured

--- webcert/src/main/webapp/js/common/wc-utils.js
+++ webcert/src/main/webapp/js/common/wc-utils.js
@@ -257,11 +257,11 @@ angular.module('wc.utils').directive("wcField", [ 'messageService', function(mes
       fieldNumber: "@",
       fieldHelpText: "@",
       fieldHasErrors: "=",
-      fieldTooltipPlacement: "@?"
+      fieldTooltipPlacement: "@?" // top,bottom,left,right
     },
     controller : function($scope) {
 
-        if(!$scope.fieldTooltipPlacement){
+        if($scope.fieldTooltipPlacement == undefined){
             $scope.fieldTooltipPlacement = "right";
         }
 
@@ -274,7 +274,7 @@ angular.module('wc.utils').directive("wcField", [ 'messageService', function(mes
             +'<h4 class="cert-field-number" ng-if="fieldNumber != undefined"><span message key="modules.label.field"></span> {{fieldNumber}}</h4>'
             +'<h3 class="title"><span message key="{{fieldLabel}}"></span>' +
                 '<span ng-if="fieldHelpText != undefined" class="help" ' +
-                'tooltip-trigger="click" tooltip-html-unsafe="{{getMessage(fieldHelpText)}}" tooltip-placement="right">?</span></h3>'
+                'tooltip-trigger="click" tooltip-html-unsafe="{{getMessage(fieldHelpText)}}" tooltip-placement="{{fieldTooltipPlacement}}">?</span></h3>'
             +'<span class="text" ng-class="{fielderror: fieldHasErrors}">'
             +'  <span ng-transclude></span>'
             +'</span>'
