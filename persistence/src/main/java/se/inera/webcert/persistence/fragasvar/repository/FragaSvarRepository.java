9d0ee91d6cc23bf5c5ade65da818208de861540e
Bjorn Beskow <bb@callistaenterprise.se>
2014-03-26 12:49:42 +0100

    Ärv från JpaRepository istället för CrudRepository, för att få tillgång till deleteAllInBatch().

--- persistence/src/main/java/se/inera/webcert/persistence/fragasvar/repository/FragaSvarRepository.java
+++ persistence/src/main/java/se/inera/webcert/persistence/fragasvar/repository/FragaSvarRepository.java
@@ -1,13 +1,9 @@
 package se.inera.webcert.persistence.fragasvar.repository;
 
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import org.springframework.data.repository.query.Param;
 import se.inera.webcert.persistence.fragasvar.model.FragaSvar;
 
-import java.util.List;
-
-public interface FragaSvarRepository extends CrudRepository<FragaSvar, Long>, FragaSvarRepositoryCustom {
+public interface FragaSvarRepository extends JpaRepository<FragaSvar, Long>, FragaSvarRepositoryCustom {
 
 }
