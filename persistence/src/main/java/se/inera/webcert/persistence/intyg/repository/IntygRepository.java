6af3e14abbacdced5efc767704425002556361b0
nikpet <niklas.pettersson@r2m.se>
2014-03-26 11:20:27 +0100

    WEBCERT-462: Consolidated the changesets in Liquibase config. Resolved conflicts in changelog.xml and removed unused entities.

--- persistence/src/main/java/se/inera/webcert/persistence/intyg/repository/IntygRepository.java
+++
@@ -1,9 +0,0 @@
-package se.inera.webcert.persistence.intyg.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import se.inera.webcert.persistence.intyg.model.Intyg;
-
-public interface IntygRepository extends CrudRepository<Intyg, String>, IntygRepositoryCustom {
-
-}
