13781e45cd4bc297fd343c2d05e86b2c104f9ee0
nikpet <niklas.pettersson@r2m.se>
2014-03-31 14:09:22 +0200

    WEBCERT-469: Fixed so that the total is calculated correctly.

--- webcert/src/test/java/se/inera/webcert/web/controller/moduleapi/StatModuleApiControllerTest.java
+++ webcert/src/test/java/se/inera/webcert/web/controller/moduleapi/StatModuleApiControllerTest.java
@@ -2,6 +2,9 @@ package se.inera.webcert.web.controller.moduleapi;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.mockito.Matchers.anyListOf;
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.CoreMatchers.*;
 
 import java.util.Arrays;
 import java.util.HashMap;
@@ -10,8 +13,11 @@ import java.util.Map;
 
 import javax.ws.rs.core.Response;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
@@ -38,45 +44,158 @@ public class StatModuleApiControllerTest {
     @Mock
     private FragaSvarService fragaSvarService;
     
+    @Captor
+    private ArgumentCaptor<List<String>> listCaptor;
+    
     @InjectMocks 
     private StatModuleApiController statController;
+
+    private WebCertUser mockUser;
     
+    private Map<String, Long> statsMap;
     
-    @Test
-    public void testGetStatistics() {
+    private Vardgivare vg;
+    
+    private Vardenhet ve1, ve2, ve3, ve4;
+    
+    @Before
+    public void setupDataAndExpectations() {
         
-        List<String> ids = Arrays.asList("VE1","VE2");
+        statsMap = new HashMap<String, Long>(); 
         
-        List<Vardgivare> vardgivare = createVardgivare();
+        statsMap.put("VE1", new Long(2L));
+        statsMap.put("VE1M1", new Long(3L));
+        statsMap.put("VE1M2", new Long(3L));
+        statsMap.put("VE2", new Long(2L));
+        statsMap.put("VE3", new Long(1L));
+                
+        mockUser = new WebCertUser();
         
-        WebCertUser mockUser = Mockito.mock(WebCertUser.class);
+        ve1 = new Vardenhet("VE1", "Vardenhet1");
+        ve1.getMottagningar().add(new Mottagning("VE1M1", "Mottagning1"));
+        ve1.getMottagningar().add(new Mottagning("VE1M2", "Mottagning2"));
+        
+        ve2 = new Vardenhet("VE2", "Vardenhet2");
+        ve2.getMottagningar().add(new Mottagning("VE2M1", "Mottagning3"));
+        
+        ve3 = new Vardenhet("VE3", "Vardenhet3");
+        
+        ve4 = new Vardenhet("VE4", "Vardenhet4");
+        
+        vg = new Vardgivare("VG1", "Vardgivaren");
+        vg.setVardenheter(Arrays.asList(ve1, ve2, ve3, ve4));
+        
+        mockUser.setVardgivare(Arrays.asList(vg));
+        mockUser.setValdVardgivare(vg);
         
         Mockito.when(webCertUserService.getWebCertUser()).thenReturn(mockUser);
+    }
+    
+    @Test
+    public void testGetStatisticsWithSelectedUnitVE2() {
+  
+        mockUser.setValdVardenhet(ve2);
         
-        Mockito.when(mockUser.getIdsOfAllVardenheter()).thenReturn(ids);
-        Mockito.when(mockUser.getVardgivare()).thenReturn(vardgivare);
-        Mockito.when(mockUser.getValdVardenhet()).thenReturn(new Vardenhet("VE1", "Valdvardenhet"));
-        
-        Map<String, Long> statsMap = createStatsMap();
-        Mockito.when(fragaSvarService.getNbrOfUnhandledFragaSvarForCareUnits(ids)).thenReturn(statsMap);
+        Mockito.when(fragaSvarService.getNbrOfUnhandledFragaSvarForCareUnits(anyListOf(String.class))).thenReturn(statsMap);
                 
         Response response = statController.getStatistics();
         
         Mockito.verify(webCertUserService).getWebCertUser();
-        Mockito.verify(fragaSvarService).getNbrOfUnhandledFragaSvarForCareUnits(ids);
                 
+        Mockito.verify(fragaSvarService).getNbrOfUnhandledFragaSvarForCareUnits(listCaptor.capture());
+                
+        List<String> listArgs = listCaptor.getValue();
+        assertEquals(7, listArgs.size());
+        
         assertNotNull(response);
         assertEquals(OK, response.getStatus());
         
         StatsResponse statsResponse = (StatsResponse) response.getEntity();
         assertNotNull(statsResponse);
         
+        assertEquals(2, statsResponse.getTotalNbrOfUnhandledFragaSvarOnSelected());
+        assertEquals(9, statsResponse.getTotalNbrOfUnhandledFragaSvarOnOtherThanSelected());
+    }
+    
+    @Test
+    public void testGetStatisticsWithSelectedUnitVE3() {
+  
+        mockUser.setValdVardenhet(ve3);
+        
+        Mockito.when(fragaSvarService.getNbrOfUnhandledFragaSvarForCareUnits(anyListOf(String.class))).thenReturn(statsMap);
+                
+        Response response = statController.getStatistics();
+        
+        Mockito.verify(webCertUserService).getWebCertUser();
+                
+        Mockito.verify(fragaSvarService).getNbrOfUnhandledFragaSvarForCareUnits(listCaptor.capture());
+                
+        List<String> listArgs = listCaptor.getValue();
+        assertEquals(7, listArgs.size());
+        
+        assertNotNull(response);
+        assertEquals(OK, response.getStatus());
+        
+        StatsResponse statsResponse = (StatsResponse) response.getEntity();
+        assertNotNull(statsResponse);
+        
+        assertEquals(1, statsResponse.getTotalNbrOfUnhandledFragaSvarOnSelected());
+        assertEquals(10, statsResponse.getTotalNbrOfUnhandledFragaSvarOnOtherThanSelected());
+    }
+    
+    @Test
+    public void testGetStatisticsWithSelectedUnitVE4() {
+  
+        mockUser.setValdVardenhet(ve4);
+        
+        Mockito.when(fragaSvarService.getNbrOfUnhandledFragaSvarForCareUnits(anyListOf(String.class))).thenReturn(statsMap);
+                
+        Response response = statController.getStatistics();
+        
+        Mockito.verify(webCertUserService).getWebCertUser();
+                
+        Mockito.verify(fragaSvarService).getNbrOfUnhandledFragaSvarForCareUnits(listCaptor.capture());
+                
+        List<String> listArgs = listCaptor.getValue();
+        assertEquals(7, listArgs.size());
+        
+        assertNotNull(response);
+        assertEquals(OK, response.getStatus());
+        
+        StatsResponse statsResponse = (StatsResponse) response.getEntity();
+        assertNotNull(statsResponse);
+        
+        assertEquals(0, statsResponse.getTotalNbrOfUnhandledFragaSvarOnSelected());
+        assertEquals(11, statsResponse.getTotalNbrOfUnhandledFragaSvarOnOtherThanSelected());
+    }
+    
+    @Test
+    public void testGetStatisticsWithSelectedUnitVE1() {
+  
+        mockUser.setValdVardenhet(ve1);
+        
+        Mockito.when(fragaSvarService.getNbrOfUnhandledFragaSvarForCareUnits(anyListOf(String.class))).thenReturn(statsMap);
+                
+        Response response = statController.getStatistics();
+        
+        Mockito.verify(webCertUserService).getWebCertUser();
+                
+        Mockito.verify(fragaSvarService).getNbrOfUnhandledFragaSvarForCareUnits(listCaptor.capture());
+                
+        List<String> listArgs = listCaptor.getValue();
+        assertEquals(7, listArgs.size());
+        
+        assertNotNull(response);
+        assertEquals(OK, response.getStatus());
+        
+        StatsResponse statsResponse = (StatsResponse) response.getEntity();
+        assertNotNull(statsResponse);
+        
+        assertEquals(8, statsResponse.getTotalNbrOfUnhandledFragaSvarOnSelected());
+        assertEquals(3, statsResponse.getTotalNbrOfUnhandledFragaSvarOnOtherThanSelected());
+        
         StatsResponse refStatsResponse = getReference("StatModuleApiControllerTest/reference.json");
         assertEquals(refStatsResponse.toString(), statsResponse.toString());
-        
-        // VE1 is selected, that gives -2 from total of 16
-        assertEquals("Should be 14 since VE1 is selected", 14, statsResponse.getTotalNbrOfUnhandledFragaSvarOnOtherThanSelected());
-        assertEquals("Should be 2 since VE1 is selected", 2, statsResponse.getTotalNbrOfUnhandledFragaSvarOnSelected());
     }
         
     private StatsResponse getReference(String referenceFilePath) {
@@ -86,37 +205,5 @@ public class StatModuleApiControllerTest {
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
-    }
-        
-    private Map<String, Long> createStatsMap() {
-        
-        Map<String, Long> statsMap = new HashMap<String, Long>(); 
-        
-        statsMap.put("VE1", new Long(2L));
-        statsMap.put("VE1M1", new Long(3L));
-        statsMap.put("VE1M2", new Long(3L));
-        statsMap.put("VE2", new Long(2L));
-        statsMap.put("VE2M1", new Long(2L));
-        statsMap.put("VE2M2", new Long(4L));
-        
-        return statsMap;
-    }
-    
-    private List<Vardgivare> createVardgivare() {
-        
-        Vardgivare vg1 = new Vardgivare("VG1", "Vardgivaren");
-        
-        Vardenhet ve1 = new Vardenhet("VE1", "Vardenhet 1");
-        ve1.getMottagningar().add(new Mottagning("VE1M1", "Mottagning 11"));
-        ve1.getMottagningar().add(new Mottagning("VE1M2", "Mottagning 12"));
-        
-        Vardenhet ve2 = new Vardenhet("VE2", "Vardenhet 2");
-        ve2.getMottagningar().add(new Mottagning("VE2M1", "Mottagning 21"));
-        ve2.getMottagningar().add(new Mottagning("VE2M2", "Mottagning 22"));
-        
-        vg1.getVardenheter().add(ve1);
-        vg1.getVardenheter().add(ve2);
-        
-        return Arrays.asList(vg1);
     }
 }
