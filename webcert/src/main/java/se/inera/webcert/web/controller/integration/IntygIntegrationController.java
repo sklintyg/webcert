28cc31c6d30abb205379135856b85fc620309faa
Niklas Pettersson <niklas.pettersson@r2m.se>
2014-04-10 11:30:56 +0200

    WEBCERT-328: Då Medcert integrationen är borta konsoliderade jag all funktionalitet i en kontroller.

--- webcert/src/main/java/se/inera/webcert/web/controller/integration/IntygIntegrationController.java
+++ webcert/src/main/java/se/inera/webcert/web/controller/integration/IntygIntegrationController.java
@@ -1,10 +1,16 @@
 package se.inera.webcert.web.controller.integration;
 
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.core.UriBuilder;
 import javax.ws.rs.core.UriInfo;
 
 import org.slf4j.Logger;
@@ -13,7 +19,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import se.inera.webcert.service.IntygService;
 import se.inera.webcert.service.dto.IntygContentHolder;
-import se.inera.webcert.web.controller.AbstractIntegrationController;
 
 /**
  * Controller to enable an external user to access certificates directly from a
@@ -23,9 +28,17 @@ import se.inera.webcert.web.controller.AbstractIntegrationController;
  *
  */
 @Path("/intyg")
-public class IntygIntegrationController extends AbstractIntegrationController {
+public class IntygIntegrationController {
+
+    private static final String PARAM_CERT_TYPE = "certType";
+
+    private static final String PARAM_CERT_ID = "certId";
 
     private static Logger LOG = LoggerFactory.getLogger(IntygIntegrationController.class);
+
+    private String urlBaseTemplate;
+
+    private String urlFragmentTemplate;
 
     @Autowired
     private IntygService intygService;
@@ -41,14 +54,35 @@ public class IntygIntegrationController extends AbstractIntegrationController {
     @GET
     @Path("/view/{intygId}")
     public Response redirectToIntyg(@Context UriInfo uriInfo, @PathParam("intygId") String intygId) {
-
+    
         IntygContentHolder intygData = intygService.fetchExternalIntygData(intygId);
-
+    
         String intygType = intygData.getMetaData().getType();
-
+    
         LOG.debug("Redirecting to view intyg {} of type {}", intygId, intygType);
-
+    
         return buildRedirectResponse(uriInfo, intygType, intygId);
+    }
+
+    private Response buildRedirectResponse(UriInfo uriInfo, String certificateType, String certificateId) {
+        
+        UriBuilder uriBuilder = uriInfo.getBaseUriBuilder();
+        
+        Map<String, Object> urlParams = new HashMap<String, Object>();
+        urlParams.put(PARAM_CERT_TYPE, certificateType);
+        urlParams.put(PARAM_CERT_ID, certificateId);
+        
+        URI location = uriBuilder.replacePath(urlBaseTemplate).fragment(urlFragmentTemplate).buildFromMap(urlParams);
+
+        return Response.status(Status.MOVED_PERMANENTLY).location(location).build();
+    }
+        
+    public void setUrlBaseTemplate(String urlBaseTemplate) {
+        this.urlBaseTemplate = urlBaseTemplate;
+    }
+
+    public void setUrlFragmentTemplate(String urlFragmentTemplate) {
+        this.urlFragmentTemplate = urlFragmentTemplate;
     }
 
 }
