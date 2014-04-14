e28343f5c3338d41425224e5c2d51989de86e4df
Benny Samuelsson <benny.samuelsson@usify.se>
2014-04-14 18:41:07 +0200

    Städa bort gamla servicar

--- webcert/src/main/webapp/js/services/QuestionAnswer.js
+++ webcert/src/main/webapp/js/services/QuestionAnswer.js
@@ -21,38 +21,6 @@ define([
         }
 
         /*
-         * Load questions and answers data for search
-         */
-/*        function _getQAByQuery(qp, onSuccess, onError) {
-            $log.debug('_getQAByQuery');
-            var restPath = '/api/fragasvar/query';
-            $http.put(restPath, qp).success(function (data) {
-                $log.debug('_getQAByQuery got data:' + data);
-                onSuccess(data);
-            }).error(function (data, status) {
-                $log.error('_getQAByQuery error ' + status);
-                // Let calling code handle the error of no data response
-                onError(data);
-            });
-        }
-*/
-        /*
-         * Load more questions and answers data
-         */
-/*        function _getQAByQueryFetchMore(qp, onSuccess, onError) {
-            $log.debug('_getQAByQueryFetchMore');
-            var restPath = '/api/fragasvar/query/paging';
-            $http.put(restPath, qp).success(function (data) {
-                $log.debug('_getQAByQueryFetchMore got data:' + data);
-                onSuccess(data);
-            }).error(function (data, status) {
-                $log.error('_getQAByQueryFetchMore error ' + status);
-                // Let calling code handle the error of no data response
-                onError(data);
-            });
-        }
-*/
-        /*
          * Get list of lakare for enhet
          */
         function _getQALakareList(enhetsId, onSuccess, onError) {
@@ -71,8 +39,6 @@ define([
         // Return public API for the service
         return {
             getQA: _getQA,
-/*            getQAByQuery: _getQAByQuery,
-            getQAByQueryFetchMore: _getQAByQueryFetchMore,*/
             getQALakareList: _getQALakareList
         };
     }];
