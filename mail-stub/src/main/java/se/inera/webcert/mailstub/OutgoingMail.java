61894add60f114f43b6d5e7d6dd6269c56d1ff77
Bjorn Beskow <bb@callistaenterprise.se>
2014-04-28 17:40:37 +0200

    WEBCERT-604: Notifierings-mail skickas inte i test- och qa-profil
    (cherry picked from commit e5387ed0cef5186dafa7ffd1905087945cd8f5be)
    Signed-off-by: Bjorn Beskow <bb@callistaenterprise.se>

--- mail-stub/src/main/java/se/inera/webcert/mailstub/OutgoingMail.java
+++ mail-stub/src/main/java/se/inera/webcert/mailstub/OutgoingMail.java
@@ -37,4 +37,43 @@ public class OutgoingMail {
     public String getBody() {
         return body;
     }
+
+    // Eclipse-generated hashCode implementation, based on recipients, subject and body
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((body == null) ? 0 : body.hashCode());
+        result = prime * result + ((recipients == null) ? 0 : recipients.hashCode());
+        result = prime * result + ((subject == null) ? 0 : subject.hashCode());
+        return result;
+    }
+
+    // Eclipse-generated equals implementation, based on recipients, subject and body
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        OutgoingMail other = (OutgoingMail) obj;
+        if (body == null) {
+            if (other.body != null)
+                return false;
+        } else if (!body.equals(other.body))
+            return false;
+        if (recipients == null) {
+            if (other.recipients != null)
+                return false;
+        } else if (!recipients.equals(other.recipients))
+            return false;
+        if (subject == null) {
+            if (other.subject != null)
+                return false;
+        } else if (!subject.equals(other.subject))
+            return false;
+        return true;
+    }
 }
