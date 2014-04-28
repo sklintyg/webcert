e5387ed0cef5186dafa7ffd1905087945cd8f5be
Bjorn Beskow <bb@callistaenterprise.se>
2014-04-28 17:40:37 +0200

    WEBCERT-604: Notifierings-mail skickas inte i test- och qa-profil

--- mail-stub/src/main/java/se/inera/webcert/mailstub/JavaMailSenderAroundAdvice.java
+++ mail-stub/src/main/java/se/inera/webcert/mailstub/JavaMailSenderAroundAdvice.java
@@ -2,6 +2,7 @@ package se.inera.webcert.mailstub;
 
 import javax.mail.internet.MimeMessage;
 
+import org.apache.cxf.common.util.StringUtils;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
@@ -14,19 +15,32 @@ import org.springframework.beans.factory.annotation.Autowired;
 public class JavaMailSenderAroundAdvice {
 
     @Autowired
-    private MailStore mailStore;
+    MailStore mailStore;
+
+    private String mailHost;
+
+    public String getMailHost() {
+        return mailHost;
+    }
+
+    public void setMailHost(String mailHost) {
+        this.mailHost = mailHost;
+    }
 
     /**
      * Intercepts and mail sending calls and stores the mime message in the MailStore.
      */
     @Around("execution(* org.springframework.mail.javamail.JavaMailSender+.send(..))")
     public Object interceptMailSending(ProceedingJoinPoint pjp) throws Throwable {
-        for (Object argument : pjp.getArgs()) {
-            if (argument instanceof MimeMessage) {
-                mailStore.getMails().add(new OutgoingMail((MimeMessage) argument));
+        if (StringUtils.isEmpty(mailHost)) {
+            for (Object argument : pjp.getArgs()) {
+                if (argument instanceof MimeMessage) {
+                    mailStore.getMails().add(new OutgoingMail((MimeMessage) argument));
+                }
             }
+            return null;
+        } else {
+            return pjp.proceed();
         }
-
-        return null;
     }
 }
