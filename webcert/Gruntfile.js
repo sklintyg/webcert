bb365a9240588c392929fd4ebc4200033108143d
Joakim Kemeny <joakim@kemeny.se>
2014-05-02 11:15:15 +0200

    Förenklade byggfilerna och anpassade dem så att de matchar dokumentationen.

--- webcert/Gruntfile.js
+++ webcert/Gruntfile.js
@@ -1,54 +1,32 @@
 /* global module */
 module.exports = function(grunt) {
-	'use strict';
+    'use strict';
 
-	grunt.loadNpmTasks('grunt-contrib-csslint');
-	grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-csslint');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
 
-	grunt.initConfig({
+    grunt.initConfig({
 
-		csslint : {
-			dev : {
-				options : {
-					csslintrc : '../src/main/resources/.csslintrc',
-					force : true
-				},
-				src : [ 'src/main/webapp/**/*.css' ]
-			},
-			build : {
-				options : {
-					csslintrc : '../src/main/resources/.csslintrc',
-					force : true,
-					formatters : [ {
-						id : 'checkstyle-xml',
-						dest : 'target/checkstyle-csslint-result.xml'
-					} ],
-					absoluteFilePathsForFormatters : true
-				},
-				src : [ 'src/main/webapp/**/*.css' ]
-			}
-		},
+        csslint: {
+            dev: {
+                options: {
+                    csslintrc: '../src/main/resources/.csslintrc',
+                    force: true
+                },
+                src: [ 'src/main/webapp/**/*.css' ]
+            }
+        },
 
-		jshint : {
-			dev : {
-				options : {
-					jshintrc : '../src/main/resources/.jshintrc',
-					force : true
-				},
-				src : [ 'Gruntfile.js', 'src/main/webapp/js/**/*.js', 'src/test/js/**/*.js' ]
-			},
-			build : {
-				options : {
-					jshintrc : '../src/main/resources/.jshintrc',
-					force : true,
-					reporter : 'checkstyle',
-					reporterOutput : 'target/checkstyle-jshint-result.xml'
-				},
-				src : [ 'Gruntfile.js', 'src/main/webapp/js/**/*.js', 'src/test/js/**/*.js' ]
-			}
-		}
-	});
+        jshint: {
+            dev: {
+                options: {
+                    jshintrc: '../src/main/resources/.jshintrc',
+                    force: true
+                },
+                src: [ 'Gruntfile.js', 'src/main/webapp/js/**/*.js', 'src/test/**/*.js' ]
+            }
+        }
+    });
 
-	grunt.registerTask('dev', [ 'csslint:dev', 'jshint:dev' ]);
-	grunt.registerTask('default', [ 'csslint:build', 'jshint:build' ]);
+    grunt.registerTask('default', [ 'jshint', 'csslint' ]);
 };
