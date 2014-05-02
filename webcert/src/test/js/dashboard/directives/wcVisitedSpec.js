c451d3b0a47558e2862afdbce1d22c79475b9e45
Joakim Kemeny <joakim@kemeny.se>
2014-05-02 09:05:05 +0200

    Uppgraderade alla Karma-dependencies för att se om det kan lösa problemen med att bygget inte fungerar ibland.

--- webcert/src/test/js/dashboard/directives/wcVisitedSpec.js
+++ webcert/src/test/js/dashboard/directives/wcVisitedSpec.js
@@ -1,30 +1,30 @@
-define([ 'angular', 'angularMocks', 'directives' ], function(angular, mocks) {
+define(['angular', 'angularMocks', 'angularScenario', 'directives'], function(angular, mocks) {
     'use strict';
 
-    describe('wcVisited', function () {
+    describe('wcVisited', function() {
         beforeEach(mocks.module('wc.dashboard.directives'));
 
         var $scope;
         var element;
 
         // Create a form to test the directive on.
-        beforeEach(mocks.inject(function ($compile, $rootScope) {
+        beforeEach(mocks.inject(function($compile, $rootScope) {
             $scope = $rootScope;
-            $scope.model = { test : 'test' };
+            $scope.model = { test: 'test' };
 
-            element = angular.element('<form name="form"><input wc-visited type="text" ng-model="model.test" name="test"></form>');
+            element =
+                angular.element('<form name="form"><input wc-visited type="text" ng-model="model.test" name="test"></form>');
             element = $compile(element)($scope);
             $scope.$digest();
         }));
 
         // Pass
 
-        it('should pass if element is focused and marked visited', function () {
+        it('should pass if element is focused and marked visited', function() {
             element = element.find('input');
-            element.triggerHandler('blur');
+            browserTrigger(element, 'blur'); // jshint ignore:line
             expect(element.hasClass('wc-visited')).toBeTruthy();
             expect($scope.form.test.$visited).toBeTruthy();
         });
-
     });
 });
