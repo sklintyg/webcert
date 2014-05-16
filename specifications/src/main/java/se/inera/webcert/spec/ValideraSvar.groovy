0c468450f1c0a9776d9f9f25124e6dc7644daa0d
Bjorn Beskow <bb@callistaenterprise.se>
2014-05-16 17:04:26 +0200

    Lagt till validerings-tester för fråga/svar.
    (cherry picked from commit 459f8ec703ff3004abfe8373fe87ece63fc919c3)
    Signed-off-by: Bjorn Beskow <bb@callistaenterprise.se>

---
+++ specifications/src/main/java/se/inera/webcert/spec/ValideraSvar.groovy
@@ -0,0 +1,51 @@
+package se.inera.webcert.spec
+
+import javax.xml.bind.JAXBContext
+import javax.xml.bind.Unmarshaller
+import javax.xml.transform.stream.StreamSource
+
+import se.inera.certificate.spec.util.FitnesseHelper
+import se.inera.webcert.receivemedicalcertificateanswer.v1.rivtabp20.ReceiveMedicalCertificateAnswerResponderInterface
+import se.inera.webcert.receivemedicalcertificateanswerresponder.v1.ReceiveMedicalCertificateAnswerResponseType
+import se.inera.webcert.receivemedicalcertificateanswerresponder.v1.ReceiveMedicalCertificateAnswerType
+import se.inera.webcert.spec.util.WsClientFixture
+
+/**
+ *
+ * @author andreaskaltenbach
+ */
+class ValideraSvar extends WsClientFixture {
+
+    ReceiveMedicalCertificateAnswerResponderInterface receiveMedicalCertificateAnswerResponder
+
+    public ValideraSvar() {
+        this(WsClientFixture.LOGICAL_ADDRESS)
+    }
+    
+    public ValideraSvar(String logiskAddress) {
+        super(logiskAddress)
+        String url = baseUrl + "receive-answer/v1.0"
+        receiveMedicalCertificateAnswerResponder = createClient(ReceiveMedicalCertificateAnswerResponderInterface.class, url)
+    }
+
+    String filnamn
+    String vardReferens
+    
+    ReceiveMedicalCertificateAnswerResponseType response
+
+    public void execute() {
+        // read request template from file
+        JAXBContext jaxbContext = JAXBContext.newInstance(ReceiveMedicalCertificateAnswerType.class);
+        Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+        ReceiveMedicalCertificateAnswerType request = unmarshaller.unmarshal(new StreamSource(new FileInputStream (FitnesseHelper.getFile(filnamn))),
+                                                                        ReceiveMedicalCertificateAnswerType.class).getValue()
+
+        if (vardReferens) request.answer.vardReferensId = vardReferens
+        
+        response = receiveMedicalCertificateAnswerResponder.receiveMedicalCertificateAnswer(logicalAddress, request);
+    }
+
+    public String resultat() {
+        resultAsString(response)
+    }
+}
