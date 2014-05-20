6f2a3eca87621a4563d234219deef3641f70ad88
Niklas Pettersson <niklas.pettersson@r2m.se>
2014-05-20 15:23:07 +0200

    WEBCERT-650: Snyggat till loggningen samt info till användaren.

--- mc2wc/mc2wc-batch/src/main/java/se/inera/certificate/mc2wc/Main.java
+++ mc2wc/mc2wc-batch/src/main/java/se/inera/certificate/mc2wc/Main.java
@@ -1,10 +1,10 @@
 package se.inera.certificate.mc2wc;
 
-import java.io.Console;
-
 import joptsimple.OptionParser;
 import joptsimple.OptionSet;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.JOptCommandLinePropertySource;
 import org.springframework.core.env.PropertySource;
@@ -12,38 +12,45 @@ import org.springframework.core.env.PropertySource;
 import se.inera.certificate.mc2wc.batch.MigrationJobExecutor;
 
 public class Main {
-
+	
+	public static final String CONSOLE_LOGGER = "mc2wc.console";
+		
 	private static final String CONTEXT_LOCATION = "/application-context.xml";
 	private static final String CONFIG_PARAM = "configFile";
+	private static final String LOGGER_PARAM = "logFile";
+	
+	private static Logger log = LoggerFactory.getLogger(CONSOLE_LOGGER);
 	
 	@Autowired
-	private MigrationJobExecutor migrationJobExecutor; 
-
+	private MigrationJobExecutor migrationJobExecutor;
+	
 	public static void main(String[] args) throws Exception {
-		Console console = System.console();
-		
-		//console.printf("%s", "Starting application...");
-		
+		log.info("Starting application...");
+		Main main = new Main();
+		main.configure(args);
+		main.start();
+	}
+
+	public void configure(String[] args) throws Exception {
+		log.info("Configuring...");
 		OptionParser parser = new OptionParser();
 		parser.accepts(CONFIG_PARAM).withRequiredArg();
 		OptionSet options = parser.parse(args);
 		PropertySource ps = new JOptCommandLinePropertySource(options);
-
-		Main main = new Main();
-		
-//		if (options.has(CONFIG_PARAM)) {
-//			//console.printf("Config file: %s", options.valueOf(CONFIG_PARAM));
-//		}
 		
 		ApplicationContextLoader appCxtLoader = new ApplicationContextLoader();
-		appCxtLoader.load(main, ps, CONTEXT_LOCATION);
-		
-		start(main, console);
+		appCxtLoader.load(this, ps, CONTEXT_LOCATION);
 	}
-
-	private static void start(Main main, Console console) throws Exception {
-		//console.printf("%s", "Starting migration job...");
-		main.migrationJobExecutor.startMigration();
+	
+	private void start() throws Exception {
+		log.info("Starting migration!");
+		int status = migrationJobExecutor.startMigration();
+		
+		while (status > 0) {
+			status = migrationJobExecutor.checkMigrationJob();
+			Thread.sleep(500);
+		}
+		
 	}
 
 }
--- mc2wc/mc2wc-batch/src/main/java/se/inera/certificate/mc2wc/batch/MigrationJobExecutor.java
+++ mc2wc/mc2wc-batch/src/main/java/se/inera/certificate/mc2wc/batch/MigrationJobExecutor.java
@@ -4,25 +4,29 @@ import java.util.Set;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.batch.core.BatchStatus;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobParametersBuilder;
-import org.springframework.batch.core.JobParametersInvalidException;
 import org.springframework.batch.core.explore.JobExplorer;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.batch.core.launch.JobOperator;
-import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
-import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
-import org.springframework.batch.core.repository.JobRestartException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
+
+import se.inera.certificate.mc2wc.Main;
 
 @Component
 public class MigrationJobExecutor {
 
 	private static final String MIGRATION_JOB = "migrationJob";
-	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	private static final int RUNNING = 1;
+	private static final int FAILED = -1;
+	private static final int FINISHED = 0;
+	
+	private static Logger logger = LoggerFactory.getLogger(Main.CONSOLE_LOGGER);
 
 	@Autowired
 	private JobLauncher jobLauncher;
@@ -37,25 +41,38 @@ public class MigrationJobExecutor {
 	@Autowired
 	private JobOperator jobOperator;
 
-	public void startMigration() throws Exception {
-		System.out.println("START!");
+	public int startMigration() throws Exception {
+		logger.info("Running job '{}'", MIGRATION_JOB);
 
 		Set<JobExecution> executions = jobExplorer
 				.findRunningJobExecutions(MIGRATION_JOB);
 
 		if (!executions.isEmpty()) {
-			System.err.println("Already running!");
-			return;
+			logger.error("Job '{}' is already running!", MIGRATION_JOB);
+			return FAILED;
 		}
 		JobParametersBuilder builder = new JobParametersBuilder();
 		builder.addLong("millis", System.currentTimeMillis(), true);
 
 		jobLauncher.run(migrationJob, builder.toJobParameters());
+		
+		return RUNNING;
 	}
 
-	public void checkMigrationJob() {
-		System.out.println("CHECK!");
+	public int checkMigrationJob() {
+		
+		Set<JobExecution> executions = jobExplorer.findRunningJobExecutions(MIGRATION_JOB);
 
+        if (executions.isEmpty()) {
+        	logger.info("Job '{}' was not found!", MIGRATION_JOB);
+            return FINISHED;
+        }
+
+        JobExecution execution = executions.iterator().next();
+		
+		logger.info("Job '{}': {}", MIGRATION_JOB, execution.getStatus());
+		
+		return RUNNING;
 	}
 
 }
