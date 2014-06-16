a47da8c866dd48b043fd59dc632a01bb72efd914
Niklas Pettersson <niklas.pettersson@r2m.se>
2014-06-16 13:32:32 +0200

    Ändrat villkoret för räknandet av frågor i Medcert att inte ta med sådana som bara är skapade.

--- mc2wc/mc2wc-batch/src/main/java/se/inera/certificate/mc2wc/medcert/jpa/MedcertDAO.java
+++ mc2wc/mc2wc-batch/src/main/java/se/inera/certificate/mc2wc/medcert/jpa/MedcertDAO.java
@@ -4,7 +4,13 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 
+import se.inera.certificate.mc2wc.medcert.jpa.model.State;
+
 public class MedcertDAO {
+    
+    private static final String COUNT_QUESTIONS = "select count(id) from Question " +
+    		"where subject is not null and text is not null " +
+    		"and state != :notLikeThisState";
 
     @PersistenceContext(unitName = "jpa.migration.medcert")
     private EntityManager entityManager;
@@ -20,7 +26,8 @@ public class MedcertDAO {
     }
 
     public Long countQuestions() {
-        Query questionQuery = entityManager.createQuery("select count(id) from Question where subject is not null and text is not null");
+        Query questionQuery = entityManager.createQuery(COUNT_QUESTIONS);
+        questionQuery.setParameter("notLikeThisState", State.CREATED);
         return (Long) questionQuery.getSingleResult();
     }
 
