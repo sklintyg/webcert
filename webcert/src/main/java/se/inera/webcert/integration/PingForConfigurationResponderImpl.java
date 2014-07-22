860e8b05b6209fda69d65655bb1ce1227565cdf9
Pierre Sandboge <pierre.sandboge@standin.se>
2014-07-22 15:34:55 +0200

    WEBCERT-659 Konfigurerade och gjorde klart ping-for-configuration utan bjällror och visselpipor.

--- webcert/src/main/java/se/inera/webcert/integration/PingForConfigurationResponderImpl.java
+++ webcert/src/main/java/se/inera/webcert/integration/PingForConfigurationResponderImpl.java
@@ -1,11 +1,15 @@
 package se.inera.webcert.integration;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.context.support.SpringBeanAutowiringSupport;
 import se.riv.itintegration.monitoring.rivtabp21.v1.PingForConfigurationResponderInterface;
+import se.riv.itintegration.monitoring.v1.ConfigurationType;
 import se.riv.itintegration.monitoring.v1.PingForConfigurationResponseType;
 import se.riv.itintegration.monitoring.v1.PingForConfigurationType;
 
+import javax.annotation.PostConstruct;
 import javax.jws.WebParam;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -14,16 +18,38 @@ import java.util.Date;
  * @author johannesc
  */
 public class PingForConfigurationResponderImpl implements PingForConfigurationResponderInterface {
+    private static final Logger LOG = LoggerFactory.getLogger(PingForConfigurationResponderImpl.class);
 
-    @Autowired
     @Value("${project.version}")
     private String projectVersion;
+
+    @Value("${buildNumber}")
+    private String buildNumberString;
+
+    @Value("${buildTime}")
+    private String buildTimeString;
 
     @Override
     public PingForConfigurationResponseType pingForConfiguration(@WebParam(partName = "LogicalAddress", name = "LogicalAddress", targetNamespace = "urn:riv:itintegration:registry:1", header = true) String logicalAddress, @WebParam(partName = "parameters", name = "PingForConfiguration", targetNamespace = "urn:riv:itintegration:monitoring:PingForConfigurationResponder:1") PingForConfigurationType parameters) {
         PingForConfigurationResponseType response = new PingForConfigurationResponseType();
         response.setPingDateTime(new SimpleDateFormat("yyyyMMddHHmmss").format(new Date()));
+        LOG.info("Version String: " + projectVersion);
         response.setVersion(projectVersion);
+
+        addConfiguration(response, "buildNumber", buildNumberString);
+        addConfiguration(response, "buildTime", buildTimeString);
         return response;
+    }
+
+    private void addConfiguration(PingForConfigurationResponseType response, String name, String value) {
+        ConfigurationType conf = new ConfigurationType();
+        conf.setName(name);
+        conf.setValue(value);
+        response.getConfiguration().add(conf);
+    }
+
+    @PostConstruct
+    public void init() {
+        SpringBeanAutowiringSupport.processInjectionBasedOnCurrentContext(this);
     }
 }
