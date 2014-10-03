74329abf50f7c16dd3d72053d6e53d9587ab8d41
Benny Samuelsson <benny.samuelsson@usify.se>
2014-10-03 17:02:31 +0200

    WEBCERT-1047 Laddningsanimation när vi väntar på pu-tjänsten

--- webcert/src/main/webapp/js/controllers/ChoosePatientCtrl.js
+++ webcert/src/main/webapp/js/controllers/ChoosePatientCtrl.js
@@ -1,23 +1,32 @@
 angular.module('webcert').controller('webcert.ChoosePatientCtrl',
-    [ '$location', '$scope', 'webcert.CreateCertificateDraft',
-        function($location, $scope, CreateCertificateDraft) {
+    [ '$location', '$scope', 'webcert.CreateCertificateDraft', '$timeout',
+        function($location, $scope, CreateCertificateDraft, $timeout) {
             'use strict';
+
+            $scope.widgetState = {
+                waiting: false
+            };
 
             $scope.personnummer = CreateCertificateDraft.personnummer;
 
             $scope.lookupPatient = function() {
 
                 var onSuccess = function() {
+                    $scope.widgetState.waiting = false;
                     $location.path('/create/choose-cert-type/index');
                 };
 
                 var onNotFound = function() {
+                    $scope.widgetState.waiting = false;
                     $location.path('/create/edit-patient-name/notFound');
                 };
 
                 var onError = function() {
+                    $scope.widgetState.waiting = false;
                     $location.path('/create/edit-patient-name/errorOccured');
                 };
+
+                $scope.widgetState.waiting = true;
 
                 CreateCertificateDraft.getNameAndAddress($scope.personnummer,
                     onSuccess, onNotFound, onError);
