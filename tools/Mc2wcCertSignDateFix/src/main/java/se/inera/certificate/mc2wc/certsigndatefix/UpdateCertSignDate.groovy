419a7cc8017a8ee810bf6201da7b2772d5ce2d7a
Niklas Pettersson <niklas.pettersson@r2m.se>
2014-10-10 13:05:47 +0200

    WEBCERT-1111: Lade till kontroll av null i updaterings SQL

--- tools/Mc2wcCertSignDateFix/src/main/java/se/inera/certificate/mc2wc/certsigndatefix/UpdateCertSignDate.groovy
+++ tools/Mc2wcCertSignDateFix/src/main/java/se/inera/certificate/mc2wc/certsigndatefix/UpdateCertSignDate.groovy
@@ -10,35 +10,36 @@ class UpdateCertSignDate {
         long start = System.currentTimeMillis()
 
         def props = new Properties()
-        new File("dataSource.properties").withInputStream {
-          stream -> props.load(stream)
+        new File("dataSource.properties").withInputStream { stream ->
+            props.load(stream)
         }
-        
+
         def config = new ConfigSlurper().parse(props)
-        
+
         Sql webcertSQL = Sql.newInstance(config.dataSource.wc.url, config.dataSource.wc.username, config.dataSource.wc.password, config.dataSource.wc.driver)
-        
+
         Sql intygstjanstSQL = Sql.newInstance(config.dataSource.intyg.url, config.dataSource.intyg.username, config.dataSource.intyg.password, config.dataSource.intyg.driver)
-        
+
         int count = 0
-                
+
         webcertSQL.eachRow("SELECT DISTINCT INTYGS_ID FROM FRAGASVAR WHERE SIGNERINGS_DATUM IS NULL") { row ->
             def intygsId = row.INTYGS_ID
-       
+
             def intyg = intygstjanstSQL.firstRow( "SELECT SIGNED_DATE FROM CERTIFICATE WHERE ID = :id" , [id : intygsId])
-       
-            webcertSQL.executeUpdate("UPDATE FRAGASVAR SET SIGNERINGS_DATUM = :signDate WHERE INTYGS_ID = :intygsId", [signDate : intyg.SIGNED_DATE, intygsId : intygsId])
-            
+
+            webcertSQL.executeUpdate("UPDATE FRAGASVAR SET SIGNERINGS_DATUM = :signDate WHERE INTYGS_ID = :intygsId AND SIGNERINGS_DATUM IS NOT NULL",
+                    [signDate : intyg.SIGNED_DATE, intygsId : intygsId])
+
             if (++count % 100 == 0) {
                 println "+ ${count} fragasvar uppdaterade"
             }
-        }      
-        
+        }
+
         webcertSQL.close()
         intygstjanstSQL.close();
-        
+
         long end = System.currentTimeMillis()
-        
+
         println "Klart! ${count} fragasvar uppdaterade med signeringsdatum på ${(int)((end-start) / 1000)} sekunder"
     }
 }
