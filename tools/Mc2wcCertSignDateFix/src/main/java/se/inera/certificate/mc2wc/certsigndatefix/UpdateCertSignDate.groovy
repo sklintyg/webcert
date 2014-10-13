6a316da19dbd0ca2706179729d7a8ffc13f65437
Niklas Pettersson <niklas.pettersson@r2m.se>
2014-10-13 09:58:30 +0200

    WEBCERT-1111: Tagit bort kollen mot IT då allt data finns i fil.

--- tools/Mc2wcCertSignDateFix/src/main/java/se/inera/certificate/mc2wc/certsigndatefix/UpdateCertSignDate.groovy
+++ tools/Mc2wcCertSignDateFix/src/main/java/se/inera/certificate/mc2wc/certsigndatefix/UpdateCertSignDate.groovy
@@ -19,16 +19,16 @@ class UpdateCertSignDate {
         }
 
         def config = new ConfigSlurper().parse(props)
-
-        def medcertDataMap = [:]
+        
         List<String[]> medcertData
         new File(config.file.medcert).withInputStream { stream ->
             medcertData = new CSVReader(
-                new InputStreamReader(stream)).readAll()
+                    new InputStreamReader(stream)).readAll()
         }
 
         DateTimeFormatter formatter = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss.SSS")
-
+        
+        def medcertDataMap = [:]
         medcertData.each { row ->
             LocalDateTime certSignDate = formatter.parseLocalDateTime(row[1])
             medcertDataMap.put(row[0], certSignDate)
@@ -38,31 +38,15 @@ class UpdateCertSignDate {
 
         Sql webcertSQL = Sql.newInstance(config.dataSource.wc.url, config.dataSource.wc.username, config.dataSource.wc.password, config.dataSource.wc.driver)
 
-        Sql intygstjanstSQL = Sql.newInstance(config.dataSource.intyg.url, config.dataSource.intyg.username, config.dataSource.intyg.password, config.dataSource.intyg.driver)
-
         int updatedCount = 0
-        int fetchedFromIT = 0
-        int fetchedFromFile = 0
-
+        
         webcertSQL.eachRow("SELECT DISTINCT INTYGS_ID FROM FRAGASVAR WHERE SIGNERINGS_DATUM IS NULL") { row ->
             def intygsId = row.INTYGS_ID
-            java.sql.Timestamp sqlSignDate
 
-            def intyg = intygstjanstSQL.firstRow("SELECT SIGNED_DATE FROM CERTIFICATE WHERE ID = :id" , [id : intygsId])
+            LocalDateTime medcertSignDate = medcertDataMap.get(intygsId)
 
-            if (intyg) {
-                sqlSignDate = intyg.SIGNED_DATE
-                fetchedFromIT++
-            } else {
-                LocalDateTime medcertSignDate = medcertDataMap.get(intygsId)
-
-                if (medcertSignDate) {
-                    sqlSignDate = new java.sql.Timestamp(medcertSignDate.toDate().getTime())
-                    fetchedFromFile++
-                }
-            }
-
-            if (sqlSignDate) {
+            if (medcertSignDate) {
+                java.sql.Timestamp sqlSignDate = new java.sql.Timestamp(medcertSignDate.toDate().getTime())
                 int updates = webcertSQL.executeUpdate("UPDATE FRAGASVAR SET SIGNERINGS_DATUM = :signDate WHERE INTYGS_ID = :intygsId",
                         [signDate : sqlSignDate, intygsId : intygsId])
                 updatedCount = updatedCount + updates
@@ -76,11 +60,9 @@ class UpdateCertSignDate {
         }
 
         webcertSQL.close()
-        intygstjanstSQL.close();
 
         long end = System.currentTimeMillis()
 
         println "Klart! ${updatedCount} rader i fragasvar uppdaterade med signeringsdatum på ${(int)((end-start) / 1000)} sekunder"
-        println "${fetchedFromIT} hämtade från Intyg, ${fetchedFromFile} hämtade från textfil"
     }
 }
