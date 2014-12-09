24d4be8488f7464d6a968eaea4bedfca091ae044
stephenwhite <stephen.white@mac.com>
2014-12-09 14:49:54 +0100

    [webcert-928] - checkHandled event on location change

--- web/src/main/webapp/js/controllers/ViewCertCtrl.js
+++ web/src/main/webapp/js/controllers/ViewCertCtrl.js
@@ -2,8 +2,8 @@
  * Controller for logic related to viewing signed certs
  */
 angular.module('webcert').controller('webcert.ViewCertCtrl',
-    [ '$rootScope', '$routeParams', '$scope', '$window', 'common.dialogService', 'webcert.ManageCertificate',
-        function($rootScope, $routeParams, $scope, $window, dialogService, ManageCertificate) {
+    [ '$rootScope', '$routeParams', '$scope', '$window', '$location', '$q', 'common.dialogService', 'webcert.ManageCertificate',
+        function($rootScope, $routeParams, $scope, $window, $location, $q, dialogService, ManageCertificate) {
             'use strict';
 
             // Check if the user used the special qa-link to get here.
@@ -32,8 +32,57 @@ angular.module('webcert').controller('webcert.ViewCertCtrl',
                 fragaSvarAvailable: false
             };
 
+            var unbindCheckHandledEvent = $rootScope.$on('$locationChangeStart', function($event, newUrl, currentUrl){
+                debugger;
+                if(newUrl !== currentUrl){  // if we're changing url
+                    $event.preventDefault();
+                    var deferred = $q.defer();
+                    $scope.$broadcast('hasUnhandledQasEvent', deferred);
+                    deferred.promise.then(function(hasUnhandledQas) {
+                        if (hasUnhandledQas) {
+                            var modal = dialogService.showDialog($scope, {
+                                dialogId: 'qa-check-hanterad-dialog',
+                                titleId: 'label.qacheckhanterad.title',
+                                bodyTextId: 'label.qacheckhanterad.body',
+                                templateUrl: '/views/partials/qa-check-hanterad-dialog.html',
+                                button1click: function() {
+                                    $scope.$broadcast('markAllAsHandledEvent');
+                                    modal.close('hantera');
+                                    // unbind the location change listener
+                                    unbindCheckHandledEvent();
+                                    $window.location.href = newUrl;
+                                },
+                                button2click: function() {
+                                    modal.close('ejhantera');
+                                    // unbind the location change listener
+                                    unbindCheckHandledEvent();
+                                    $window.location.href = newUrl;
+                                },
+                                button3click: function() {
+                                    // bara stänga modal fönstret
+                                    modal.close('button3 close');
+                                },
+                                button1text: 'label.qacheckhanterad.hanterad',
+                                button1id: 'button1checkhanterad-dialog',
+                                button2text: 'label.qacheckhanterad.ejhanterad',
+                                button2id: 'button1checkhanterad-dialog',
+                                button3text: 'label.qacheckhanterad.tillbaka',
+                                button3id: 'button1checkhanterad-dialog',
+                                autoClose: true
+                            });
+                        } else {
+                            // unbind the location change listener
+                            unbindCheckHandledEvent();
+                            $window.location.href = newUrl;
+                        }
+                    });
+                }
+            });
+            $scope.$on('$destroy', unbindCheckHandledEvent);
+
             ManageCertificate.getCertType($routeParams.certificateType, function(intygType) {
                 $scope.widgetState.fragaSvarAvailable = intygType.fragaSvarAvailable;
                 $scope.widgetState.printStatus = intygType.printStatus;
             });
+
         }]);
