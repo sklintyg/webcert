bf675f0107e784d8c8be975974698f0580f2091c
stephenwhite <stephen.white@mac.com>
2014-12-10 14:31:57 +0100

    [webcert-928] - test now includes all button click cases : handled, unhandled, back

--- web/src/test/js/dashboard/controllers/ViewCertCtrlSpec.js
+++ web/src/test/js/dashboard/controllers/ViewCertCtrlSpec.js
@@ -10,12 +10,19 @@ describe('ViewCertCtrl', function() {
     var $rootScope;
     var $location;
     var $window;
+    var modalMock;
+    var newUrl = '/url/a';
+    var currentUrl = '/url/b';
 
     // Load the webcert module and mock away everything that is not necessary.
     beforeEach(angular.mock.module('webcert', function($provide) {
         dialogService = jasmine.createSpyObj('common.dialogService', [ 'showDialog' ]);
+        modalMock = jasmine.createSpyObj('modal', [ 'close' ]);
+        dialogService.showDialog.andCallFake(function(){
+            return modalMock;
+        });
         $provide.value('common.dialogService', dialogService);
-        $provide.value('$window', {location:{href:'dummy'}});
+        $provide.value('$window', {location:{href:currentUrl}});
         manageCertificateSpy = jasmine.createSpyObj('webcert.ManageCertificate', [ 'getCertType' ]);
         $provide.value('webcert.ManageCertificate', manageCertificateSpy);
     }));
@@ -48,8 +55,6 @@ describe('ViewCertCtrl', function() {
             spyOn($q, 'defer').andReturn(deferred);
 
             // kick off the window change event
-            var newUrl = '/url/a';
-            var currentUrl = '/url/b';
             $rootScope.$broadcast('$locationChangeStart', newUrl, currentUrl);
 
             expect($scope.$broadcast).toHaveBeenCalledWith('hasUnhandledQasEvent', deferred);
@@ -67,17 +72,21 @@ describe('ViewCertCtrl', function() {
     });
 
     describe('#checkHasUnhandledMessages', function() {
-        it('should check that a dialog is opened if there unhandled qas', function(){
 
-            // arrange
+        var newUrl = '/url/a';
+        var currentUrl = '/url/b';
+
+        beforeEach(function(){
+            // the below is run before each sub test as a means to fire a location change event and so opening the dialog.
+            // ----- arrange
             // spy on the defferd
             var deferred = $q.defer();
+            //noinspection JSUnresolvedFunction
             spyOn($q, 'defer').andReturn(deferred);
 
-            //act
+            // ------ act
             // kick off the window change event
-            var newUrl = '/url/a';
-            var currentUrl = '/url/b';
+
             $rootScope.$broadcast('$locationChangeStart', newUrl, currentUrl);
 
             var areThereUnhandledMessages = true;
@@ -86,7 +95,7 @@ describe('ViewCertCtrl', function() {
             // promises are resolved/dispatched only on next $digest cycle
             $rootScope.$apply();
 
-            //assert
+            // ------ assert
             expect(manageCertificateSpy.getCertType).toHaveBeenCalled();
 
             expect($scope.$broadcast).toHaveBeenCalledWith('hasUnhandledQasEvent', deferred);
@@ -94,10 +103,60 @@ describe('ViewCertCtrl', function() {
             // dialog should be opened
             expect(dialogService.showDialog).toHaveBeenCalled();
 
-            // the url wont be changed until a button is pressed!!
-            expect($window.location.href).toEqual('dummy');
-
         });
+
+        it('just check that the dialog is opened and the url is the same', function(){
+            // the url wont be changed until a button is pressed!!
+            expect($window.location.href).toEqual(currentUrl);
+        });
+
+        describe('#buttonHandle', function() {
+            it('handle button click', function(){
+                // inside the handled button click, test that :
+                var args = dialogService.showDialog.mostRecentCall.args;
+                var dialogOptions = args[1];
+                // press the handled button
+                dialogOptions.button1click();
+                // markAllAsHandledEvent is broadcast
+                expect($scope.$broadcast).toHaveBeenCalledWith('markAllAsHandledEvent');
+                // modal is closed
+                expect(modalMock.close).toHaveBeenCalled();
+                // the url wont be changed until a button is pressed!!
+                expect($window.location.href).toEqual(newUrl);
+            });
+        });
+
+        describe('#buttonUnHandle', function() {
+            it('un handled button click', function(){
+                // inside the handled button click, test that :
+                var args = dialogService.showDialog.mostRecentCall.args;
+                var dialogOptions = args[1];
+                // press the handled button
+                dialogOptions.button2click();
+                // no action is taken, just close the dialog
+                // modal is closed
+                expect(modalMock.close).toHaveBeenCalled();
+                // the url wont be changed until a button is pressed!!
+                expect($window.location.href).toEqual(newUrl);
+            });
+        });
+
+        describe('#buttonBack', function() {
+            it('back button click', function(){
+                // inside the handled button click, test that :
+                var args = dialogService.showDialog.mostRecentCall.args;
+                var dialogOptions = args[1];
+                // press the handled button
+                dialogOptions.button3click();
+                // no action is taken, just close the dialog
+                // modal is closed
+                expect(modalMock.close).toHaveBeenCalled();
+                // the url should be the same no changes
+                expect($window.location.href).toEqual(currentUrl);
+            });
+        });
+
+
     });
 
 });
