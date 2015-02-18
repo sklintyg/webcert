0aecf4adf809a4944481d6953b4d9ef6fd4da253
Jesper Holmberg <jesper.holmberg@callistaenterprise.se>
2015-02-18 13:20:18 +0100

    Första embryo till fitnesse-tester

---
+++ specifications/src/main/java/se/inera/webcert/spec/util/SkickadeNotifieringar.groovy
@@ -0,0 +1,17 @@
+package se.inera.webcert.spec.util
+
+class SkickadeNotifieringar extends RestClientFixture {
+
+    def notifieringar
+
+    public void execute() {
+        def restClient = createRestClient(baseUrl)
+        notifieringar = restClient.get(path: "notification-stub/notifieringar/").data
+    }
+
+    public String intygsId() {
+        notifieringar[0].utlatandeType.utlatandeId;
+    }
+
+
+}
