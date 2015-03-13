54b8384edf0c17f886ba724447f8ebb95ecd1f5c
Bjorn Beskow <bb@callistaenterprise.se>
2015-03-13 15:24:07 +0100

    JSON-Serialisera med @class-attributet, för att möjliggöra polymorf deserialisering av WebCertUser objekt.
    (cherry picked from commit ef3bd9e1c8e99ae16e001426c64c890c90a19e64)
    Signed-off-by: Bjorn Beskow <bb@callistaenterprise.se>

--- integration/hsa-integration/src/main/java/se/inera/webcert/hsa/model/SelectableVardenhet.java
+++ integration/hsa-integration/src/main/java/se/inera/webcert/hsa/model/SelectableVardenhet.java
@@ -3,7 +3,9 @@ package se.inera.webcert.hsa.model;
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 
+@JsonTypeInfo(use=JsonTypeInfo.Id.CLASS, include=JsonTypeInfo.As.PROPERTY, property="@class")
 public interface SelectableVardenhet {
 
     String getId();
