ce2dc322163f33f38f589cb4fde7bb40c746e992
Magnus Ekstrand <magnus@ekstrand.net>
2015-03-30 20:10:32 +0200

    Fixed missing overidden method

--- notification-sender/src/test/java/se/inera/webcert/notifications/service/MockIntygModuleRegistry.java
+++ notification-sender/src/test/java/se/inera/webcert/notifications/service/MockIntygModuleRegistry.java
@@ -64,11 +64,6 @@ public class MockIntygModuleRegistry implements IntygModuleRegistry {
             }
 
             @Override
-            public void sendCertificateToRecipient(InternalModelHolder internalModel, String logicalAddress) throws ModuleException {
-
-            }
-
-            @Override
             public void sendCertificateToRecipient(InternalModelHolder internalModel, String logicalAddress, String recipientId) throws ModuleException {
 
             }
