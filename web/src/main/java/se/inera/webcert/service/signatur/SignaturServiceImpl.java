f20cf911ead7471463154553f07119c99f5725fe
Bjorn Beskow <bb@callistaenterprise.se>
2015-05-12 13:08:26 +0200

    Revert "WEBCERT-1797 Refaktorerade transaktionshantering för att få ut rätt versionnummer vid klientsignering. saveDraft körs nu också i transaktion."
    This reverts commit a01e66b16c21196a0f8da8c52a6ce08a0aee8f68.

--- web/src/main/java/se/inera/webcert/service/signatur/SignaturServiceImpl.java
+++ web/src/main/java/se/inera/webcert/service/signatur/SignaturServiceImpl.java
@@ -85,19 +85,26 @@ public class SignaturServiceImpl implements SignaturService {
 
     @Override
     @Transactional
-    public Utkast prepareUtkastForSignering(String intygId, long version, WebCertUser user, LocalDateTime signeringstid) {
+    public SignaturTicket createDraftHash(String intygId, long version) {
         LOG.debug("Hash for clientsignature of draft '{}'", intygId);
 
         // Fetch the certificate draft
         Utkast utkast = getUtkastForSignering(intygId, version);
 
+        // Fetch Webcert user
+        WebCertUser user = webCertUserService.getWebCertUser();
+
+        LocalDateTime signeringstid = LocalDateTime.now();
+        
         // Update certificate with user information
         utkast = updateUtkastForSignering(utkast, user, signeringstid);
 
         // Save the certificate draft
         utkast = utkastRepository.save(utkast);
 
-        return utkast;
+        SignaturTicket statusTicket = createSignaturTicket(utkast.getIntygsId(), utkast.getVersion(), utkast.getModel(), signeringstid);
+
+        return statusTicket;
     }
 
     @Override
@@ -173,12 +180,19 @@ public class SignaturServiceImpl implements SignaturService {
 
     @Override
     @Transactional(noRollbackFor=javax.xml.ws.WebServiceException.class)
-    public SignaturTicket serverSignature(Utkast utkast, WebCertUser user, LocalDateTime signeringstid) {
-        LOG.debug("Signera utkast '{}'", utkast.getIntygsId());
+    public SignaturTicket serverSignature(String intygsId, long version) {
+        LOG.debug("Signera utkast '{}'", intygsId);
+
+        // On server side we need to create our own signature ticket
+        SignaturTicket ticket = createDraftHash(intygsId, version);
+
+        // Fetch Webcert user
+        WebCertUser user = webCertUserService.getWebCertUser();
+
+        // Fetch the certificate
+        Utkast utkast = getUtkastForSignering(intygsId, version);
 
         // Create and persist signature
-        SignaturTicket ticket = createSignaturTicket(utkast.getIntygsId(), utkast.getVersion(), utkast.getModel(), signeringstid);
-
         ticket = createAndPersistSignature(utkast, ticket, "Signatur", user);
         
         // Audit signing
@@ -240,7 +254,7 @@ public class SignaturServiceImpl implements SignaturService {
         return utkast;
     }
 
-    public SignaturTicket createSignaturTicket(String intygId, long version, String payload, LocalDateTime signeringstid) {
+    private SignaturTicket createSignaturTicket(String intygId, long version, String payload, LocalDateTime signeringstid) {
         try {
             String hash = createHash(payload);
             String id = UUID.randomUUID().toString();
