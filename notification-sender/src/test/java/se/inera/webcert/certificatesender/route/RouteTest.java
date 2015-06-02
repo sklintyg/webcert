d5e9c3df151a1ac74a26b53df7893e5bcb1e392b
Jesper Holmberg <jesper.holmberg@callistaenterprise.se>
2015-06-02 15:23:30 +0200

    WEBCERT-1314 route-tester för fler feltyper

--- notification-sender/src/test/java/se/inera/webcert/certificatesender/route/RouteTest.java
+++ notification-sender/src/test/java/se/inera/webcert/certificatesender/route/RouteTest.java
@@ -2,31 +2,23 @@ package se.inera.webcert.certificatesender.route;
 
 import static org.apache.camel.component.mock.MockEndpoint.assertIsSatisfied;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.xml.bind.JAXBException;
-
-import com.google.common.collect.ImmutableMap;
 import org.apache.camel.*;
 import org.apache.camel.component.mock.MockEndpoint;
 import org.apache.camel.test.spring.CamelSpringJUnit4ClassRunner;
 import org.apache.camel.test.spring.MockEndpointsAndSkip;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.annotation.DirtiesContext.ClassMode;
 import org.springframework.test.context.ContextConfiguration;
 
+import se.inera.webcert.certificatesender.exception.PermanentException;
+import se.inera.webcert.certificatesender.exception.TemporaryException;
 import se.inera.webcert.common.Constants;
-import se.inera.webcert.notifications.service.exception.CertificateStatusUpdateServiceException;
-import se.inera.webcert.notifications.service.exception.NonRecoverableCertificateStatusUpdateServiceException;
-import se.riv.clinicalprocess.healthcond.certificate.certificatestatusupdateforcareresponder.v1.CertificateStatusUpdateForCareType;
+
+import com.google.common.collect.ImmutableMap;
 
 
 @RunWith(CamelSpringJUnit4ClassRunner.class)
@@ -37,7 +29,7 @@ public class RouteTest {
     
     private static final Logger LOG = LoggerFactory.getLogger(RouteTest.class);
 
-    private static final String STORE_MESSAGE = "store";
+    private static final String MESSAGE_BODY = "message";
 
     @Produce(uri = "direct:receiveCertificateTransferEndpoint")
     private ProducerTemplate producerTemplate;
@@ -51,10 +43,10 @@ public class RouteTest {
     @EndpointInject(uri = "mock:bean:certificateRevokeProcessor")
     private MockEndpoint revokeProcessor;
 
-    @EndpointInject(uri = "mock:direct:errorHandlerEndpoint")
+    @EndpointInject(uri = "mock:direct:certPermanentErrorHandlerEndpoint")
     private MockEndpoint permanentErrorHandlerEndpoint;
 
-    @EndpointInject(uri = "mock:direct:errorHandlerEndpoint")
+    @EndpointInject(uri = "mock:direct:certTemporaryErrorHandlerEndpoint")
     private MockEndpoint temporaryErrorHandlerEndpoint;
 
     @Test
@@ -67,7 +59,148 @@ public class RouteTest {
         permanentErrorHandlerEndpoint.expectedMessageCount(0);
 
         // When
-        producerTemplate.sendBodyAndHeaders(STORE_MESSAGE, ImmutableMap.<String, Object>of(Constants.MESSAGE_TYPE, Constants.STORE_MESSAGE));
+        producerTemplate.sendBodyAndHeaders(MESSAGE_BODY, ImmutableMap.<String, Object>of(Constants.MESSAGE_TYPE, Constants.STORE_MESSAGE));
+
+        // Then
+        assertIsSatisfied(storeProcessor);
+        assertIsSatisfied(sendProcessor);
+        assertIsSatisfied(revokeProcessor);
+        assertIsSatisfied(temporaryErrorHandlerEndpoint);
+        assertIsSatisfied(permanentErrorHandlerEndpoint);
+    }
+
+    @Test
+    public void testNormalSendRoute() throws InterruptedException {
+        // Given
+        storeProcessor.expectedMessageCount(0);
+        sendProcessor.expectedMessageCount(1);
+        revokeProcessor.expectedMessageCount(0);
+        temporaryErrorHandlerEndpoint.expectedMessageCount(0);
+        permanentErrorHandlerEndpoint.expectedMessageCount(0);
+
+        // When
+        producerTemplate.sendBodyAndHeaders(MESSAGE_BODY, ImmutableMap.<String, Object>of(Constants.MESSAGE_TYPE, Constants.SEND_MESSAGE));
+
+        // Then
+        assertIsSatisfied(storeProcessor);
+        assertIsSatisfied(sendProcessor);
+        assertIsSatisfied(revokeProcessor);
+        assertIsSatisfied(temporaryErrorHandlerEndpoint);
+        assertIsSatisfied(permanentErrorHandlerEndpoint);
+    }
+
+    @Test
+    public void testNormalRevokeRoute() throws InterruptedException {
+        // Given
+        storeProcessor.expectedMessageCount(0);
+        sendProcessor.expectedMessageCount(0);
+        revokeProcessor.expectedMessageCount(1);
+        temporaryErrorHandlerEndpoint.expectedMessageCount(0);
+        permanentErrorHandlerEndpoint.expectedMessageCount(0);
+
+        // When
+        producerTemplate.sendBodyAndHeaders(MESSAGE_BODY, ImmutableMap.<String, Object>of(Constants.MESSAGE_TYPE, Constants.REVOKE_MESSAGE));
+
+        // Then
+        assertIsSatisfied(storeProcessor);
+        assertIsSatisfied(sendProcessor);
+        assertIsSatisfied(revokeProcessor);
+        assertIsSatisfied(temporaryErrorHandlerEndpoint);
+        assertIsSatisfied(permanentErrorHandlerEndpoint);
+    }
+
+    @Test
+    public void testUnknownMessageType() throws InterruptedException {
+        // Given
+        storeProcessor.expectedMessageCount(0);
+        sendProcessor.expectedMessageCount(0);
+        revokeProcessor.expectedMessageCount(0);
+        temporaryErrorHandlerEndpoint.expectedMessageCount(0);
+        permanentErrorHandlerEndpoint.expectedMessageCount(0);
+
+        // When
+        producerTemplate.sendBodyAndHeaders(MESSAGE_BODY, ImmutableMap.<String, Object>of(Constants.MESSAGE_TYPE, "non-existant"));
+
+        // Then
+        assertIsSatisfied(storeProcessor);
+        assertIsSatisfied(sendProcessor);
+        assertIsSatisfied(revokeProcessor);
+        assertIsSatisfied(temporaryErrorHandlerEndpoint);
+        assertIsSatisfied(permanentErrorHandlerEndpoint);
+    }
+
+    @Test
+    public void testPermanentException() throws InterruptedException {
+        // Given
+        sendProcessor.whenAnyExchangeReceived(new Processor() {
+            @Override
+            public void process(Exchange exchange) throws Exception {
+                throw new PermanentException();
+            }
+        });
+
+        storeProcessor.expectedMessageCount(0);
+        sendProcessor.expectedMessageCount(1);
+        revokeProcessor.expectedMessageCount(0);
+        temporaryErrorHandlerEndpoint.expectedMessageCount(0);
+        permanentErrorHandlerEndpoint.expectedMessageCount(1);
+
+        // When
+        producerTemplate.sendBodyAndHeaders(MESSAGE_BODY, ImmutableMap.<String, Object>of(Constants.MESSAGE_TYPE, Constants.SEND_MESSAGE));
+
+        // Then
+        assertIsSatisfied(storeProcessor);
+        assertIsSatisfied(sendProcessor);
+        assertIsSatisfied(revokeProcessor);
+        assertIsSatisfied(temporaryErrorHandlerEndpoint);
+        assertIsSatisfied(permanentErrorHandlerEndpoint);
+    }
+
+    @Test(expected=CamelExecutionException.class)
+    public void testTemporaryException() throws InterruptedException {
+        // Given
+        revokeProcessor.whenAnyExchangeReceived(new Processor() {
+            @Override
+            public void process(Exchange exchange) throws Exception {
+                throw new TemporaryException();
+            }
+        });
+
+        storeProcessor.expectedMessageCount(0);
+        sendProcessor.expectedMessageCount(0);
+        revokeProcessor.expectedMessageCount(1);
+        temporaryErrorHandlerEndpoint.expectedMessageCount(1);
+        permanentErrorHandlerEndpoint.expectedMessageCount(0);
+
+        // When
+        producerTemplate.sendBodyAndHeaders(MESSAGE_BODY, ImmutableMap.<String, Object>of(Constants.MESSAGE_TYPE, Constants.REVOKE_MESSAGE));
+
+        // Then
+        assertIsSatisfied(storeProcessor);
+        assertIsSatisfied(sendProcessor);
+        assertIsSatisfied(revokeProcessor);
+        assertIsSatisfied(temporaryErrorHandlerEndpoint);
+        assertIsSatisfied(permanentErrorHandlerEndpoint);
+    }
+
+    @Test
+    public void testUnexpectedException() throws InterruptedException {
+        // Given
+        storeProcessor.whenAnyExchangeReceived(new Processor() {
+            @Override
+            public void process(Exchange exchange) throws Exception {
+                throw new IllegalArgumentException();
+            }
+        });
+
+        storeProcessor.expectedMessageCount(1);
+        sendProcessor.expectedMessageCount(0);
+        revokeProcessor.expectedMessageCount(0);
+        temporaryErrorHandlerEndpoint.expectedMessageCount(0);
+        permanentErrorHandlerEndpoint.expectedMessageCount(1);
+
+        // When
+        producerTemplate.sendBodyAndHeaders(MESSAGE_BODY, ImmutableMap.<String, Object>of(Constants.MESSAGE_TYPE, Constants.STORE_MESSAGE));
 
         // Then
         assertIsSatisfied(storeProcessor);
