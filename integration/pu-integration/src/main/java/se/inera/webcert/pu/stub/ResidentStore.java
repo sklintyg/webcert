980c2f84fb1b45043330b64294b2475400cc49af
Magnus Ekstrand <magnus@ekstrand.net>
2015-06-22 13:11:29 +0200

    Tidy code

--- integration/pu-integration/src/main/java/se/inera/webcert/pu/stub/ResidentStore.java
+++ integration/pu-integration/src/main/java/se/inera/webcert/pu/stub/ResidentStore.java
@@ -7,12 +7,12 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ResidentStore {
+
     private final Map<String, ResidentType> residents = new HashMap<>();
 
     public void addUser(ResidentType residentPost) {
         residents.put(residentPost.getPersonpost().getPersonId(), residentPost);
     }
-
 
     public ResidentType get(String id) {
         return residents.get(id);
