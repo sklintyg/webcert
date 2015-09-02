56756132714866041b04f0771ade32b28736aeaf
Magnus Ekstrand <magnus@ekstrand.net>
2015-09-02 08:13:51 +0200

    Moved WebCertUser from hsa-integration to webcert/web

---
+++ web/src/main/java/se/inera/webcert/dto/WebCertUser.java
@@ -0,0 +1,333 @@
+package se.inera.webcert.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.SpringSecurityCoreVersion;
+import org.springframework.util.Assert;
+import se.inera.certificate.integration.json.CustomObjectMapper;
+import se.inera.webcert.common.model.UserDetails;
+import se.inera.webcert.hsa.model.AuthenticationMethod;
+import se.inera.webcert.hsa.model.SelectableVardenhet;
+import se.inera.webcert.hsa.model.Vardgivare;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+/**
+ * @author andreaskaltenbach
+ */
+public class WebCertUser implements UserDetails {
+
+    private static final long serialVersionUID = -2624303818412468774L;
+
+    private boolean lakare;
+
+    private String personId;
+    private String hsaId;
+    private String namn;
+    private String titel;
+    private String forskrivarkod;
+    private String authenticationScheme;
+
+    private List<Vardgivare> vardgivare;
+    private List<String> specialiseringar;
+    private List<String> legitimeradeYrkesgrupper;
+
+    private Set<String> aktivaFunktioner;
+
+    private SelectableVardenhet valdVardenhet;
+    private SelectableVardenhet valdVardgivare;
+
+    private final Set<GrantedAuthority> authorities;
+
+    private AuthenticationMethod authenticationMethod; // TODO - temporary hack. BANKID, NETID
+
+
+    /**
+     * Construct the <code>WebCertUser</code> with its authority details.
+     *
+     * @param authorities the authorities that should be granted to the caller. Not null.
+     */
+    public WebCertUser(Collection<? extends GrantedAuthority> authorities) {
+        this.authorities = Collections.unmodifiableSet(sortAuthorities(authorities));
+    }
+
+
+    // - - Getters and setters
+
+    /**
+     * Returns the personal identifier used to authenticate the user.
+     *
+     * @return the personal identifier
+     */
+    @Override
+    public String getPersonId() {
+        return this.personId;
+    }
+
+    public void setPersonId(String personId) {
+        this.personId = personId;
+    }
+
+    /**
+     * Returns the name to authenticate the user. Cannot return <code>null</code>.
+     *
+     * @return the name (never <code>null</code>)
+     */
+    @Override
+    public String getNamn() {
+        return null;
+    }
+
+    public void setNamn(String namn) {
+        this.namn = namn;
+    }
+
+    /**
+     * Returns the authorities granted to the user. Cannot return <code>null</code>.
+     *
+     * @return the authorities, sorted by natural key (never <code>null</code>)
+     */
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    public boolean isLakare() {
+        return lakare;
+    }
+
+    public void setLakare(boolean lakare) {
+        this.lakare = lakare;
+    }
+
+    public String getHsaId() {
+        return hsaId;
+    }
+
+    public void setHsaId(String hsaId) {
+        this.hsaId = hsaId;
+    }
+
+    public List<Vardgivare> getVardgivare() {
+
+        if (vardgivare == null) {
+            vardgivare = Collections.emptyList();
+        }
+
+        return vardgivare;
+    }
+
+    public void setVardgivare(List<Vardgivare> vardgivare) {
+        this.vardgivare = vardgivare;
+    }
+
+    public List<String> getSpecialiseringar() {
+
+        if (specialiseringar == null) {
+            specialiseringar = Collections.emptyList();
+        }
+
+        return specialiseringar;
+    }
+
+    public void setSpecialiseringar(List<String> specialiseringar) {
+        this.specialiseringar = specialiseringar;
+    }
+
+    public String getTitel() {
+        return titel;
+    }
+
+    public void setTitel(String titel) {
+        this.titel = titel;
+    }
+
+    public List<String> getLegitimeradeYrkesgrupper() {
+
+        if (legitimeradeYrkesgrupper == null) {
+            legitimeradeYrkesgrupper = Collections.emptyList();
+        }
+
+        return legitimeradeYrkesgrupper;
+    }
+
+    public void setLegitimeradeYrkesgrupper(List<String> legitimeradeYrkesgrupper) {
+        this.legitimeradeYrkesgrupper = legitimeradeYrkesgrupper;
+    }
+
+    public SelectableVardenhet getValdVardenhet() {
+        return valdVardenhet;
+    }
+
+    public void setValdVardenhet(SelectableVardenhet valdVardenhet) {
+        this.valdVardenhet = valdVardenhet;
+    }
+
+    public String getAuthenticationScheme() {
+        return authenticationScheme;
+    }
+
+    public void setAuthenticationScheme(String authenticationScheme) {
+        this.authenticationScheme = authenticationScheme;
+    }
+
+    public SelectableVardenhet getValdVardgivare() {
+        return valdVardgivare;
+    }
+
+    public void setValdVardgivare(SelectableVardenhet valdVardgivare) {
+        this.valdVardgivare = valdVardgivare;
+    }
+
+    public String getForskrivarkod() {
+        return forskrivarkod;
+    }
+
+    public void setForskrivarkod(String forskrivarkod) {
+        this.forskrivarkod = forskrivarkod;
+    }
+
+    public Set<String> getAktivaFunktioner() {
+        if (aktivaFunktioner == null) {
+            aktivaFunktioner = new HashSet<String>();
+        }
+
+        return aktivaFunktioner;
+    }
+
+    public void setAktivaFunktioner(Set<String> aktivaFunktioner) {
+        this.aktivaFunktioner = aktivaFunktioner;
+    }
+
+    public boolean hasAktivFunktion(String aktivFunktion) {
+        return getAktivaFunktioner().contains(aktivFunktion);
+    }
+
+    public AuthenticationMethod getAuthenticationMethod() {
+        return authenticationMethod;
+    }
+
+    public void setAuthenticationMethod(AuthenticationMethod authenticationMethod) {
+        this.authenticationMethod = authenticationMethod;
+    }
+
+
+    // - - Public scope
+
+    @JsonIgnore
+    public String getAsJson() {
+        try {
+            return new CustomObjectMapper().writeValueAsString(this);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @JsonIgnore
+    public List<String> getIdsOfSelectedVardgivare() {
+
+        SelectableVardenhet selected = getValdVardgivare();
+
+        if (selected == null) {
+            return Collections.emptyList();
+        }
+
+        return selected.getHsaIds();
+    }
+
+    @JsonIgnore
+    public List<String> getIdsOfSelectedVardenhet() {
+
+        SelectableVardenhet selected = getValdVardenhet();
+
+        if (selected == null) {
+            return Collections.emptyList();
+        }
+
+        return selected.getHsaIds();
+    }
+
+    @JsonIgnore
+    public List<String> getIdsOfAllVardenheter() {
+        List<String> allIds = new ArrayList<>();
+        for (Vardgivare v : getVardgivare()) {
+            allIds.addAll(v.getHsaIds());
+        }
+        return allIds;
+    }
+
+    public int getTotaltAntalVardenheter() {
+        return getIdsOfAllVardenheter().size();
+    }
+
+    public boolean changeValdVardenhet(String vardenhetId) {
+        if (vardenhetId == null) {
+            return false;
+        }
+
+        for (Vardgivare vg : getVardgivare()) {
+            SelectableVardenhet ve = vg.findVardenhet(vardenhetId);
+            if (ve != null) {
+                setValdVardenhet(ve);
+                setValdVardgivare(vg);
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    @JsonIgnore
+    @Override
+    public String toString() {
+        return hsaId +  " [authScheme=" + authenticationScheme + ", lakare=" + lakare + "]";
+    }
+
+
+    // - - Private scope
+
+    /*
+     * Ensure array iteration order is predictable
+     */
+    private static SortedSet<GrantedAuthority> sortAuthorities(Collection<? extends GrantedAuthority> authorities) {
+        Assert.notNull(authorities, "Cannot pass a null GrantedAuthority collection");
+        SortedSet<GrantedAuthority> sortedAuthorities =
+                new TreeSet<GrantedAuthority>(new AuthorityComparator());
+
+        for (GrantedAuthority grantedAuthority : authorities) {
+            Assert.notNull(grantedAuthority, "GrantedAuthority list cannot contain any null elements");
+            sortedAuthorities.add(grantedAuthority);
+        }
+
+        return sortedAuthorities;
+    }
+
+    private static class AuthorityComparator implements Comparator<GrantedAuthority>, Serializable {
+        private static final long serialVersionUID = SpringSecurityCoreVersion.SERIAL_VERSION_UID;
+
+        public int compare(GrantedAuthority g1, GrantedAuthority g2) {
+            // Neither should ever be null as each entry is checked before adding it to the set.
+            // If the authority is null, it is a custom authority and should precede others.
+            if (g2.getAuthority() == null) {
+                return -1;
+            }
+
+            if (g1.getAuthority() == null) {
+                return 1;
+            }
+
+            return g1.getAuthority().compareTo(g2.getAuthority());
+        }
+    }
+
+}
