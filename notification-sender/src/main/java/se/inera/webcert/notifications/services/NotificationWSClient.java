2f4a0cc14b4d29fb6edc384133d21ced6e0b0b8c
Marcus Cedergren <marcus.cedergren@callistaenterprise.se>
2015-10-07 12:46:24 +0200

    WEBCERT-2023: Rättat felaktig loggning av intygsId

--- notification-sender/src/main/java/se/inera/webcert/notifications/services/NotificationWSClient.java
+++ notification-sender/src/main/java/se/inera/webcert/notifications/services/NotificationWSClient.java
@@ -24,7 +24,7 @@ public class NotificationWSClient {
     public void sendStatusUpdate(CertificateStatusUpdateForCareType request,
             @Header(RouteHeaders.LOGISK_ADRESS) String logicalAddress) throws Exception {
 
-        LOG.debug("Sending status update to '{}' for intyg '{}'", logicalAddress, request.getUtlatande().getUtlatandeId().toString());
+        LOG.debug("Sending status update to '{}' for intyg '{}'", logicalAddress, request.getUtlatande().getUtlatandeId().getExtension());
 
         CertificateStatusUpdateForCareResponseType response = null;
 
