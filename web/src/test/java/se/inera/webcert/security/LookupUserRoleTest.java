431c26a9dd89ea2238882f59167e30fed5860853
Erik Lupander <erik.lupander@callistaenterprise.se>
2015-10-12 10:55:18 +0200

    WEBCERT-2025 : Merge conflict in test class fixed

--- web/src/test/java/se/inera/webcert/security/LookupUserRoleTest.java
+++ web/src/test/java/se/inera/webcert/security/LookupUserRoleTest.java
@@ -7,6 +7,7 @@ import static org.mockito.Mockito.when;
 import java.util.Arrays;
 import java.util.List;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
