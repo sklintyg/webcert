d896ee4bca1cf1af5e7cf5a94cb6059f6203f9f8
Erik Lupander <erik.lupander@callistaenterprise.se>
2015-11-26 13:16:55 +0100

    WEBCERT-2215: Merge fix

--- integration/pu-integration/src/test/java/se/inera/webcert/pu/stub/LookupResidentForFullProfileWsStubTest.java
+++ integration/pu-integration/src/test/java/se/inera/webcert/pu/stub/LookupResidentForFullProfileWsStubTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 import static org.mockito.Mockito.when;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
