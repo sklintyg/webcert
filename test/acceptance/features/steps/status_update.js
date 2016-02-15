3383bb863e628388942c1695805663e7ce3a02a9
Peter R. Torpman <peter.torpman@nordicmedtest.se>
2016-02-15 11:28:19 +0100

    ACCTEST: Uppdaterade status update testfallen

--- test/acceptance/features/steps/status_update.js
+++ test/acceptance/features/steps/status_update.js
@@ -235,7 +235,7 @@ module.exports = function () {
     });
 
     this.Given(/^jag markerar frågan som hanterad$/, function (callback) {
-        element(basIntyg.handeledBtn + global.intyg.fragaId).sendKeys(protractor.Key.SPACE).then(callback);
+        element(by.id('markAsHandledWcOriginBtn-' + global.intyg.fragaId)).sendKeys(protractor.Key.SPACE).then(callback);
     });
 
     this.Given(/^jag markerar frågan från Försäkringskassan som hanterad$/, function (callback) {
