309d855d7c692df7827a09ea866f3412b95c49a8
Rasmus Lundberg-Åblad <rasmus.ablad@usify.se>
2016-04-07 00:56:39 +0200

    Revert "INTYG-1991: FMB porting från k7263 till sjukpenning uttökad, ändrat backend som nu returnerar nya formulärs namn."
    This reverts commit c526b59c752fb683adccf28a907db157c935de57.

--- web/src/main/java/se/inera/intyg/webcert/web/web/controller/api/dto/FmbFormName.java
+++ web/src/main/java/se/inera/intyg/webcert/web/web/controller/api/dto/FmbFormName.java
@@ -20,5 +20,7 @@
 package se.inera.intyg.webcert.web.web.controller.api.dto;
 
 public enum FmbFormName {
-    FORM2, FORM4, FORM5, FORM8B, DIAGNOS, FUNKTIONSNEDSATTNING, AKTIVITETSERSATTNING, ARBETSFORMOGA
+
+    FORM2, FORM4, FORM5, FORM8B;
+
 }
