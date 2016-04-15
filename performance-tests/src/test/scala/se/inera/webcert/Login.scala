fdca516c762f218d7e5f58c2e703972a419ef17b
Erik Forsgren <erik.forsgren@r2m.se>
2016-04-15 13:12:08 +0200

    DEVENV: Använd en user som finns i test-hsa för gatling-tester samt  bumpat version till 2.17

--- performance-tests/src/test/scala/se/inera/webcert/Login.scala
+++ performance-tests/src/test/scala/se/inera/webcert/Login.scala
@@ -7,7 +7,8 @@ object Login {
 
 	val logins = Map("Åsa-Enhet1" -> """{"fornamn": "Åsa", "efternamn": "Andersson", "hsaId": "IFV1239877878-104B", "enhetId": "IFV1239877878-1042","lakare": true}""",
 	                 "Åsa-Enhet2" -> """{"fornamn": "Åsa", "efternamn": "Andersson", "hsaId": "IFV1239877878-104B", "enhetId": "IFV1239877878-1045","lakare": true}""",
-	                 "Eva" -> """{"fornamn": "Eva", "efternamn": "Holgersson", "hsaId": "eva", "enhetId": "centrum-vast", "lakare": true}"""
+	                 "Eva" -> """{"fornamn": "Eva", "efternamn": "Holgersson", "hsaId": "eva", "enhetId": "centrum-vast", "lakare": true}""",
+	                 "Leonie" -> """{"fornamn": "Leonie", "efternamn": "Koehl", "hsaId": "TSTNMT2321000156-103F", "enhetId": "TSTNMT2321000156-1039", "lakare": true}"""
 	                 )
 	
 	def loginAs(login: String) = {
