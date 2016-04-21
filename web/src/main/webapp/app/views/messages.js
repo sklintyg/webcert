4122fe8f8c7ed3db307dc4249a4e2c20a5928016
Marcus Cedergren <marcus.cedergren@callistaenterprise.se>
2016-04-21 12:45:13 +0200

    INTYG-2250: lagt till luae_fs i authorities.yaml (bara att det finns)

--- web/src/main/webapp/app/views/messages.js
+++ web/src/main/webapp/app/views/messages.js
@@ -72,7 +72,7 @@ var wcMessages = {
         'certificatetypes.ts-diabetes.typename': 'Transportstyrelsens läkarintyg, diabetes',
         'certificatetypes.lisu.typename': 'Läkarintyg för sjukpenning utökat',
         'certificatetypes.luse.typename': 'Läkarutlåtande för sjukersättning',
-        'certificatetypes.luae_fs.typename': 'Läkarutlåtande för aktivitetsersättning vid förängd skolgång',
+        'certificatetypes.luae_fs.typename': 'Läkarutlåtande för aktivitetsersättning vid förlängd skolgång',
 
         //certificate help texts old
         'certificatetypes.ts-bas.helptext': '<p>Transportstyrelsens läkarintyg ska användas vid förlängd giltighet av högre behörighet från 45 år, ansökan om körkortstillstånd för grupp II och III och vid ansökan om taxiförarlegitimation. Transportstyrelsens läkarintyg kan även användas när Transportstyrelsen i annat fall begärt ett allmänt läkarintyg avseende lämplighet att inneha körkort.</p>Specialistintyg finns bl.a. för alkohol, läkemedel, synfunktion, Alkolås m.m. Se <a href="http://www.transportstyrelsen.se" target="_blank">www.transportstyrelsen.se</a>.',
