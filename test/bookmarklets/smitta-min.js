3320d61f7c6d2850482e06c06ff7b29245a94737
jonasglenne <jonas.glenne@nordicmedtest.se>
2016-05-11 13:57:10 +0200

    Feltänk i bookmarklet filer i commit 679bb9787623

--- test/bookmarklets/smitta-min.js
+++ test/bookmarklets/smitta-min.js
@@ -1,9 +1,8 @@
-javascript: (function() {
-    $('#smittskydd').trigger("click");
-    $('#nedsattMed50').trigger("click");
-    $("#nedsattMed50from").val("2015-02-01");
-    $("#nedsattMed50tom").val("2015-12-01");
-    $("#capacityForWorkText").val("capacityForWorkText");
-    $("#clinicInfoPhone").val("07388888888");
-    $("#clinicInfoPhone").change();
-})();
+$('#smittskydd').trigger("click");
+$('#nedsattMed50').trigger("click");
+$("#nedsattMed50from").val("2015-02-01");
+$("#nedsattMed50tom").val("2015-12-01");
+$("#capacityForWorkText").val("capacityForWorkText");
+$("#clinicInfoPhone").val("07388888888");
+$("#clinicInfoPhone").change();
+
