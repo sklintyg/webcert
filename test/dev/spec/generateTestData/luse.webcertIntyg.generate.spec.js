b78bbb4dcf1e5bbf702224c71254c40787fb5daf
Benny Samuelsson <BESA@usify.local>
2016-05-17 15:28:53 +0200

    DEVENV: Ignore jasmine words.

--- test/dev/spec/generateTestData/luse.webcertIntyg.generate.spec.js
+++ test/dev/spec/generateTestData/luse.webcertIntyg.generate.spec.js
@@ -16,6 +16,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+/*globals fdescribe, browser*/
 
 'use strict';
 var wcTestTools = require('webcert-testtools');
