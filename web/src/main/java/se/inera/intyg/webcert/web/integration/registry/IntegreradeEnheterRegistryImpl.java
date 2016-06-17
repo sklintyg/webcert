703b91780b04ee01739d41adc7cc949625c0a806
Karl Johan Andreasson <kalle.kaan@gmail.com>
2016-06-17 13:34:24 +0200

    DEVENV: Förtydligande kommentar kring övergångsperiod från notifiering v1->v2

--- web/src/main/java/se/inera/intyg/webcert/web/integration/registry/IntegreradeEnheterRegistryImpl.java
+++ web/src/main/java/se/inera/intyg/webcert/web/integration/registry/IntegreradeEnheterRegistryImpl.java
@@ -134,6 +134,20 @@ public class IntegreradeEnheterRegistryImpl implements IntegreradeEnheterRegistr
             return Optional.empty();
         }
 
+        /*
+         * This is complicated because we have a transition period for fk7263 where we still send notifications for
+         * events for old certificates.
+         *
+         * First if-case handles new (which means rivta version 2 - SIT certificates). These certificates should only
+         * use VERSION_2.
+         *
+         * Then we get to the case where fk7263 is handled during a transition period. When this is the case VERSION_1
+         * will always be set to true - which means they have previously accepted notifications for certificates written
+         * on this unit. If this is not the case we are dealing with Cambio which does not want notifications for these
+         * old certificates but still receive mail that a question or answer was received.
+         *
+         * If VERSION_1 is set we use the latest version available.
+         */
         if (!oldIntygTypes.contains(intygType)) {
             return enhet.isSchemaVersion2() ? Optional.of(SchemaVersion.VERSION_2) : Optional.empty();
         } else if (!enhet.isSchemaVersion1()) {
