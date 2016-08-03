bb71a63a0b5a2d1f98e66259e83cd9b162f45bb8
Marcus Cedergren <marcus.cedergren@callistaenterprise.se>
2016-08-03 10:12:02 +0200

    INTYG-2521: Lagt till wc-fmb-help-display demo

--- web/src/main/webapp/pubapp/showcase/views/utkast.controller.js
+++ web/src/main/webapp/pubapp/showcase/views/utkast.controller.js
@@ -18,7 +18,7 @@
  */
 
 angular.module('showcase').controller('showcase.UtkastCtrl',
-    [ '$scope',  '$httpBackend', 'common.UtkastViewStateService',
+    ['$scope', '$httpBackend', 'common.UtkastViewStateService',
         function($scope, $httpBackend, CommonViewState) {
             'use strict';
 
@@ -44,5 +44,22 @@ angular.module('showcase').controller('showcase.UtkastCtrl',
 
             CommonViewState.showComplete = true;
             CommonViewState.intyg.isComplete = false;
+
+            $scope.fmbState =
+            {
+                cls: 'sit-fmb-small',
+                code:'J22',
+
+                helpTextContents:[
+                            {
+                                heading:'SYMPTOM_PROGNOS_BEHANDLING',
+                                text:'Akut bronkit orsakas vanligen av luftvägsinflammation och epitelskada (skador på hud och slemhinnor i kroppen) efter vanlig virusförkylning. Akut bronkit kan ge hosta under flera månader och är ofta tecken på inflammation i bronkerna. Symtom är akut påkommande torr eller slemmig hosta.'
+                            },
+                            {
+                                heading:'GENERELL_INFO',
+                                text:'Återkommande akuta bronkiter hos rökare bör medföra rökstopp. Bihåleinflammationer efter viroser kan ligga bakom återkommande akuta bronkiter. Långvarig bronkit kan bero på twar eller infektion med mykoplasma pneumoni. \n\nHos patienter med samtidig annan luftvägs- eller lungsjukdom som exempelvis astma eller kol kan symtomen vid akut bronkit bli mer uttalade och funktionsnedsättningen bli mer långdragen. Om det är långa texter kan läggs det till en vertikal scrollbar.Själva rutan finns i 3 olika maxstorlekar för att inte hamna över ev andra fmbrutor som ligger nedanför.'
+                            }
+                ]
+            }
         }]);
 
