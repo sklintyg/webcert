8d067b147de38ae98e1c37c8fad65fec7e43e7a1
Anton Danielsson <github@antondanielsson.se>
2016-08-15 14:48:48 +0200

    ACCTEST: report use jsonp

--- test/acceptance/report/index.js
+++ test/acceptance/report/index.js
@@ -46,43 +46,65 @@
      return scenarioResult;
  }
 
- //4a.function creation
- var slingshot = function(tplId, anchor) {
-     $.getJSON('acc_results.json', function(features) {
-         var template = $(tplId).html();
+ function drawResults(features) {
+     var template = $('#tpl').html();
 
 
-         //Loop features
-         var featureResult = '';
-         console.log('features:' + features);
-         $.each(features, function(fIndex, feature) {
-             featureResult = 'passed';
-             // Loop scenarios
-             console.log('elements:' + feature.elements);
-             $.each(feature.elements, function(eIndex, scenario) {
-                 var scenarioResult = getScenarioResult(scenario.steps);
-                 features[fIndex].elements[eIndex].status = scenarioResult;
+     //Loop features
+     var featureResult = '';
+     console.log('features:' + features);
+     $.each(features, function(fIndex, feature) {
+         featureResult = 'passed';
+         // Loop scenarios
+         console.log('elements:' + feature.elements);
+         $.each(feature.elements, function(eIndex, scenario) {
+             var scenarioResult = getScenarioResult(scenario.steps);
+             features[fIndex].elements[eIndex].status = scenarioResult;
 
-                 if (scenarioResult !== 'passed') {
-                     featureResult = scenarioResult;
-                 }
-             });
-
-             features[fIndex].status = featureResult;
-         });
-         var stone = Handlebars.compile(template)(features);
-         $(anchor).append(stone);
-
-         $('img').click(function() {
-             $(this).toggleClass('bigger');
+             if (scenarioResult !== 'passed') {
+                 featureResult = scenarioResult;
+             }
          });
 
+         features[fIndex].status = featureResult;
+     });
+     var stone = Handlebars.compile(template)(features);
+     $('#anchor').append(stone);
+
+     $('img').click(function() {
+         $(this).toggleClass('bigger');
      });
 
+ }
 
- };
+ // $.ajax({
+ //     dataType: 'jsonp',
+ //     url: 'acc_results.json?callback=?',
+ //     contentType: 'application/json',
+ //     mimeType: 'application/json'
+ // })
+ //     .always(function(msg) {
+ //         console.log(msg);
+ //     });
 
 
+ // $.ajax({
+ //     url: "acc_results.json?callback=aaa",
+ //     type: 'GET',
+ //     dataType: 'JSONP',
+ //     success: function(data) {
+ //         alert(data);
+ //     }
+ // });
 
- //4b.function firing
- slingshot('#tpl', '#anchor'); // since url = 'data.json' , we can use both notations.
+ $.getJSON("acc_results.json?callback=?")
+     .done(function(result) {
+         console.log("success");
+         console.log(result);
+         drawResults(result);
+
+     })
+     .fail(function(result) {
+         console.log("error");
+         drawResults(JSON.parse(result.responseText));
+     });
