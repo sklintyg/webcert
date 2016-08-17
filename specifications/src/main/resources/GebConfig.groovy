3089e474b424c0fbb073b49f3b3363fd31710a87
Erik Forsgren <erik.forsgren@r2m.se>
2016-08-17 10:06:03 +0200

    BUILDENV: Kör fitnesse på byggservern mot NMTs selenium-server

--- specifications/src/main/resources/GebConfig.groovy
+++ specifications/src/main/resources/GebConfig.groovy
@@ -33,39 +33,6 @@ waiting {
 atCheckWaiting = true
 
 environments {
-    saucelabschrome {
-        driver = {
-            DesiredCapabilities capabilities = DesiredCapabilities.chrome();
-            capabilities.setCapability("version", "31");
-            capabilities.setCapability("platform", Platform.WIN8);
-            createDriver(capabilities);
-        }
-    }
-    saucelabsfirefox {
-        driver = {
-            DesiredCapabilities capabilities = DesiredCapabilities.firefox();
-            capabilities.setCapability("version", "23");
-            capabilities.setCapability("platform", Platform.WIN8);
-            createDriver(capabilities);
-        }
-    }
-    saucelabsie {
-        driver = {
-            DesiredCapabilities capabilities = DesiredCapabilities.internetExplorer();
-            capabilities.setCapability("version", "10");
-            capabilities.setCapability("platform", Platform.WIN8);
-            createDriver(capabilities);
-        }
-    }
-    saucelabssafari {
-        // Login to saucelabs.com. Name: olofklason  Pwd: MittSauceLabs1
-        driver = {
-            DesiredCapabilities capabilities = DesiredCapabilities.safari();
-            capabilities.setCapability("version", "5");
-            capabilities.setCapability("platform", "OS X 10.6");
-            createDriver(capabilities);
-        }
-    }
     chrome {
         driver = { new ChromeDriver() }
     }
@@ -79,6 +46,11 @@ environments {
     firefox {
         driver = { new FirefoxDriver() }
     }
+    firefoxRemote {
+        driver = {
+            new RemoteWebDriver(new URL("http://selenium1.nordicmedtest.se:4444/wd/hub"), DesiredCapabilities.firefox())
+        }
+    }
     headless {
         driver = { new HtmlUnitDriver() }
     }
@@ -87,11 +59,4 @@ environments {
             new RemoteWebDriver(new URL("http://windows.ci-server.local"), DesiredCapabilities.internetExplorer())
         }
     }
-}
-
-private RemoteWebDriver createDriver(DesiredCapabilities capabilities) {
-    // Login to saucelabs.com. Name: olofklason  Pwd: MittSauceLabs1
-    new RemoteWebDriver(
-            new URL("http://olofklason:da0ae2dc-0522-4c76-972c-26e4e900ecfe@ondemand.saucelabs.com:80/wd/hub"),
-            capabilities)
 }
