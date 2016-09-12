9d1c9b13e3867687df098bc5ca0505a2aead6ee7
Kristina Gullman <kristina.gullman@nordicmedtest.se>
2016-09-12 14:37:57 +0200

    ACCTEST:Felaktigheter i HSA-katalogen

--- test/acceptance/features/steps/inloggning/login.helpers.js
+++ test/acceptance/features/steps/inloggning/login.helpers.js
@@ -20,44 +20,57 @@
 /*global logger, JSON, browser, pages */
 'use strict';
 
-module.exports = {
-    logInAsUserRole: function(userObj, roleName, skipCookieConsent, secondBrowser) {
-        logger.info((secondBrowser) ? 'Login in second browser >>' : '');
-        if (skipCookieConsent) {
-            logger.info('Lämnar inte samtycke för kakor');
-        }
-        logger.info('Loggar in som ' + userObj.fornamn + ' ' + userObj.efternamn);
+var logInAsUser = function(userObj, skipCookieConsent, secondBrowser) {
+    logger.info((secondBrowser) ? 'Login in second browser >>' : '');
+    if (skipCookieConsent) {
+        logger.info('Lämnar inte samtycke för kakor');
+    }
+    logger.info('Loggar in som ' + userObj.fornamn + ' ' + userObj.efternamn);
 
-        // Fattigmans-kloning av användar-hashen.
-        global.user = JSON.parse(JSON.stringify(userObj));
+    // Fattigmans-kloning av användar-hashen.
+    global.user = JSON.parse(JSON.stringify(userObj));
+
+
+    var login;
+    if (!secondBrowser) {
+
+        browser.ignoreSynchronization = true;
+        pages.welcome.get();
+        browser.sleep(2000);
+        login = pages.welcome.loginByJSON(JSON.stringify(userObj), !skipCookieConsent);
+
+        browser.ignoreSynchronization = false;
+        browser.sleep(3000);
+        console.log('inte SecondBrowser ');
+
+
+    } else {
+        secondBrowser.ignoreSynchronization = true;
+        secondBrowser.get('welcome.jsp');
+        secondBrowser.sleep(2000);
+        login = pages.welcome.loginByJSON(JSON.stringify(userObj), !skipCookieConsent, secondBrowser);
+        console.log('SecondBrowser ');
+        secondBrowser.ignoreSynchronization = false;
+        secondBrowser.sleep(3000);
+    }
+
+    return login;
+};
+
+module.exports = {
+    logInAsUser: logInAsUser,
+    logInAsUserRole: function(userObj, roleName, skipCookieConsent, secondBrowser) {
+        console.log(userObj);
         global.user.roleName = roleName;
 
-        var login;
-        if (!secondBrowser) {
+        return logInAsUser(userObj, skipCookieConsent, secondBrowser).then(function() {
+            logger.info((secondBrowser) ? 'Login second browser successful' : 'Login default browser successful');
 
-            browser.ignoreSynchronization = true;
-            pages.welcome.get();
-            browser.sleep(2000);
-            login = pages.welcome.loginByJSON(JSON.stringify(userObj), !skipCookieConsent);
-
-            browser.ignoreSynchronization = false;
-            browser.sleep(3000);
-
-        } else {
-            secondBrowser.ignoreSynchronization = true;
-            secondBrowser.get('welcome.jsp');
-            secondBrowser.sleep(2000);
-            login = pages.welcome.loginByJSON(JSON.stringify(userObj), !skipCookieConsent, secondBrowser);
-
-            secondBrowser.ignoreSynchronization = false;
-            secondBrowser.sleep(3000);
-        }
-
-        return login.then(function() {
-            logger.info((secondBrowser) ? 'Login second browser successful' : '');
             var wcHeader = secondBrowser ? secondBrowser.findElement(by.id('wcHeader')) : element(by.id('wcHeader'));
             return expect(wcHeader.getText()).to.eventually.contain(roleName + ' - ' + userObj.fornamn + ' ' + userObj.efternamn);
         });
 
     }
+
+
 };
