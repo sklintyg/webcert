4a6268c1a5605a2a19ab561433f5b315a783ea77
Anton Danielsson <github@antondanielsson.se>
2016-09-16 09:39:25 +0200

    TESTENV: Moved bookmarklets to separate repo

--- test/bookmarklets/smitta-min-url-encoded.js
+++
@@ -1,2 +0,0 @@
-javascript:(function()%7B%24('%23smittskydd').trigger(%22click%22)%3B%24('%23nedsattMed50').trigger(%22click%22)%3B%24(%22%23nedsattMed50from%22).val(%222015-02-01%22)%3B%24(%22%23nedsattMed50tom%22).val(%222015-12-01%22)%3B%24(%22%23capacityForWorkText%22).val(%22capacityForWorkText%22)%3B%24(%22%23clinicInfoPhone%22).val(%2207388888888%22)%3B%24(%22%23clinicInfoPhone%22).change()%7D)()
-
