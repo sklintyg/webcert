9938a54dd79e11430f27ad8dbca408ade3375d92
Anton Danielsson <github@antondanielsson.se>
2016-09-26 08:03:07 +0200

    ACCTEST: Tog bort felspecat namn på databas

--- test/acceptance/features/steps/dbActions/makeConnection.js
+++ test/acceptance/features/steps/dbActions/makeConnection.js
@@ -28,7 +28,7 @@ module.exports = function() {
         host: process.env.DATABASE_HOST,
         user: process.env.DATABASE_USER,
         password: process.env.DATABASE_PASSWORD,
-        database: process.env.DATABASE_NAME,
+        // database: process.env.DATABASE_NAME,
         multipleStatements: true
     });
 };
