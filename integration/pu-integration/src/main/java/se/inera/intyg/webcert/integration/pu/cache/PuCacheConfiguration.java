029f0e45d96c72bb08b88101ff3e265ac05feff3
Erik Lupander <erik.lupander@callistaenterprise.se>
2016-10-25 11:11:41 +0200

    INTYG-3046: Lagt till konfiguration för HSA-cachning samt cache-monitorering

---
+++ integration/pu-integration/src/main/java/se/inera/intyg/webcert/integration/pu/cache/PuCacheConfiguration.java
@@ -0,0 +1,35 @@
+package se.inera.intyg.webcert.integration.pu.cache;
+
+import org.apache.ignite.cache.spring.SpringCacheManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.cache.expiry.CreatedExpiryPolicy;
+import javax.cache.expiry.Duration;
+
+/**
+ * While the cacheManager.getCache(...) isn't strictly necessary for creating the cache used by
+ * {@link se.inera.intyg.webcert.integration.pu.services.PUServiceImpl}, this class provides us with the capability
+ * of configuring individual caches based on the current state of the
+ * {@link org.apache.ignite.cache.spring.SpringCacheManager#dynamicCacheCfg}
+ *
+ * Created by eriklupander on 2016-10-20.
+ */
+@Component
+public class PuCacheConfiguration {
+
+    public static final String PERSON_CACHE_NAME = "personCache";
+
+    private Duration defaultPersonCacheExpiry = Duration.ONE_MINUTE;
+
+    @Autowired
+    private SpringCacheManager cacheManager;
+
+    @PostConstruct
+    public void init() {
+        cacheManager.getDynamicCacheConfiguration().setExpiryPolicyFactory(CreatedExpiryPolicy.factoryOf(defaultPersonCacheExpiry));
+        cacheManager.getCache(PERSON_CACHE_NAME);
+    }
+
+}
