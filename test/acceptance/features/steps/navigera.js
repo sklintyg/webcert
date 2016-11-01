6943f01016ede67b3f18c1a391f1ec51ae9c5209
Anton Danielsson <github@antondanielsson.se>
2016-11-01 09:55:44 +0100

    ACCTEST:Småfixar efter namnbyte

--- test/acceptance/features/steps/navigera.js
+++ test/acceptance/features/steps/navigera.js
@@ -41,7 +41,15 @@ module.exports = function() {
         return (global.user.enhetId = enhet);
     });
 
-    this.Given(/^jag går in på (intygsutkastet|intyget)( via djupintegrationslänk| via uthoppslänk)( med annat namn och adress)?$/, function(intygstyp, origin, annatNamnOchAddress) {
+    this.Given(/^jag går in på intygsutkastet via djupintegrationslänk med annat namn och adress$/, function() {
+        return gotoIntyg('intygsutkastet', ' via djupintegrationslänk');
+    });
+
+    this.Given(/^jag går in på (intygsutkastet|intyget)( via djupintegrationslänk| via uthoppslänk)*$/, function(intygstyp, origin) {
+        return gotoIntyg(intygstyp, origin);
+    });
+
+    function gotoIntyg(intygstyp, origin) {
         var url;
         var isSMIIntyg;
         if (intyg && intyg.typ) {
@@ -105,54 +113,5 @@ module.exports = function() {
 
 
         });
-    });
-
-    // this.Given(/^jag går in på intygsutkastet via djupintegrationslänk med annat namn och adress$/, function() {
-    //     var url;
-    //     var isSMIIntyg;
-    //     if (intyg && intyg.typ) {
-    //         isSMIIntyg = helpers.isSMIIntyg(intyg.typ);
-    //     }
-    //     if (isSMIIntyg) {
-    //         url = process.env.WEBCERT_URL + 'visa/intyg/' + global.intyg.id;
-    //         url = url + '?';
-    //         url += 'fornamn=test-arparn&';
-    //         url += 'efternamn=arparnsson&';
-    //         url += 'postadress=Langgatan%2012&';
-    //         url += 'postnummer=990%2090&';
-    //         url += 'postort=Simrishamn&';
-    //         url += 'ref=testref&';
-    //         url += 'enhet=' + global.user.enhetId;
-
-    //     } else {
-    //         //EN WORKAROUND med parameter TILLS INTYG 2711 är LÖST
-    //         url = process.env.WEBCERT_URL + 'visa/intyg/' + global.intyg.id + '?fornamn=TODO';
-    //     }
-
-    //     return browser.get(url).then(function() {
-    //         console.log('Går till url: ' + url);
-    //         if (!isSMIIntyg) { // om djupintegration v1 så kommer det fram uppdragsval
-    //             var enhetSelectorLink = element(by.id('wc-integration-enhet-selector-select-active-unit-' + global.user.enhetId + '-link'));
-    //             enhetSelectorLink.isPresent().then(function(isPresent) {
-    //                 if (isPresent) {
-    //                     return enhetSelectorLink.click().then(function() {
-    //                         return browser.sleep(3000).then(function() { //sleep eftersom vi directas via säkerhetstjänsten
-    //                             return helpers.fetchMessageIds(intyg.typ);
-    //                         });
-    //                     });
-    //                 } else {
-    //                     return browser.sleep(3000).then(function() { //sleep eftersom vi directas via säkerhetstjänsten
-    //                         return helpers.fetchMessageIds(intyg.typ);
-    //                     });
-    //                 }
-
-    //             });
-    //         } else {
-    //             return helpers.fetchMessageIds(intyg.typ);
-    //         }
-
-
-
-    //     });
-    // });
+    }
 };
