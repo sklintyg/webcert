72de5b83cc9386ab271ab1a4a7038c02d95663da
Anton Danielsson <github@antondanielsson.se>
2016-11-11 08:19:52 +0100

    ACCTEST: Förenklat och reparerat kopiera-test

--- test/acceptance/features/steps/checkValues/check.values.steps.js
+++ test/acceptance/features/steps/checkValues/check.values.steps.js
@@ -17,10 +17,13 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-/* globals person, Promise,pages*/
+/* globals person, Promise,pages,intyg*/
 
 'use strict';
 var lusePage = pages.intyg.luse.intyg;
+var fkUtkastPage = pages.intyg.fk['7263'].utkast;
+var luseUtkastPage = pages.intyg.luse.utkast;
+var helpers = require('../helpers');
 module.exports = function() {
 
     this.Given(/^ska intyget visa den nya addressen$/, function() {
@@ -37,6 +40,26 @@ module.exports = function() {
 
     this.Given(/^ska intyget visa det nya personnummret$/, function() {
         return expect(lusePage.patientNamnOchPersonnummer.getText()).to.eventually.contain(person.fornamn + ' ' + person.efternamn + ' - ' + person.id);
+    });
+
+    this.Given(/^ska adressen kopieras till det kopierade intyget$/, function() {
+        var promiseArray = [];
+        var isSMIIntyg = helpers.isSMIIntyg(intyg.typ);
+        if (isSMIIntyg) {
+            promiseArray.push(expect(luseUtkastPage.enhetensAdress.enhetsTelefon.getAttribute('value')).to.eventually.equal(global.user.enhetsAdress.telefon));
+            promiseArray.push(expect(luseUtkastPage.enhetensAdress.postAdress.getAttribute('value')).to.eventually.equal(global.user.enhetsAdress.postadress));
+            promiseArray.push(expect(luseUtkastPage.enhetensAdress.postNummer.getAttribute('value')).to.eventually.equal(global.user.enhetsAdress.postnummer));
+            promiseArray.push(expect(luseUtkastPage.enhetensAdress.postOrt.getAttribute('value')).to.eventually.equal(global.user.enhetsAdress.postort));
+
+        } else {
+
+            promiseArray.push(expect(fkUtkastPage.enhetensAdress.postAdress.getAttribute('value')).to.eventually.equal(global.user.enhetsAdress.postadress));
+            promiseArray.push(expect(fkUtkastPage.enhetensAdress.postNummer.getAttribute('value')).to.eventually.equal(global.user.enhetsAdress.postnummer));
+            promiseArray.push(expect(fkUtkastPage.enhetensAdress.postOrt.getAttribute('value')).to.eventually.equal(global.user.enhetsAdress.postort));
+            promiseArray.push(expect(fkUtkastPage.enhetensAdress.enhetsTelefon.getAttribute('value')).to.eventually.equal(global.user.enhetsAdress.telefon));
+        }
+        return Promise.all(promiseArray);
+
     });
 
 };
