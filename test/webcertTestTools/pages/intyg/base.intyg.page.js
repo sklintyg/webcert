35cb6de103e8f83fcd77decb6f5120b5f2598ee7
Martin Pring <martin.pring@learningwell.se>
2016-12-21 14:07:49 +0100

    ACCTEST: replaced sendKeys(protractor.Key.SPACE) with click()

--- test/webcertTestTools/pages/intyg/base.intyg.page.js
+++ test/webcertTestTools/pages/intyg/base.intyg.page.js
@@ -135,7 +135,7 @@ var BaseIntyg = JClass._extend({
         return this.fornya.dialogConfirmButton;
     },
     skrivUtFullstandigtIntyg: function() {
-        return element(by.id('intyg-header-dropdown-select-pdf-type')).sendKeys(protractor.Key.SPACE).then(function() {
+        return element(by.id('intyg-header-dropdown-select-pdf-type')).click().then(function() {
             return element(by.cssContainingText('a', 'Fullständigt intyg')).click();
         });
     },
