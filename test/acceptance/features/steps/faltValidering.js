5544e5736579f6004d98b030c98d510b6bcc7484
Anton Danielsson <github@antondanielsson.se>
2016-12-22 10:29:59 +0100

    ACCTEST: La tillbaka saknade funktioner för fältvalidering-test

--- test/acceptance/features/steps/faltValidering.js
+++ test/acceptance/features/steps/faltValidering.js
@@ -27,10 +27,29 @@ var testdata = wcTestTools.testdata;
 var testdataHelpers = wcTestTools.helpers.testdata;
 var tmpDiagnos;
 
+var anhorigIgnoreKeys = ['forsakringsmedicinsktBeslutsstodBeskrivning', 'arbetstidsforlaggning', 'arbetsresor', 'formagaTrotsBegransningBeskrivning', 'prognos'];
+
+
 function setDiagnos(diagnos) {
     tmpDiagnos = diagnos;
     // console.log(diagnos);
 
+}
+
+function populateFieldArray(object, ignoreKeys) {
+    var re = [];
+    if (object) {
+        for (var key in object) {
+            if (object.hasOwnProperty(key)) {
+                var index = (typeof ignoreKeys !== 'undefined') ? ignoreKeys.indexOf(key) : -1;
+                if (index === -1) {
+
+                    re.push(object[key]);
+                }
+            }
+        }
+    }
+    return re;
 }
 
 function checkFMB(promArray, isSMIIntyg, egenDiagnos, fmbDiagnos, antalDiagnoser) {
@@ -162,44 +181,78 @@ module.exports = function() {
 
     });
 
+    this.Given(/^ska valideringsfelet "([^"]*)" visas$/, function(arg1) {
+        var alertTexts = element.all(by.css('.alert-danger')).map(function(elm) {
+            return elm.getText();
+        });
+        return alertTexts.then(function(result) {
+            return expect(result.join('\n')).to.have.string(arg1);
+        });
+    });
 
-    this.Given(/^ska "([^"]*)" valideringsfelet, "([^"]*)" visas$/, function(arg1, arg2) {
+    // this.Given(/^ska "([^"]*)" valideringsfelet, "([^"]*)" visas$/, function(arg1, arg2) {
 
-        if (arg1 === 'postnummer') {
-            element(by.css('span[stat-key="common.validation.postnummer.incorrect-format"]')).getText().then(function(text) {
-                return expect(text).to.contain(arg2);
-            });
-        } else if (arg1 === 'underlag') {
-            element(by.css('span[stat-key="luse.validation.underlag.date.incorrect_format"]')).getText().then(function(text) {
-                return expect(text).to.contain(arg2);
-            });
-        } else if (arg1 === 'underlag-LUAEFS') {
-            element(by.css('span[stat-key="luae_fs.validation.underlag.date.incorrect_format"]')).getText().then(function(text) {
-                return expect(text).to.contain(arg2);
-            });
-        } else if (arg1 === 'underlag-LUAENA') {
-            element(by.css('span[stat-key="luae_na.validation.underlag.date.incorrect_format"]')).getText().then(function(text) {
-                return expect(text).to.contain(arg2);
-            });
-        } else if (arg1 === 'datum') {
-            element(by.css('span[key="common.validation.date.INVALID_FORMAT"]')).getText().then(function(text) {
-                return expect(text).to.contain(arg2);
-            });
-        } else if (arg1 === 'kännedom') {
-            element(by.css('span[key="common.validation.singleDate.INVALID_FORMAT"]')).getText().then(function(text) {
-                return expect(text).to.contain(arg2);
-            });
-        } else if (arg1 === 'arbetsförmåga') {
-            element(by.css('span[key="common.validation.date-period.INVALID_FORMAT"]')).getText().then(function(text) {
-                return expect(text).to.contain(arg2);
-            });
+    //     if (arg1 === 'postnummer') {
+    //         element(by.css('span[stat-key="common.validation.postnummer.incorrect-format"]')).getText().then(function(text) {
+    //             return expect(text).to.contain(arg2);
+    //         });
+    //     } else if (arg1 === 'underlag') {
+    //         element(by.css('span[stat-key="luse.validation.underlag.date.incorrect_format"]')).getText().then(function(text) {
+    //             return expect(text).to.contain(arg2);
+    //         });
+    //     } else if (arg1 === 'underlag-LUAEFS') {
+    //         element(by.css('span[stat-key="luae_fs.validation.underlag.date.incorrect_format"]')).getText().then(function(text) {
+    //             return expect(text).to.contain(arg2);
+    //         });
+    //     } else if (arg1 === 'underlag-LUAENA') {
+    //         element(by.css('span[stat-key="luae_na.validation.underlag.date.incorrect_format"]')).getText().then(function(text) {
+    //             return expect(text).to.contain(arg2);
+    //         });
+    //     } else if (arg1 === 'datum') {
+    //         element(by.css('span[key="common.validation.date.INVALID_FORMAT"]')).getText().then(function(text) {
+    //             return expect(text).to.contain(arg2);
+    //         });
+    //     } else if (arg1 === 'kännedom') {
+    //         element(by.css('span[key="common.validation.singleDate.INVALID_FORMAT"]')).getText().then(function(text) {
+    //             return expect(text).to.contain(arg2);
+    //         });
+    //     } else if (arg1 === 'arbetsförmåga') {
+    //         element(by.css('span[key="common.validation.date-period.INVALID_FORMAT"]')).getText().then(function(text) {
+    //             return expect(text).to.contain(arg2);
+    //         });
 
+    //     } else {
+    //         return;
+    //     }
+
+    this.Given(/^jag fyller i text i "([^"]*)" fältet$/, function(fieldtype) {
+        var isSMIIntyg = helpers.isSMIIntyg(intyg.typ);
+
+
+        var date = helpers.randomTextString().substring(0, 4);
+
+        if (isSMIIntyg && fieldtype === 'kännedom-datum') {
+            return luseUtkastPage.baseratPa.kannedomOmPatient.datum.sendKeys(date);
+        } else if (isSMIIntyg && fieldtype === 'slumpat-datum') {
+            return testdataHelpers.shuffle(populateFieldArray(luseUtkastPage.baseratPa, ['anhorigBeskrivning', 'kannedomOmPatient']))[0].datum.sendKeys(date);
+
+        } else if (isSMIIntyg && fieldtype === 'underlag-datum') {
+            luseUtkastPage.andraMedicinskaUtredningar.finns.JA.sendKeys(protractor.Key.SPACE);
+            return testdataHelpers.shuffle(populateFieldArray(luseUtkastPage.underlag))[0].datum.sendKeys(date);
+
+        } else if (isSMIIntyg && fieldtype === 'postnummer') {
+            return luseUtkastPage.enhetensAdress.postNummer.sendKeys(date);
+        } else if (isSMIIntyg && fieldtype === 'arbetsförmåga-datum') {
+            var arbetsfarmagaProcent = testdataHelpers.shuffle(populateFieldArray(lisjpUtkastPage.sjukskrivning, anhorigIgnoreKeys))[0];
+            return testdataHelpers.shuffle([arbetsfarmagaProcent.fran, arbetsfarmagaProcent.till])[0].sendKeys(date);
         } else {
-            return;
+            return fkUtkastPage.diagnosKod.sendKeys(date);
         }
 
-
     });
+
+
+    // });
     this.Given(/^jag raderar ett  slumpat obligatoriskt fält$/, function(callback) {
 
     });
