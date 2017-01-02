5d49df77457cde1fc2fefcc7658bec8293e2b848
Mikaela Nöteberg <mnoteberg@gmail.com>
2017-01-02 12:18:58 +0100

    DEVENV: Uppdaterar license-header

--- log-sender/src/main/java/se/inera/intyg/webcert/logsender/exception/LoggtjanstExecutionException.java
+++ log-sender/src/main/java/se/inera/intyg/webcert/logsender/exception/LoggtjanstExecutionException.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016 Inera AB (http://www.inera.se)
+ * Copyright (C) 2017 Inera AB (http://www.inera.se)
  *
  * This file is part of sklintyg (https://github.com/sklintyg).
  *
@@ -16,7 +16,6 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 package se.inera.intyg.webcert.logsender.exception;
 
 /**
