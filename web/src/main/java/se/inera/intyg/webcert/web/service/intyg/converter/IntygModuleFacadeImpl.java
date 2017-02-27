04f907b2547ed2fccfc52f305d28be0b35170a1c
Karl Johan Andreasson <kalle.kaan@gmail.com>
2017-02-27 10:42:34 +0100

    DEVENV: Checkstyle fix

--- web/src/main/java/se/inera/intyg/webcert/web/service/intyg/converter/IntygModuleFacadeImpl.java
+++ web/src/main/java/se/inera/intyg/webcert/web/service/intyg/converter/IntygModuleFacadeImpl.java
@@ -22,7 +22,6 @@ import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
 
-import autovalue.shaded.com.google.common.common.collect.Lists;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
