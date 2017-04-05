b7703c09611e33dbcde0e36aa7e09a77b4774172
Martin Pring <martin.pring@learningwell.se>
2017-04-05 14:14:13 +0200

    ACCTEST: Ersätt intyg länk test

--- test/acceptance/features/ersatt.feature
+++ test/acceptance/features/ersatt.feature
@@ -21,3 +21,12 @@ Scenario: När man klickat på ersätta-knappen så ska följande text visas
     Och jag signerar intyget
 	Och klickar på ersätta knappen
 	Så ska meddelandet som visas innehålla texten "Ett intyg kan ersättas om det innehåller felaktiga uppgifter eller om ny information tillkommit efter att intyget utfärdades."
+
+@ersatt-intyg-lank
+Scenario: När man ersatt intyg så ska informationstext på ett ersatt intyg finnas
+	När jag går in på att skapa ett slumpat intyg
+	Och jag fyller i alla nödvändiga fält för intyget
+	Och jag signerar intyget
+	Och klickar på ersätta knappen och ersätter intyget
+	Och gå tillbaka till det ersatta intyget
+	Så ska jag se en texten "Intyget har ersatts av detta intyg" som innehåller en länk till det ersatta intyget
--- test/acceptance/features/steps/ersatt.js
+++ test/acceptance/features/steps/ersatt.js
@@ -17,10 +17,27 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-/* globals protractor*/
+/* globals protractor, intyg, browser, Promise, logger */
 
 'use strict';
 
+function intygURL(typAvIntyg, intygId) {
+    if (typAvIntyg === 'Läkarutlåtande för sjukersättning') {
+        return Promise.resolve(process.env.WEBCERT_URL + 'web/dashboard#/intyg/luse/' + global.intyg.id);
+    } else if (typAvIntyg === 'Läkarintyg för sjukpenning') {
+        return Promise.resolve(process.env.WEBCERT_URL + 'web/dashboard#/intyg/lisjp/' + global.intyg.id);
+    } else if (typAvIntyg === 'Läkarutlåtande för aktivitetsersättning vid förlängd skolgång') {
+        return Promise.resolve(process.env.WEBCERT_URL + 'web/dashboard#/intyg/luae_fs/' + global.intyg.id);
+    } else if (typAvIntyg === 'Läkarutlåtande för aktivitetsersättning vid nedsatt arbetsförmåga') {
+        return Promise.resolve(process.env.WEBCERT_URL + 'web/dashboard#/intyg/luae_na/' + global.intyg.id);
+    } else if (typAvIntyg === 'Läkarintyg FK 7263') {
+        return Promise.resolve(process.env.WEBCERT_URL + 'web/dashboard#/intyg/fk7263/' + global.intyg.id);
+    } else if (typAvIntyg === 'Transportstyrelsens läkarintyg, diabetes') {
+        return Promise.resolve(process.env.WEBCERT_URL + 'web/dashboard#/intyg/ts-diabetes/' + global.intyg.id);
+    } else if (typAvIntyg === 'Transportstyrelsens läkarintyg') {
+        return Promise.resolve(process.env.WEBCERT_URL + 'web/dashboard#/intyg/ts-bas/' + global.intyg.id);
+    }
+}
 
 module.exports = function() {
 
@@ -30,6 +47,32 @@ module.exports = function() {
 
     this.Given(/^klickar på ersätta knappen$/, function() {
         return element(by.id('ersattBtn')).sendKeys(protractor.Key.SPACE);
+    });
+
+    this.Given(/^klickar på ersätta knappen och ersätter intyget$/, function() {
+        global.ersattintyg = {};
+        global.ersattintyg.id = intyg.id;
+        global.ersattintyg.typ = intyg.typ;
+
+        return element(by.id('ersattBtn')).sendKeys(protractor.Key.SPACE).then(function() {
+            return element(by.id('button1ersatt-dialog')).sendKeys(protractor.Key.SPACE).then(function() {
+                logger.info('Clicked ersätt button');
+            });
+        });
+    });
+
+    this.Given(/^gå tillbaka till det ersatta intyget$/, function() {
+        return browser.sleep(4000).then(function() {
+            return intygURL(global.ersattintyg.typ, global.ersattintyg.id).then(function(url) {
+                return browser.get(url).then(function() {
+                    logger.info('Går till url: ' + url);
+                });
+            });
+        });
+    });
+
+    this.Given(/^ska jag se en texten "([^"]*)" som innehåller en länk till det ersatta intyget$/, function(replacedMessage) {
+        return expect(element(by.id('wc-intyg-replaced-message')).getText()).to.eventually.contain(replacedMessage);
     });
 
     this.Given(/^ska meddelandet som visas innehålla texten "([^"]*)"$/, function(modalMsg) {
