fb53298c63454448dd70739eb950c6b966265787
Marcus Cedergren <marcus.cedergren@callistaenterprise.se>
2017-06-07 15:13:44 +0200

    INTYG-4044: Lade till ett enhetstest av tidigareintygFilter

---
+++ web/src/main/webapp/app/views/sokSkrivIntyg/tidigareIntyg.filter.spec.js
@@ -0,0 +1,55 @@
+describe('Filter: TidigareIntygFilter', function() {
+    'use strict';
+
+    var _filter;
+
+    var ersatt = {
+        status: 'SIGNED',
+        relations: {
+            children: [ {
+                relationKod: 'ERSATT'
+            } ]
+        }
+    };
+
+    var kompletterat = {
+        status: 'SIGNED',
+        relations: {
+            children: [ {
+                relationKod: 'KOMPLT'
+            } ]
+        }
+    };
+
+    var revoked = {
+        status: 'CANCELLED'
+    };
+
+    var normal = {
+        status: 'SIGNED'
+    };
+
+    var list = [ ersatt, kompletterat, revoked, normal ];
+
+    beforeEach(angular.mock.module('webcert'));
+
+    beforeEach(inject(function(_TidigareIntygFilterFilter_) {
+        _filter = _TidigareIntygFilterFilter_;
+    }));
+
+    it('using "all" should return all intyg', function() {
+        expect(_filter(list, 'all').length).toBe(4);
+        expect(_filter(list, 'all')).toContain(ersatt, kompletterat, revoked, normal);
+    });
+
+    it('using "revoked" should not return normal intyg', function() {
+        expect(_filter(list, 'revoked').length).toBe(3);
+        expect(_filter(list, 'revoked')).toContain(ersatt, kompletterat, revoked);
+    });
+
+    it('using "current" should only return normal intyg', function() {
+        expect(_filter(list, 'current').length).toBe(1);
+        expect(_filter(list, 'current')).toContain(normal);
+    });
+
+});
