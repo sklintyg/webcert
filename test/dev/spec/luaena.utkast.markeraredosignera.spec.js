48d90d1c53dec9e107bb10a3c701c9710a429f08
Martin Hesslund <martin.hesslund@callistaenterprise.se>
2017-06-30 15:53:54 +0200

    BUILDENV: Tagit bort fdescribe

--- test/dev/spec/luaena.utkast.markeraredosignera.spec.js
+++ test/dev/spec/luaena.utkast.markeraredosignera.spec.js
@@ -25,7 +25,7 @@ var testdataHelper = wcTestTools.helpers.restTestdata;
 var UtkastPage = wcTestTools.pages.intyg.luaeNA.utkast;
 var restUtil = wcTestTools.restUtil;
 
-fdescribe('Create partially complete luae_na utkast and mark as ready to sign', function() {
+describe('Create partially complete luae_na utkast and mark as ready to sign', function() {
 
     var utkastId = null,
         data = null;
