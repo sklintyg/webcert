26a09a2440842ab3e02c5b96cc6c3627d300b7af
Martin Hesslund <martin.hesslund@callistaenterprise.se>
2017-08-01 15:21:44 +0200

    INTYG-4092: Fixat fel i ts-bas

--- test/webcertTestTools/pages/intyg/ts/ts_bas/tsBas.intyg.page.js
+++ test/webcertTestTools/pages/intyg/ts/ts_bas/tsBas.intyg.page.js
@@ -108,14 +108,14 @@ var TsBasIntyg = TsBaseIntyg._extend({
     },
     verifieraHorsel: function(horsel) {
         expect(this.horselBalansbalansrubbningar.getText()).toBe(horsel.yrsel);
-        expect(this.horselSamtal.getText()).toBe(horsel.samtal);
+        expect(this.horselSamtal.getText()).toBe(horsel.samtal ? horsel.samtal : 'Nej');
     },
     verifieraRorelseorganensFunktioner: function(rorelseorganensFunktioner) {
         expect(this.funktionsnedsattning.getText()).toBe(rorelseorganensFunktioner.nedsattning);
         if (rorelseorganensFunktioner.nedsattning === 'Ja') {
             expect(this.funktionsnedsattningbeskrivning.getText()).toBe(rorelseorganensFunktioner.nedsattningBeskrivning);
         }
-        expect(this.funktionsnedsRorelseformaga.getText()).toBe(rorelseorganensFunktioner.inUtUrFordon);
+        expect(this.funktionsnedsRorelseformaga.getText()).toBe(rorelseorganensFunktioner.inUtUrFordon ? rorelseorganensFunktioner.inUtUrFordon : 'Nej');
     },
     verifieraHjartOchKarlsjukdomar: function(data) {
         expect(this.hjartKarlSjukdom.getText()).toBe(data.hjartHjarna);
