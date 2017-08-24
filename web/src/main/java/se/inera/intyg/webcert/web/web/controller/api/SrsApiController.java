04335e52a5cbe30bafa3b4ab7b5767e22c3d357a
Erik Forsgren <erik.forsgren@r2m.se>
2017-08-24 13:05:15 +0200

    INTYG-4328: Hantera statistik i SrsApiController

--- web/src/main/java/se/inera/intyg/webcert/web/web/controller/api/SrsApiController.java
+++ web/src/main/java/se/inera/intyg/webcert/web/web/controller/api/SrsApiController.java
@@ -24,6 +24,7 @@ import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
+import se.inera.intyg.clinicalprocess.healthcond.srs.getsrsinformation.v1.Utdatafilter;
 import se.inera.intyg.infra.integration.srs.model.SrsException;
 import se.inera.intyg.infra.integration.srs.model.SrsResponse;
 import se.inera.intyg.infra.integration.srs.services.SrsService;
@@ -31,10 +32,7 @@ import se.inera.intyg.schemas.contract.InvalidPersonNummerException;
 import se.inera.intyg.schemas.contract.Personnummer;
 import se.inera.intyg.webcert.web.web.controller.AbstractApiController;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -59,16 +57,30 @@ public class SrsApiController extends AbstractApiController {
             @ApiResponse(code = NO_CONTENT, message = "No prediction model found")
     })
     public Response getSrs(@ApiParam(value = "Personnummer", required = true) @PathParam("personnummer") String personnummer,
-            @ApiParam(value = "Diagnosis Code", required = true) @PathParam("diagnosisCode") String diagnosisCode) {
+                           @ApiParam(value = "Diagnosis Code", required = true) @PathParam("diagnosisCode") String diagnosisCode,
+                           @ApiParam(value = "Utdatafilter: Prediktion") @QueryParam("isPrediktion") @DefaultValue("false") boolean isPrediktion,
+                           @ApiParam(value = "Utdatafilter: AtgardRekommendation") @QueryParam("isAtgard") @DefaultValue("false") boolean isAtgard,
+                           @ApiParam(value = "Utdatafilter: FmbInformation") @QueryParam("isFmbInfo") @DefaultValue("false") boolean isFmbInfo,
+                           @ApiParam(value = "Utdatafilter: Statistik") @QueryParam("isStatistik") @DefaultValue("false") boolean isStatistik) {
         if (personnummer == null || personnummer.isEmpty() || diagnosisCode == null || diagnosisCode.isEmpty()) {
             return Response.status(Response.Status.BAD_REQUEST).entity("Bad request").build();
         }
         try {
-            return Response.ok(srsService.getSrs(new Personnummer(personnummer), diagnosisCode)).build();
+            Utdatafilter filter = buildUtdatafilter(isPrediktion, isAtgard, isFmbInfo, isStatistik);
+            return Response.ok(srsService.getSrs(new Personnummer(personnummer), diagnosisCode, filter)).build();
         } catch (InvalidPersonNummerException e) {
             return Response.status(Response.Status.BAD_REQUEST).entity("Faulty personnummer").build();
         } catch (SrsException e) {
             return Response.status(Response.Status.NO_CONTENT).entity("No prediction model found for diagnosis code " + diagnosisCode).build();
         }
+    }
+
+    private Utdatafilter buildUtdatafilter(boolean isPrediktion, boolean isAtgard, boolean isFmbInfo, boolean isStatistik) {
+        Utdatafilter filter = new Utdatafilter();
+        filter.setPrediktion(isPrediktion);
+        filter.setAtgardsrekommendation(isAtgard);
+        filter.setFmbinformation(isFmbInfo);
+        filter.setStatistik(isStatistik);
+        return filter;
     }
 }
