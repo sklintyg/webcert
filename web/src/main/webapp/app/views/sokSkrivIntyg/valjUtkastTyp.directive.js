e4147eae078f6ece0dc1a544e4c9c370e0fdfaa8
Kebire Breitholtz <kebire.breitholtz@inera.se>
2017-12-04 14:29:45 +0100

    INTYG-2998: brutit ut innehållet i sökSkrivIntyg till direktiv

---
+++ web/src/main/webapp/app/views/sokSkrivIntyg/valjUtkastTyp.directive.js
@@ -0,0 +1,10 @@
+angular.module('webcert').directive('valjUtkastTyp',
+    function() {
+        'use strict';
+
+        return {
+            restrict: 'E',
+            templateUrl: '/app/views/sokSkrivIntyg/valjUtkastTyp.directive.html'
+        }
+    }
+);
