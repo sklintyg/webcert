287c923a63e07bb0db2eefd46dd7a452a852a251
an3ll <jocke.anell@gmail.com>
2018-03-16 15:57:01 +0100

    INTYG-5737: imports fixed

--- web/src/main/java/se/inera/intyg/webcert/web/service/arende/ArendeServiceImpl.java
+++ web/src/main/java/se/inera/intyg/webcert/web/service/arende/ArendeServiceImpl.java
@@ -76,7 +76,14 @@ import se.riv.clinicalprocess.healthcond.certificate.sendMessageToRecipient.v2.S
 import javax.xml.bind.JAXBException;
 import java.time.Clock;
 import java.time.LocalDateTime;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
