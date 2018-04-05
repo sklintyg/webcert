1df8cf7145623e7352342842529083d5f5712d16
Martin Hesslund <martin.hesslund@callistaenterprise.se>
2018-04-05 16:52:49 +0200

    INTYG-5927: Backat karma version

--- web/karma.conf.js
+++ web/karma.conf.js
@@ -88,9 +88,6 @@ module.exports = function(config) {
         // enable / disable watching file and executing tests whenever any file changes
         autoWatch: true,
 
-        // Run specs in semi-random order
-        random: false,
-
         // Start these browsers, currently available:
         // - Chrome
         // - ChromeCanary
