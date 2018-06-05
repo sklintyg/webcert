e7050263a02f0aceda3d620cebfac6798a9030c6
Ulf Petersson <ulf@winosa.se>
2018-06-05 12:46:01 +0200

    INTYG-5788: Datumformat: åååå-mm-dd

--- web/src/main/webapp/app/views/messages.js
+++ web/src/main/webapp/app/views/messages.js
@@ -57,8 +57,8 @@ angular.module('webcert').constant('webcert.messages', {
         'label.qacheckhanterad.tillbaka': 'Tillbaka',
 
         // validation messages
-        'validation.invalidfromdate': 'Från-datum är felaktigt. Använd formatet ÅÅÅÅ-MM-DD.',
-        'validation.invalidtodate': 'Till-datum är felaktigt. Använd formatet ÅÅÅÅ-MM-DD.',
+        'validation.invalidfromdate': 'Från-datum är felaktigt. Använd formatet åååå-mm-dd.',
+        'validation.invalidtodate': 'Till-datum är felaktigt. Använd formatet åååå-mm-dd.',
         'validation.invalidtobeforefromdate': 'Till-datum är före från-datum.',
 
         //info messages
