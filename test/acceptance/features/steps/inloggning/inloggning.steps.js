48b46fa30a86a26f7b639f6b4e3fea8ea286bad4
Markus Lundin <m.lundin.1986@gmail.com>
2018-06-29 16:21:13 +0200

    ACCTEST: Slå av silly loggning

--- test/acceptance/features/steps/inloggning/inloggning.steps.js
+++ test/acceptance/features/steps/inloggning/inloggning.steps.js
@@ -138,8 +138,6 @@ Then(/^jag går in på (?:"([^"]*)" )?testpatienten för "([^"]*)"$/, function(i
     let patienter = wcTestTools.testdata.values.dedikeradeTestPatienter.medSyfte(testSyfte);
 
     this.patient = patienter[helpers.getIntFromTxt(index || 'första')];
-
-    console.log(this.patient);
     return gotoPatient(this.patient, this.user);
 });
 
