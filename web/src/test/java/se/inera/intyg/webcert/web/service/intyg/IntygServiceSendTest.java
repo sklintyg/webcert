7fe9ff15830e533bcf11b3c8d66b46d68b529e56
HannahLoranga <hannah.borjesson@gmail.com>
2018-09-18 12:45:16 +0200

    INTYG-7048: Ändrade ett test

--- web/src/test/java/se/inera/intyg/webcert/web/service/intyg/IntygServiceSendTest.java
+++ web/src/test/java/se/inera/intyg/webcert/web/service/intyg/IntygServiceSendTest.java
@@ -141,7 +141,7 @@ public class IntygServiceSendTest extends AbstractIntygServiceTest {
         certificateResponse = new CertificateResponse(json, utlatande, metaData, false);
 
         WebcertCertificateRelation ersattRelation = new WebcertCertificateRelation(INTYG_ID, RelationKod.ERSATT, LocalDateTime.now(),
-                UtkastStatus.SIGNED);
+                UtkastStatus.SIGNED, null);
 
         when(patientDetailsResolver.getSekretessStatus(any(Personnummer.class))).thenReturn(SekretessStatus.FALSE);
         when(webCertUserService.isAuthorizedForUnit(anyString(), anyString(), anyBoolean())).thenReturn(true);
