697f13f0dd919865da9c20b0c8be0cb64ea6e5cc
Carl Frendin <carl.frendin@inera.se>
2018-10-25 11:03:17 +0200

    INTYG-5888: csrf-skydd aktiverat för prod och wc-security-test
    Testat och fungerar för fake-login med profile wc-security-test.

---
+++ web/src/main/java/se/inera/intyg/webcert/web/security/CreateCsrfTokenFilter.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) 2018 Inera AB (http://www.inera.se)
+ *
+ * This file is part of sklintyg (https://github.com/sklintyg).
+ *
+ * sklintyg is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * sklintyg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package se.inera.intyg.webcert.web.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class CreateCsrfTokenFilter extends OncePerRequestFilter {
+
+    private static final String ANGULARJS_CSRF_COOKIE_NAME = "XSRF-TOKEN";
+
+    @Autowired
+    private CookieCsrfTokenRepository csrfTokenRepository;
+
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        Cookie csrfCookie = new Cookie(ANGULARJS_CSRF_COOKIE_NAME, csrfTokenRepository.generateToken(request).getToken());
+        response.addCookie(csrfCookie);
+
+        // Proceed with other filters
+        filterChain.doFilter(request, response);
+    }
+}
