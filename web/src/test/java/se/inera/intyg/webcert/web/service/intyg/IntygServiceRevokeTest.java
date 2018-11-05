e179f73bef4f1f57aac1026f1801c301ae375de1
Joy Zomborszki <joy.zomborszki@inera.se>
2018-11-05 08:37:12 +0100

    Revert "INTYG-7449: Visa namn- och adressuppgifter från det signerade intyget."
    This reverts commit cd500a355f955b9d16c9129aed0ff4ea7b28d958.

--- web/src/test/java/se/inera/intyg/webcert/web/service/intyg/IntygServiceRevokeTest.java
+++ web/src/test/java/se/inera/intyg/webcert/web/service/intyg/IntygServiceRevokeTest.java
@@ -100,6 +100,7 @@ public class IntygServiceRevokeTest extends AbstractIntygServiceTest {
         when(patientDetailsResolver.resolvePatient(any(Personnummer.class), anyString())).thenReturn(buildPatient(false, false));
         when(moduleRegistry.getModuleApi(anyString(), anyString())).thenReturn(moduleApi);
         when(moduleApi.getUtlatandeFromJson(anyString())).thenReturn(new Fk7263Utlatande());
+        when(moduleApi.updateBeforeSave(anyString(), any(Patient.class))).thenReturn("MODEL");
     }
 
     @Before
