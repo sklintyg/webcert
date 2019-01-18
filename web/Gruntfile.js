782dbe261aae7d3b6d12ebaa582474d2301e25b7
Matias Wurschmidt-Wang <matias.wurschmidt@soprasteria.com>
2019-01-18 14:06:10 +0100

    INTYG-7052: Lagt till nytt intyg för ADHD (TSTRK1062).

--- web/Gruntfile.js
+++ web/Gruntfile.js
@@ -60,6 +60,7 @@ module.exports = function(grunt) {
         'fk7263':      { base: 'common/fk7263' },
         'ts-bas':      { base: 'common/ts/ts-bas' },
         'ts-diabetes': { base: 'common/ts/ts-diabetes' },
+        'ts-tstrk1062': { base: 'common/ts/ts-tstrk1062' },
         'luse':        { base: 'common/fk/luse', angularModule:'luse' },
         'lisjp':       { base: 'common/fk/lisjp', angularModule:'lisjp' },
         'luae_na':     { base: 'common/fk/luae_na', angularModule:'luae_na' },
