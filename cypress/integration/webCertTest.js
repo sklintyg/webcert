215c8f497fb6f0dcdeb840eee5dd0626ba0fb020
Johannes Larsson <johannes.larsson@nordicmedtest.se>
2019-02-05 16:10:49 +0100

    TESTENV: Flytta ut statisk data till fixture

--- cypress/integration/webCertTest.js
+++ cypress/integration/webCertTest.js
@@ -46,7 +46,7 @@ describe('Creating and signing a max filled LISJP and sending it to FK', functio
 			cy.contains('cookies').should('not.exist');
 		});
 
-		// Skapa alias för "Fortsätt"-knappen
+		// Alias för "Fortsätt"-knappen
 		cy.contains(this.valjPatient.fortsattKnappText).as('continueBtn');
 
 		cy.get('@continueBtn').should('be.disabled');
@@ -155,41 +155,41 @@ describe('Creating and signing a max filled LISJP and sending it to FK', functio
 		// ----- 'Medicinsk behandling' ----- //
 		cy.contains(this.lisjpData.medicinskaBehandlingar).parent().parent().parent().within(($form) => {
 			cy.wrap($form).find('textarea')
-			.type(this.lisjpData.behandlingsBeskrivning);
+			.type(this.lisjpData.medicinskaBehandlingarBeskrivning);
 		});
 
-		cy.contains('Planerade medicinska behandlingar/åtgärder.').parent().parent().parent().within(($form) => {
+		cy.contains(this.lisjpData.planeradeBehandlingar).parent().parent().parent().within(($form) => {
 			cy.wrap($form).find('textarea')
-			.type('Specialist, ultraljudskontroller');
+			.type(this.lisjpData.planeradeBehandlingarBeskrivning);
 		});
 
 		// ----- 'Bedömning' -----//
-		cy.contains('Min bedömning av patientens nedsättning av arbetsförmågan').parent().parent().parent().within(($form) => {
+		cy.contains(this.lisjpData.arbetsförmågaBedömning).parent().parent().parent().within(($form) => {
 			cy.get('[type="checkbox"]').within(($checkboxes) => {
-				cy.wrap($checkboxes).check(); // Will check all checkboxes in this section. The "from" date field will get today's date
+				cy.wrap($checkboxes).check(); // Klickar i alla checkboxar i sektionen. "Från"-datum blir dagens datum
 
-				cy.wrap($checkboxes.eq(0)).parent().parent().parent().parent().within(($row) => {  // Get row with first checkbox (i.e. 25% sjukskrivning)
+				cy.wrap($checkboxes.eq(0)).parent().parent().parent().parent().within(($row) => { // Hämta raden med 25% sjukskrivning
 					cy.get('[type="text"]').then(($textFields) => {
 						cy.wrap($textFields.eq(0)).clear().type(this.todayPlus1);
 						cy.wrap($textFields.eq(1)).clear().type(this.todayPlus11);
 					});
 				});
 
-				cy.wrap($checkboxes.eq(1)).parent().parent().parent().parent().within(($row) => {  // Get row with second checkbox (i.e. 50% sjukskrivning)
+				cy.wrap($checkboxes.eq(1)).parent().parent().parent().parent().within(($row) => { // Hämta raden med 50% sjukskrivning
 					cy.get('[type="text"]').then(($textFields) => {
 						cy.wrap($textFields.eq(0)).clear().type(this.todayPlus12);
 						cy.wrap($textFields.eq(1)).clear().type(this.todayPlus19);
 					});
 				});
 
-				cy.wrap($checkboxes.eq(2)).parent().parent().parent().parent().within(($row) => {  // Get row with third checkbox (i.e. 75% sjukskrivning)
+				cy.wrap($checkboxes.eq(2)).parent().parent().parent().parent().within(($row) => { // Hämta raden med 75% sjukskrivning
 					cy.get('[type="text"]').then(($textFields) => {
 						cy.wrap($textFields.eq(0)).clear().type(this.todayPlus20);
 						cy.wrap($textFields.eq(1)).clear().type(this.todayPlus28);
 					});
 				});
 
-				cy.wrap($checkboxes.eq(3)).parent().parent().parent().parent().within(($row) => {  // Get row with fourth checkbox (i.e. 100% sjukskrivning)
+				cy.wrap($checkboxes.eq(3)).parent().parent().parent().parent().within(($row) => { // Hämta raden med 100% sjukskrivning
 					cy.get('[type="text"]').then(($textFields) => {
 						cy.wrap($textFields.eq(0)).clear().type(this.todayPlus29);
 						cy.wrap($textFields.eq(1)).clear().type(this.todayPlus41);
@@ -199,31 +199,31 @@ describe('Creating and signing a max filled LISJP and sending it to FK', functio
 		});
 
 		cy
-		.contains('Patientens arbetsförmåga bedöms nedsatt längre tid än den som Socialstyrelsens försäkringsmedicinska beslutsstöd anger, därför att')
+		.contains(this.lisjpData.längreNedsattArbetsförmåga)
 		.parent().parent().parent()
-		.find('textarea').type('Haft tidigare bäckeninsuffiens vid graviditet, haft tidigare missfall och kräver nu fler kontroller och avlastning');
+		.find('textarea').type(this.lisjpData.längreNedsattArbetsförmågaText);
 
-		cy.contains('Finns det medicinska skäl att förlägga arbetstiden på något annat sätt än att minska arbetstiden lika mycket varje dag?')
+		cy.contains(this.lisjpData.förläggaArbetstidAnnorlunda)
 		.parent().parent().parent().within(($elem) => {
-			cy.get('[type="radio"]').eq(0).check(); // The first radio button is 'YES'
+			cy.get('[type="radio"]').eq(0).check(); // Första radioknappen är "Ja"
 		});
 
-		cy.contains('Beskriv medicinska skäl till annan förläggning av arbetstiden').parent().parent().parent().find('textarea')
-		.type('Har lättare kontorsarbete på måndagar som hon klarar av med nuvarande besvär om ingen försämring sker.');
+		cy.contains(this.lisjpData.arbetstidAnnorlundaMedicinskaSkäl).parent().parent().parent().find('textarea')
+		.type(this.lisjpData.arbetstidAnnorlundaMedicinskaSkälBeskrivning);
 
-		cy.contains('Resor till och från arbetet med annat färdmedel än normalt kan göra det möjligt för patienten att återgå till arbetet under sjukskrivningsperioden.')
+		cy.contains(this.lisjpData.resaMöjliggörArbete)
 		.parent().parent().within(() => {
 			cy.get('[type="checkbox"]').check();
 		});
 
-		cy.contains('Prognos för arbetsförmåga utifrån aktuellt undersökningstillfälle').parent().parent().parent().within(($ele) => {
-			cy.get('[type="radio"]').eq(0).check() // The wanted radio button is the top one
+		cy.contains(this.lisjpData.arbetsförmågaPrognos).parent().parent().parent().within(($ele) => {
+			cy.get('[type="radio"]').eq(0).check() // Översta radioknappen är den som ska anges
 		});
 
 		// ----- 'Åtgärder' -----//
-		cy.contains('Här kan du ange åtgärder som du tror skulle göra det lättare för patienten att återgå i arbete').parent().parent().parent().within(($elem) => {
+		cy.contains(this.lisjpData.föreslåÅtgärder).parent().parent().parent().within(($elem) => {
 			cy.get('[type="checkbox"]').each(($el, index, $list) => {
-				if (index != 0) { // Index 0 is "Inte aktuellt". This checkbox should NOT be checked
+				if (index != 0) { // Index 0 är "Inte aktuellt", detta är den enda checkboxen som INTE ska anges
 					cy.wrap($el).check();
 				}
 			});
@@ -231,41 +231,48 @@ describe('Creating and signing a max filled LISJP and sending it to FK', functio
 
 		cy.wait(3000);
 
-		cy.contains('Här kan du ange fler åtgärder. Du kan också beskriva hur åtgärderna kan underlätta återgång i arbete.').should('be.visible').parent().parent().parent().find('textarea')
-		.type('Om anpassningar till lättare uppgifter utan tunga lyft och mycket gående kan ordnas kvarstår arbetsförmåga under en tid framöver.');
+		cy.contains(this.lisjpData.flerÅtgärder).should('be.visible').parent().parent().parent().find('textarea')
+		.type(this.lisjpData.flerÅtgärderBeskrivning);
 
 		// ----- 'Övriga upplysningar' -----//
 		//cy.contains('Övriga upplysningar').parent().parent().parent().find('textarea').type('Planerad partus ' + this.todayPlus41);
-		cy.get('[name="ovrigt"]').type('Planerad partus ' + this.todayPlus41); 	// Temporary workaround. Using attribute 'name' since the textarea
-																				// heading is present in more than one place
+		cy.get('[name="ovrigt"]').type(this.lisjpData.övrigaUpplysningarBeskrivning + this.todayPlus41); 	// Tillfällig lösning. Använder attribut 'name' eftersom textareans rubrik finns på fler än ett ställe
+
 		// ----- 'Kontakt' -----//
-		cy.contains('Jag önskar att Försäkringskassan kontaktar mig.').parent().parent().parent().within(($elem) => {
+		cy.contains(this.lisjpData.kontaktaMig).parent().parent().parent().within(($elem) => {
 			cy.get('[type="checkbox"]').check();
 		});
 
-		cy.contains('Ange gärna varför du vill ha kontakt.').parent().parent().parent().find('textarea')
-			.type('För att diskutera sjukskrivningsperiod och tydliggörande kring besvärsbilden samt framtida arbetsförmåga.');
+		cy.contains(this.lisjpData.anledningKontakt).parent().parent().parent().find('textarea')
+			.type(this.lisjpData.anledningKontaktBeskrivning);
 
 		// ----- 'Signera intyg' -----//
 
 		// Lägger till en paus här för att se om det är så att Cypress klickar för snabbt på Signera-knappen. I Jenkins
 		// failar testfallet ofta p.g.a att det dyker upp en modal som säger att intyger har ändrats av annan person (men personen som
 		// vill signera intyget är samma som anges som den som har ändrat det)
+		// Just nu verkar testfallet gå igenom men frågan är om detta har något med saken att göra eller om det fungerar bara för att jag bytte till
+		// ny slav på Jenkins (det förkortade exekveringstiden till hälften ungefär)
 		cy.wait(6000);
 
-		// On battery and Wifi, Cypress too often says that it presses the button but it doesn't
-		cy.contains('button', 'Signera intyget', {timeout: 15000}).should('be.enabled').then(($button) => {
-			cy.wait(1000); // This is a workaround when running on battery and wifi.... Cypress claims to click but nothing happen. Trying with a wait
+		// Om laptop kör på batteri och Wifi så visar Cypress att knappen trycks in,
+		// men ofta händer inget
+		cy.contains('button', this.lisjpData.signeraKnappText, {timeout: 15000})
+		.should('be.enabled')
+		.then(($button) => {
+			cy.wait(1000);
 			cy.wrap($button).click(); // prova att ändra till <button?
 		});
 
-		// Paus här också, precis som ovanför "Signera intyg"-knappen. Åtgärda om möjligt
+		// Paus här också, precis som ovanför "Signera intyg"-knappen. Behövs den nu när vi kör på snabbare slav?
 		cy.wait(6000);
 
-		cy.contains('div', 'Välj vilka intygsmottagare invånaren kan skicka intyget till via Mina intyg.', {timeout: 20000}).should('be.visible').within(($elem) => {
-			cy.get('[type="radio"]').each(($el, index, $list) => { // Just picking out all radio buttons... this can be improved
-				if (index >= 2) { // Index 0 and 1 is "Försäkringskassan" 'Ja' and 'Nej'. Skip those.
-					if (index % 2 != 0) { // Only check odd numbers since those are "Nej"
+		cy.contains('div', this.lisjpData.väljIntygsmottagare, {timeout: 20000})
+		.should('be.visible')
+		.within(($elem) => {
+			cy.get('[type="radio"]').each(($el, index, $list) => { // Hämtar ut samtliga radioknappar... kan förbättras
+				if (index >= 2) { // Index 0 och 1 är "Försäkringskassan" 'Ja' och 'Nej'. Hoppas över.
+					if (index % 2 != 0) { // Klicka endast i udda nummer eftersom dessa är "Nej"
 						cy.wrap($el).check();
 					}
 				}
@@ -274,15 +281,20 @@ describe('Creating and signing a max filled LISJP and sending it to FK', functio
 
 		cy.get('[name="approveForm"]').within(($form) => {
 			cy.get('[type="button"]').then(($button) => {
-				cy.wrap($button).contains('Spara').should('be.visible');
+				cy.wrap($button).contains(this.lisjpData.intygsmottagareKnappText).should('be.visible');
 				cy.wrap($button).click({force: true});
 			});
 		});
 
-		// Testfallet har failat enstaka gånger i CI p.g.a. timeout på denna knapp
-		cy.contains('button','Skicka till Försäkringskassan', {timeout: 20000}).should('be.visible').and('be.enabled').click();
-		cy.contains('Om du går vidare kommer intyget skickas direkt till Försäkringskassans system vilket ska göras i samråd med patienten.')
-			.should('be.visible');
-		cy.contains('button', "Skicka").should('be.visible').click();
+		// Testfallet har failat enstaka gånger i CI p.g.a. timeout på denna knapp, ökar därför timeout
+		cy.contains('button', this.lisjpData.skickaTillFKKnappText, {timeout: 20000})
+		.should('be.visible').and('be.enabled')
+		.click();
+
+		cy.contains(this.lisjpData.varningSkickaTillFK).should('be.visible');
+
+		cy.contains('button', this.lisjpData.skickaKnappTextEfterVarning)
+		.should('be.visible')
+		.click();
 	});
 });
