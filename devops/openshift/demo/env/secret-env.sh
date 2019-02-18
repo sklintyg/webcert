2d7a0dcf832097020f211a4a77199e5c8c63b2ad
Peter Larsson <peter.larsson@callistaenterprise.se>
2019-02-18 15:30:06 +0100

    INTYG-8170: Hämta refdata från GitHub repo

--- devops/openshift/demo/env/secret-env.sh
+++ devops/openshift/demo/env/secret-env.sh
@@ -8,6 +8,5 @@ export CATALINA_OPTS_APPEND="\
 -Dlogback.file=/opt/$APP_NAME/config/logback-ocp.xml \
 -Dcertificate.folder=/opt/$APP_NAME/certifikat \
 -Dcredentials.file=/opt/$APP_NAME/env/secret-env.properties \
--Dresources.folder=classpath: \
 -Dfile.encoding=UTF-8 \
 -DbaseUrl=http://${APP_NAME}:8080"
