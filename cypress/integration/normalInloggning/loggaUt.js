d750f0ebc7c77b5a76eb264ec2d4031bcd9d7790
Johannes Larsson <johannes.larsson@nordicmedtest.se>
2019-03-06 13:35:07 +0100

    TESTENV: uppdatera loggaUt-testfallet för Jenkins

--- cypress/integration/normalInloggning/loggaUt.js
+++ cypress/integration/normalInloggning/loggaUt.js
@@ -8,13 +8,18 @@ describe('Logga ut vårdgivare', function () {
         cy.fixture('alfaEnheten').as('vårdenhet');
     });
 
-    // Funkar inte i Jenkins
-    it.skip('är möjligt att logga ut vårdgivare', function() {
+    it('är möjligt att logga ut vårdgivare', function() {
         cy.loggaInVårdgivareNormal(this);
         cy.visit('/#/create/choose-patient/index');
 
-        // Klicka på "Logga ut"
-        cy.get('[id="logoutLink"]').click();
+        // Cookie-banner är ivägen för utloggning, den måste godkännas först
+        const cookiebannerText = "Vi använder kakor (cookies)";
+        cy.contains(cookiebannerText);
+        cy.get("#cookie-usage-consent-btn").click();
+        cy.contains(cookiebannerText).should('not.exist');
+
+        // Klicka på "Logga ut" och verifiera att vi dirigeras till rätt sida
+        cy.get("#logoutLink").click();
         cy.url().should('include', "/welcome.html");
     });
 });
