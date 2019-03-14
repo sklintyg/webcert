2014292b5c9fcc09b7c19312db4303e9f0e58fbf
Johannes Larsson <johannes.larsson@nordicmedtest.se>
2019-03-14 14:01:16 +0100

    TESTENV: Mindre förbättring i testfall väljaPatient

--- test/cypress/integration/normalInloggning/valjaPatient.js
+++ test/cypress/integration/normalInloggning/valjaPatient.js
@@ -16,9 +16,7 @@ describe('Välja patient', function () {
     });
 
     it('är möjligt att ange korrekt personnummer och komma vidare', function() {
-        // Alias för "Fortsätt"-knappen
-		cy.contains("Fortsätt").as('continueBtn');
-		cy.get('@continueBtn').should('be.disabled');
+		cy.get('#skapapersonnummerfortsatt').should('be.disabled');
 
         // Mata in patientens personnummer
         cy
@@ -26,10 +24,7 @@ describe('Välja patient', function () {
 		.type(this.vårdtagare.personnummer)
 		.should('have.value', this.vårdtagare.personnummer);
 
-		// Verfiera att knappen för att gå vidare är aktiverad och klicka på den
-		cy.get('@continueBtn').should('be.enabled').then(() => {
-			cy.get('@continueBtn').click();
-        });
+        cy.get('#skapapersonnummerfortsatt').should('be.enabled').click();
 
         cy.url().should('include', "#/create/choose-intyg-type/" + this.vårdtagare.personnummer + "/index");
     });
