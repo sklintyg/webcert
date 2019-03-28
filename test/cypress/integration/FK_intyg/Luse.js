b72c06023ee51f9cbb6e65f74c6af2e8d67b4ca9
Johannes Larsson <johannes.larsson@nordicmedtest.se>
2019-03-28 14:07:02 +0100

    TESTENV: Brutit ut LUSE till egen fil enligt nya mönstret

--- test/cypress/integration/FK_intyg/Luse.js
+++ test/cypress/integration/FK_intyg/Luse.js
@@ -1,12 +1,13 @@
 /* globals context cy */
 /// <reference types="Cypress" />
+import * as intyg from '../../support/FK_intyg/luseIntyg'
 
 // LUSE = Läkarutlåtande för sjukersättning, FK 7800
 
 describe('LUSE-intyg', function () {
 
     before(function() {
-        cy.fixture('FK_intyg/maxLuseData').as('luseData');
+        cy.fixture('FK_intyg/maxLuseData').as('intygsdata');
         cy.fixture('vårdgivare/arnoldJohansson').as('vårdgivare');
         cy.fixture('vårdenheter/alfaEnheten').as('vårdenhet');
         cy.fixture('vårdtagare/tolvanTolvansson').as('vårdtagare');
@@ -23,6 +24,56 @@ describe('LUSE-intyg', function () {
         cy.loggaInVårdgivareIntegrerat(this);
 
         // Gå till intyget, redigera det, signera och skicka till FK
-        cy.visit("/visa/intyg/" + this.utkastId + "?enhet=" + this.vårdenhet.id).fyllIMaxLuse(this);
+        const önskadUrl = "/visa/intyg/" + this.utkastId + "?enhet=" + this.vårdenhet.id
+        cy.visit(önskadUrl);
+
+        // Om vi dirigeras till sidan som säger att 'Intygsutkastet är raderat'
+        // så försöker vi igen eftersom det antagligen gick för snabbt.
+        cy.get('body').then(($body) => {
+            if ($body.text().includes('Intygsutkastet är raderat och kan därför inte längre visas.')) {
+                cy.log("Kom till 'Intygetsutkastet är raderat', antagligen gick det för snabbt. Provar igen.");
+                cy.loggaInVårdgivareIntegrerat(this); // Vi behöver logga in igen
+                cy.visit(önskadUrl);
+            }
+        });
+
+        cy.url().should('include', this.utkastId);
+
+        intyg.sektionGrundFörMedicinsktUnderlag(this.intygsdata.grundFörMedicinsktUnderlag);
+        intyg.sektionDiagnos(this.intygsdata.diagnos);
+        intyg.sektionBakgrund(this.intygsdata.bakgrund);
+        intyg.sektionFunktionsnedsättningar(this.intygsdata.funkNedsättningar);
+        intyg.sektionAktivitetsbegränsningar(this.intygsdata.aktivitetsbegränsningar);
+        intyg.sektionMedicinskBehandling(this.intygsdata.medicinskBehandling);
+        intyg.sektionMedicinskaFörutsättningarFörArbete(this.intygsdata.medicinskaFörutsättningar);
+        intyg.sektionÖvrigt(this.intygsdata.övrigt);
+        intyg.sektionKontakt(this.intygsdata.kontakt);
+
+        // Signera intyget
+        cy.contains("Klart att signera");
+        cy.contains("Obligatoriska uppgifter saknas").should('not.exist');
+        cy.contains("Utkastet sparas").should('not.exist');
+
+        // cy.click() fungerar inte alltid. Det finns ärenden rapporterade
+        // (stängd pga inaktivitet):
+        // https://github.com/cypress-io/cypress/issues/2551
+        // https://www.cypress.io/blog/2019/01/22/when-can-the-test-click/ :
+        // "If a tree falls in the forest and no one has attached a “fall” event listener, did it really fall?"
+
+        const click = $el => { return $el.click() }
+
+        // Parent() p.g.a. att ett element täcker knappen
+        cy.get('#signera-utkast-button').parent().should('be.visible')
+
+        cy.get('#signera-utkast-button')
+        .pipe(click, {timeout: 60000}) // ToDo: Lång timeout (problem endast på Jenkins, överlastad slav?)
+        .should($el => {
+            expect($el.parent()).to.not.be.visible;
+        })
+
+        // Skicka till FK
+        cy.get("#sendBtn", { timeout: 60000 }).click();
+        cy.get("#button1send-dialog").click(); // Modal som dyker upp och frågar om man verkligen vill skicka
+        cy.contains("Intyget är skickat till Försäkringskassan");
     });
 });
---
+++ test/cypress/support/FK_intyg/luseIntyg.js
@@ -0,0 +1,220 @@
+// Intyget är uppdelat på samma sätt som det är uppdelat när det fylls i genom WebCert
+
+// Datumen är inte specifika för något testfall
+
+// -------------------- 'Grund för medicinskt underlag' --------------------
+export function sektionGrundFörMedicinsktUnderlag(medUnderlag) {
+    const idagMinus5    = Cypress.moment().subtract(5,   'days').format('YYYY-MM-DD');
+    const idagMinus6    = Cypress.moment().subtract(6,   'days').format('YYYY-MM-DD');
+    const idagMinus14   = Cypress.moment().subtract(14,  'days').format('YYYY-MM-DD');
+    const idagMinus15   = Cypress.moment().subtract(15,  'days').format('YYYY-MM-DD');
+    const idagMinus2Mån = Cypress.moment().subtract(2, 'months').format('YYYY-MM-DD');
+
+    const utlåtandeBaseratPå = medUnderlag.utlåtandetÄrBaseratPå;
+    if (utlåtandeBaseratPå.minUndersökning) {
+        // TODO: Lägger väldigt lång timeout vid första elementet i intyget eftersom
+        // sidan ibland inte har hunnit ladda innan den får timeout.
+        // Initial analys är att Jenkins är överbelastad då det verkar fungera bra när
+        // man kör lokalt.
+        cy.get('#checkbox_undersokningAvPatienten', {timeout: 60000}).check();
+        cy.get("#datepicker_undersokningAvPatienten").clear().type(idagMinus5);
+    }
+
+    if (utlåtandeBaseratPå.journaluppgifter) {
+        cy.get('#checkbox_journaluppgifter').check();
+        cy.get("#datepicker_journaluppgifter").clear().type(idagMinus15);
+    }
+
+    if (utlåtandeBaseratPå.anhörigsBeskrivning) {
+        cy.get('#checkbox_anhorigsBeskrivningAvPatienten').check();
+        cy.get("#datepicker_anhorigsBeskrivningAvPatienten").clear().type(idagMinus6);
+    }
+
+    if (utlåtandeBaseratPå.annat) {
+        cy.get('#checkbox_annatGrundForMU').check();
+        cy.get("#datepicker_annatGrundForMU").clear().type(idagMinus14);
+
+        // cy.type() tar bara in text eller nummer (så vi behöver inte verifiera värdet)
+        cy.get("#annatGrundForMUBeskrivning").type(utlåtandeBaseratPå.annatText);
+    }
+
+    cy.get("#datepicker_kannedomOmPatient").clear().type(idagMinus14);
+
+    const andraUtrEllerUnderlag = medUnderlag.andraUtredningarEllerUnderlag;
+    if (andraUtrEllerUnderlag.ja) {
+        cy.get("#underlagFinnsYes").click();
+
+        // TODO: Testa andra id:n här. Fundera också på om det ska brytas ut till en funktion
+        if (andraUtrEllerUnderlag.rad1) {
+            const rad = andraUtrEllerUnderlag.rad1;
+            cy.get("#underlag-0--typ-selected-item-label").click();
+            cy.get("#wcdropdown-underlag-0--typ")
+            .contains(rad.underlagstyp)
+            .then(option => {
+                cy.wrap(option).contains(rad.underlagstyp); // Säkerställ att rätt alternativ valts
+                option[0].click(); // jquery "click()", inte Cypress "click()"
+            });
+
+            cy.get("#datepicker_underlag\\[0\\]\\.datum").clear().type(idagMinus2Mån);
+            cy.get("#underlag-0--hamtasFran").type(rad.underlagHämtasFrån);
+        }
+
+        if (andraUtrEllerUnderlag.rad2) {
+            expect(andraUtrEllerUnderlag.rad1).to.exist;
+            const rad = andraUtrEllerUnderlag.rad2;
+            cy.get("#underlag-1--typ-selected-item-label").click();
+            cy.get("#wcdropdown-underlag-1--typ")
+            .contains(rad.underlagstyp)
+            .then(option => {
+                cy.wrap(option).contains(rad.underlagstyp); // Säkerställ att rätt alternativ valts
+                option[0].click(); // jquery "click()", inte Cypress "click()"
+            });
+
+            cy.get("#datepicker_underlag\\[1\\]\\.datum").clear().type(idagMinus2Mån);
+            cy.get("#underlag-1--hamtasFran").type(rad.underlagHämtasFrån);
+        }
+
+        if (andraUtrEllerUnderlag.rad3) {
+            expect(andraUtrEllerUnderlag.rad2).to.exist;
+            const rad = andraUtrEllerUnderlag.rad3;
+            cy.get("#underlag-2--typ-selected-item-label").click();
+            cy.get("#wcdropdown-underlag-2--typ")
+            .contains(rad.underlagstyp)
+            .then(option => {
+                cy.wrap(option).contains(rad.underlagstyp); // Säkerställ att rätt alternativ valts
+                option[0].click(); // jquery "click()", inte Cypress "click()"
+            });
+
+            cy.get("#datepicker_underlag\\[2\\]\\.datum").clear().type(idagMinus2Mån);
+            cy.get("#underlag-2--hamtasFran").type(rad.underlagHämtasFrån);
+        }
+    }
+}
+
+// -------------------- 'Diagnos' --------------------
+export function sektionDiagnos(diagnos) {
+    if (diagnos.rad1) {
+        cy.get('#diagnoseCode-0').parent().within(($diagnoskodrad) => {
+            cy.get('#diagnoseCode-0').type(diagnos.rad1.kod);
+            cy.wrap($diagnoskodrad).contains(diagnos.rad1.text).click();
+        });
+        cy.get('#diagnoseDescription-0').invoke('val').should('contain', diagnos.rad1.text);
+    }
+
+    if (diagnos.rad2) {
+        expect(diagnos.rad1).to.exist;
+        cy.get('#diagnoseCode-1').parent().within(($diagnoskodrad) => {
+            cy.get('#diagnoseCode-1').type(diagnos.rad2.kod);
+            cy.wrap($diagnoskodrad).contains(diagnos.rad2.text).click();
+        });
+        cy.get('#diagnoseDescription-1').invoke('val').should('contain', diagnos.rad2.text);
+    }
+
+    if (diagnos.rad3) {
+        expect(diagnos.rad2).to.exist;
+        cy.get('#diagnoseCode-2').parent().within(($diagnoskodrad) => {
+            cy.get('#diagnoseCode-2').type(diagnos.rad3.kod);
+            cy.wrap($diagnoskodrad).contains(diagnos.rad3.text).click();
+        });
+        cy.get('#diagnoseDescription-2').invoke('val').should('contain', diagnos.rad3.text);
+    }
+
+    cy.get("#diagnosgrund").type(diagnos.diagnosgrund);
+
+    if (diagnos.revidera.ja) {
+        cy.get("#nyBedomningDiagnosgrundYes").click();
+        cy.get("#diagnosForNyBedomning").type(diagnos.revidera.text);
+    }
+}
+
+// -------------------- 'Bakgrund' --------------------
+export function sektionBakgrund(bakgrund) {
+    cy.get('#sjukdomsforlopp').type(bakgrund.text);
+}
+
+// -------------------- 'Funktionsnedsättningar' --------------------
+export function sektionFunktionsnedsättningar(funkNedsättningar) {
+    if (funkNedsättningar.intellektuell) {
+        cy.get('#toggle-funktionsnedsattningIntellektuell').click();
+        cy.get('#funktionsnedsattningIntellektuell').type(funkNedsättningar.intellektuell.text);
+    }
+
+    if (funkNedsättningar.kommunikation) {
+        cy.get('#toggle-funktionsnedsattningKommunikation').click();
+        cy.get('#funktionsnedsattningKommunikation').type(funkNedsättningar.kommunikation.text);
+    }
+
+    if (funkNedsättningar.koncentration) {
+        cy.get('#toggle-funktionsnedsattningKoncentration').click();
+        cy.get('#funktionsnedsattningKoncentration').type(funkNedsättningar.koncentration.text);
+    }
+
+    if (funkNedsättningar.psykisk) {
+        cy.get('#toggle-funktionsnedsattningPsykisk').click();
+        cy.get('#funktionsnedsattningPsykisk').type(funkNedsättningar.psykisk.text);
+    }
+
+    if (funkNedsättningar.sinnesfunktionerOchSmärta) {
+        cy.get('#toggle-funktionsnedsattningSynHorselTal').click();
+        cy.get('#funktionsnedsattningSynHorselTal').type(funkNedsättningar.sinnesfunktionerOchSmärta.text);
+    }
+
+    if (funkNedsättningar.balansOchKoordination) {
+        cy.get('#toggle-funktionsnedsattningBalansKoordination').click();
+        cy.get('#funktionsnedsattningBalansKoordination').type(funkNedsättningar.balansOchKoordination.text);
+    }
+
+    if (funkNedsättningar.annan) {
+        cy.get('#toggle-funktionsnedsattningAnnan').click();
+        cy.get('#funktionsnedsattningAnnan').type(funkNedsättningar.annan.text);
+    }
+}
+
+// -------------------- 'Aktivitetsbegränsningar' --------------------
+export function sektionAktivitetsbegränsningar(aktBegränsningar) {
+    cy.get("#aktivitetsbegransning").type(aktBegränsningar.text);
+}
+
+// -------------------- 'Medicinsk behandling' --------------------
+export function sektionMedicinskBehandling(medBehandling) {
+    if (medBehandling.avslutadBehandling) {
+        cy.get("#avslutadBehandling").type(medBehandling.avslutadBehandling.text);
+    }
+
+    if (medBehandling.pågåendeBehandling) {
+        cy.get("#pagaendeBehandling").type(medBehandling.pågåendeBehandling.text);
+    }
+
+    if (medBehandling.planeradBehandling) {
+        cy.get("#planeradBehandling").type(medBehandling.planeradBehandling.text);
+    }
+
+    if (medBehandling.substansintag) {
+        cy.get("#substansintag").type(medBehandling.substansintag.text);
+    }
+}
+
+// -------------------- 'Medicinska förutsättningar för arbete' --------------------
+export function sektionMedicinskaFörutsättningarFörArbete(medFörutsättningar) {
+    cy.get("#medicinskaForutsattningarForArbete").type(medFörutsättningar.utvecklingÖverTid);
+
+    if (medFörutsättningar.förmågaTrotsBegränsning) {
+        cy.get("#formagaTrotsBegransning").type(medFörutsättningar.förmågaTrotsBegränsning.text);
+    }
+}
+
+// -------------------- 'Övrigt' --------------------
+export function sektionÖvrigt(övrigt) {
+    cy.get("#ovrigt").type(övrigt.text);
+}
+
+// -------------------- 'Kontakt' --------------------
+export function sektionKontakt(kontakt) {
+    if (kontakt.ja) {
+        cy.get("#kontaktMedFk").check();
+
+        if (kontakt.text) {
+            cy.get("#anledningTillKontakt").type(kontakt.text);
+        }
+    }
+}
