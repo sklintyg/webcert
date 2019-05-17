af0ec86fd2435624dbadccf5a0585f2a0623a04d
Matias Wurschmidt-Wang <matias.wurschmidt@soprasteria.com>
2019-05-17 08:27:09 +0200

    INTYG-6719: Javadoc for ActionLinkType.

--- web/src/main/java/se/inera/intyg/webcert/web/web/util/resourcelinks/dto/ActionLinkType.java
+++ web/src/main/java/se/inera/intyg/webcert/web/web/util/resourcelinks/dto/ActionLinkType.java
@@ -19,21 +19,89 @@
 
 package se.inera.intyg.webcert.web.web.util.resourcelinks.dto;
 
+/**
+ * Type of links that are returned through WebCerts REST API for relevant resources.
+ * 
+ * These types can be resolved on the client side to assess if the user has correct access.
+ */
 public enum ActionLinkType {
+    /**
+     * Create new draft.
+     */
     SKAPA_UTKAST,
+
+    /**
+     * Delete draft.
+     */
     TA_BORT_UTKAST,
+
+    /**
+     * Print draft
+     */
     SKRIV_UT_UTKAST,
+
+    /**
+     * Invaliate locked draft
+     */
     MAKULERA_UTKAST,
+
+    /**
+     * Use the draft when creating new as a copy
+     */
     KOPIERA_UTKAST,
+
+    /**
+     * Renew certificate
+     */
     FORNYA_INTYG,
+
+    /**
+     * Invalidate certificate
+     */
     MAKULERA_INTYG,
+
+    /**
+     * Print certificate
+     */
     SKRIV_UT_INTYG,
+
+    /**
+     * Replace certificate
+     */
     ERSATT_INTYG,
+
+    /**
+     * Send certificate
+     */
     SKICKA_INTYG,
+
+    /**
+     * Read certificate
+     */
     LASA_INTYG,
+
+    /**
+     * Create new administrative question related to a certificate
+     */
     SKAPA_FRAGA,
+
+    /**
+     * Answer a administrative question related to a certificate
+     */
     BESVARA_FRAGA,
+
+    /**
+     * Answer a complement question related to a certificate
+     */
     BESVARA_KOMPLETTERING,
+
+    /**
+     * Read questions related to a certificate
+     */
     LASA_FRAGA,
+
+    /**
+     * Forward questions related to a certificate
+     */
     VIDAREBEFODRA_FRAGA;
 }
