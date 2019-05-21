88c9ed3756a538d309cc8ece6543a0c89af0f529
Johannes Larsson <johannes.larsson@nordicmedtest.se>
2019-05-21 08:41:54 +0200

    TESTENV: Lägg till cypress-plugin-retries som kör om failade testfall en gång

--- test/cypress/support/index.js
+++ test/cypress/support/index.js
@@ -6,6 +6,7 @@
 // Import commands.js using ES2015 syntax:
 import './commands'
 import 'cypress-pipe'
+import 'cypress-plugin-retries' // Läser miljövariablen "RETRIES" och kör om failade testfall så många gånger
 import 'cypress-failed-log'
 
 // Alternatively you can use CommonJS syntax:
