b95658f4bde910fe50127c5e6e306c62a11074c2
Magnus Ekstrand <magnus@ekstrand.net>
2019-05-23 15:04:42 +0200

    INTYGFV-11441: bumpat till ny verion av internt TJK (#51)
    * DEVENV: soapUI uppdatering
    
    * Revert "DEVENV: soapUI uppdatering"
    
    This reverts commit 024427158be9450e984f8f80278acfda29d3a53e.
    
    * INTYGFV-11441: Lade till endpoint för att kunna hämta upp kompletteringar
    
    * DEVENV: soapUI uppdatering
    
    * Revert "DEVENV: soapUI uppdatering"
    
    This reverts commit 024427158be9450e984f8f80278acfda29d3a53e.
    
    * INTYGFV-11441: Lagt till RestAssured-test för kompletteringar plus ändringar i befintligt junit-test
    
    * INTYGFV-11441: Lagt till RestAssured-test för kompletteringar plus ändringar i befintligt junit-test
    
    * DEVENV: soapUI uppdatering
    
    * Revert "DEVENV: soapUI uppdatering"
    
    This reverts commit 024427158be9450e984f8f80278acfda29d3a53e.
    
    * INTYGFV-11441: Ersatte REST-api med SOAP-service för att hämta kompletteringar
    
    * INTYGFV-11441: Bumpat intygClinicalprocessSchemasVersion till 1.0.18
    
    * INTYGFV-11441: ArendeService.getKompletteringar skall inte kontrollera user/enheter (har ingen user i detta kontext)
    
    * Feature/intygfv 11697 fix (#27)
    
    * INTYGFV-11697: Lagt till uppslag med personId för händelser
    
    * INTYGFV-11562: Hämtar driftbanners från intygsadmin (#26)
    
    * INTYGFV-11441: Remove arende-stub
    
    * INTYGFV-11441: Remove arende-stub context
    
    * INTYGFV-11441: Added schema configuration to endpoints
    
    * INTYGFV-11441: bumpat till ny verion av internt TJK
    
    * INTYGFV-11441: tagit bort referenser till arende-stub i gradle-filer

---
+++ web/src/test/java/se/inera/intyg/webcert/web/integration/interactions/getcertificateadditions/GetCertificateAdditionsResponderImplTest.java
@@ -0,0 +1,119 @@
+/*
+ * Copyright (C) 2019 Inera AB (http://www.inera.se)
+ *
+ * This file is part of sklintyg (https://github.com/sklintyg).
+ *
+ * sklintyg is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * sklintyg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package se.inera.intyg.webcert.web.integration.interactions.getcertificateadditions;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import se.inera.intyg.clinicalprocess.healthcond.certificate.getcertificateadditions.v1.GetCertificateAdditionsResponseType;
+import se.inera.intyg.clinicalprocess.healthcond.certificate.getcertificateadditions.v1.GetCertificateAdditionsType;
+import se.inera.intyg.webcert.persistence.arende.model.Arende;
+import se.inera.intyg.webcert.persistence.model.Status;
+import se.inera.intyg.webcert.web.service.arende.ArendeService;
+import se.riv.clinicalprocess.healthcond.certificate.types.v3.IntygId;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+/**
+ * @author Magnus Ekstrand on 2019-05-16.
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class GetCertificateAdditionsResponderImplTest {
+
+    private static final List<Long> ARENDE_IDS = Arrays.asList(1234567L, 2345678L, 3456789L);
+
+    private static final List<String> INTYG_IDS = Arrays.asList("ABC123", "DEF456", "GHI789");
+
+    @Mock
+    private ArendeService arendeService;
+
+    @InjectMocks
+    GetCertificateAdditionsResponderImpl testee;
+
+    @Test
+    public void whenGettingKompletteringarSuccessfully() {
+        when(arendeService.getKompletteringar(INTYG_IDS)).thenReturn(mockKompetteringar());
+
+        GetCertificateAdditionsResponseType additions = testee.getCertificateAdditions("", buildRequest());
+        assertEquals(3, additions.getAdditions().size());
+    }
+
+    @Test
+    public void whenThereAreNoKompletteringar() {
+        when(arendeService.getKompletteringar(INTYG_IDS)).thenReturn(new ArrayList<>());
+
+        GetCertificateAdditionsResponseType additions = testee.getCertificateAdditions("", buildRequest());
+        assertEquals(0, additions.getAdditions().size());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenInvalidRequest() {
+        testee.getCertificateAdditions("", buildEmptyRequest());
+    }
+
+    private GetCertificateAdditionsType buildEmptyRequest() {
+        GetCertificateAdditionsType request = new GetCertificateAdditionsType();
+        request.getIntygsId().addAll(new ArrayList<>());
+        return request;
+    }
+
+    private GetCertificateAdditionsType buildRequest() {
+        List<IntygId> identities = INTYG_IDS.stream()
+                .map(s -> {
+                    IntygId intygId = new IntygId();
+                    intygId.setRoot("some-root-value");
+                    intygId.setExtension(s);
+                    return intygId;
+                })
+                .collect(Collectors.toList());
+
+        GetCertificateAdditionsType request = new GetCertificateAdditionsType();
+        request.getIntygsId().addAll(identities);
+
+        return request;
+    }
+
+    private List<Arende> mockKompetteringar() {
+        List<Arende> arenden = new ArrayList<>();
+
+        for (int i = 0; i < ARENDE_IDS.size(); i++) {
+            Arende arende = new Arende();
+            arende.setId(ARENDE_IDS.get(i));
+            arende.setStatus(Status.PENDING_INTERNAL_ACTION);
+            arende.setTimestamp(LocalDateTime.now(ZoneId.systemDefault()));
+            arende.setIntygsId(INTYG_IDS.get(i));
+
+            arenden.add(arende);
+        }
+
+        return arenden;
+    }
+
+}
