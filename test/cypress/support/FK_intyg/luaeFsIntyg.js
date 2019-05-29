25d993a4bf9ace72e83c299ea3311389cc4b3b01
Johannes Larsson <johannes.larsson@nordicmedtest.se>
2019-05-29 10:57:21 +0200

    TESTENV: PDL-TC för LUAE-FS. Ej fullständigt verifierad då Webcert har PDL-bugg

--- test/cypress/support/FK_intyg/luaeFsIntyg.js
+++ test/cypress/support/FK_intyg/luaeFsIntyg.js
@@ -88,6 +88,8 @@ export function sektionGrundFörMedicinsktUnderlag(medUnderlag) {
             cy.get("#datepicker_underlag\\[2\\]\\.datum").clear().type(idagMinus2Mån);
             cy.get("#underlag-2--hamtasFran").type(rad.underlagHämtasFrån);
         }
+    } else {
+        cy.get("#underlagFinnsNo").click();
     }
 }
 
@@ -183,4 +185,44 @@ export function skickaTillFk() {
     // Modal som dyker upp och frågar om man verkligen vill skicka
     cy.get("#button1send-dialog").click();
     cy.contains("Intyget är skickat till Försäkringskassan");
+}
+
+// -------------------- 'Skriv ut intyget' --------------------
+export function skrivUt(typAvUtskrift, intygsId){
+    switch(typAvUtskrift) {
+        case "utkast":
+        case "fullständigt":
+            cy.request({
+                method: 'GET',
+                url: 'moduleapi/intyg/luae_fs/' + intygsId + "/pdf",
+            });
+            cy.log('Skriver ut ett ' + typAvUtskrift + ' intyg (via cy.request, ej grafiskt)');
+            break;
+        default:
+            cy.log('Ingen korrekt typ av utskrift vald');
+    }
+}
+
+// ------------------'Förnya intyg'---------------------------
+export function fornya() {
+    cy.get('#fornyaBtn').click();
+}
+
+// ------------------'Radera utkast'--------------------------
+export function raderaUtkast() {
+    cy.get('#ta-bort-utkast').click();
+    cy.get('#confirm-draft-delete-button').click();
+}
+
+// ------------------'Makulera intyg'-------------------------
+export function makuleraIntyg(arg) {
+    cy.get('#makuleraBtn').click();
+    if (arg === "Annat allvarligt fel") {
+        cy.get('#reason-ANNAT_ALLVARLIGT_FEL').check();
+        cy.get('#clarification-ANNAT_ALLVARLIGT_FEL').type('Testanledning');
+        cy.get('#button1makulera-dialog').click();
+    } else {
+        cy.get('#reason-FEL_PATIENT').check();
+        cy.get('#button1makulera-dialog').click();
+    }
 }
