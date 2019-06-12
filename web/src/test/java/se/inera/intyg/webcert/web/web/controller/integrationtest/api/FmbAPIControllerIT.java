2342ddb485d319927ef3ef71a29939b306d8883e
Marcus Cedergren <marcus.cedergren@callistaenterprise.se>
2019-06-12 14:21:26 +0200

    INTYGFV-11059: Rättar JPQL GROUP BY för valideraSjukskrivningstid och lägger till smoketest RestAssuredtest som saknades helt.

--- web/src/test/java/se/inera/intyg/webcert/web/web/controller/integrationtest/api/FmbAPIControllerIT.java
+++ web/src/test/java/se/inera/intyg/webcert/web/web/controller/integrationtest/api/FmbAPIControllerIT.java
@@ -18,15 +18,18 @@
  */
 package se.inera.intyg.webcert.web.web.controller.integrationtest.api;
 
-import com.jayway.restassured.RestAssured;
-import com.jayway.restassured.response.ResponseBody;
-import org.junit.Test;
-import se.inera.intyg.webcert.web.web.controller.integrationtest.BaseRestIntegrationTest;
-
 import static com.jayway.restassured.RestAssured.given;
 import static com.jayway.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
-import static org.hamcrest.Matchers.hasSize;
-import static org.hamcrest.core.IsEqual.equalTo;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Test;
+
+import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.response.ResponseBody;
+
+import se.inera.intyg.webcert.web.web.controller.integrationtest.BaseRestIntegrationTest;
 
 /**
  * Very basic test of the FMB API that requests an icd10 code known to have fmb info and one that doesn't.
@@ -65,6 +68,27 @@ public class FmbAPIControllerIT extends BaseRestIntegrationTest {
         given().cookie("ROUTEID", BaseRestIntegrationTest.routeId).pathParam("icd10", "X1337")
                 .expect().statusCode(204)
                 .when().get("api/fmb/{icd10}");
+    }
+
+    @Test
+    public void getValideraSjukskrivningstid() {
+
+        //Currently, the fmb database is empty when staring webcert so we need to manually trigger fmb sync to make
+        // sure database is populated before actual testing begins. When/if this is automated, this trigger request
+        // can be removed.
+        given().expect().statusCode(200).when().get("testability/fmb/updatefmbdata");
+
+        RestAssured.sessionId = getAuthSession(DEFAULT_LAKARE);
+
+        Map<String, String> params = new HashMap<>();
+        params.put("icd10Kod1", "S62");
+        params.put("foreslagenSjukskrivningstid", "200");
+        params.put("personnummer", "191212121212");
+
+        given().cookie("ROUTEID", BaseRestIntegrationTest.routeId).queryParams(params)
+                .expect().statusCode(200)
+                .when().get("api/fmb/valideraSjukskrivningstid").then()
+                .body(matchesJsonSchemaInClasspath("jsonschema/webcert-fmb-validerasjukskrivningstid-response-schema.json"));
     }
 
 }
