c43bd2624a5e37d748aeead0b581604cbf0d664f
Daniel Petersson <daniel.pete@gmail.com>
2019-08-29 16:01:49 +0200

    INTYGFV-12173: Revert to old IdP

--- web/src/main/java/se/inera/intyg/webcert/web/auth/WebcertAuthenticationProvider.java
+++ web/src/main/java/se/inera/intyg/webcert/web/auth/WebcertAuthenticationProvider.java
@@ -19,35 +19,28 @@
 package se.inera.intyg.webcert.web.auth;
 
 import java.util.Date;
-
 import org.opensaml.saml2.core.AuthnStatement;
 import org.springframework.security.saml.SAMLAuthenticationProvider;
 import org.springframework.security.saml.SAMLCredential;
-
 import se.inera.intyg.webcert.web.auth.common.AuthConstants;
 
 /**
- * This customized AuthenticationProvider provides custom logic for setting the expiration date of the SAML
- * credential.
+ * This customized AuthenticationProvider provides custom logic for setting the expiration date of the SAML credential.
  *
- * For SITHS-based authentications we typically get 60 minutes from the IdP, but we can safely ignore this by setting
- * the expiration to null which mitigates the problem of users being automatically logged out after 60 minutes
- * regardless
- * of user activity.
+ * For SITHS-based authentications we typically get 60 minutes from the IdP, but we can safely ignore this by setting the expiration to null
+ * which mitigates the problem of users being automatically logged out after 60 minutes regardless of user activity.
  *
  * Created by eriklupander on 2016-08-18.
  */
 public class WebcertAuthenticationProvider extends SAMLAuthenticationProvider {
 
     /**
-     * Retrieves the expirationDate per the SAML credential using the superclass method, but then overrides its value
-     * with null
-     * if there is a "SITHS" authenticationStatement.
+     * Retrieves the expirationDate per the SAML credential using the superclass method, but then overrides its value with null if there is
+     * a "SITHS" authenticationStatement.
      *
-     * @param credential
-     *            credential to use for expiration parsing.
-     * @return null if no expiration is present OR if this is a SITHS-based authentication. Otherwise, expiration time
-     *         based on SAML attribute onOrAfter.
+     * @param credential credential to use for expiration parsing.
+     * @return null if no expiration is present OR if this is a SITHS-based authentication. Otherwise, expiration time based on SAML
+     * attribute onOrAfter.
      */
     @Override
     protected Date getExpirationDate(SAMLCredential credential) {
@@ -73,7 +66,13 @@ public class WebcertAuthenticationProvider extends SAMLAuthenticationProvider {
         if (statement.getAuthnContext().getAuthnContextClassRef().getAuthnContextClassRef() == null) {
             return false;
         }
+
+        if (statement.getAuthnContext().getAuthnContextClassRef().getAuthnContextClassRef()
+            .equals(AuthConstants.URN_OASIS_NAMES_TC_SAML_2_0_AC_CLASSES_TLS_CLIENT)) {
+            return true;
+        }
+
         return statement.getAuthnContext().getAuthnContextClassRef().getAuthnContextClassRef()
-                .equals(AuthConstants.HTTP_ID_SAMBI_SE_LOA_LOA3);
+            .equals(AuthConstants.HTTP_ID_SAMBI_SE_LOA_LOA3);
     }
 }
