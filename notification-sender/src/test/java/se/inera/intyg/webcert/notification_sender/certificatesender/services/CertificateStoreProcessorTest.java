f03a23156b610bad8750a9bda551a142a1fee1ea
Matias Wurschmidt-Wang <matias.wurschmidt@soprasteria.com>
2020-01-24 08:57:41 +0100

    INTYGFV-12680: Handle catched non-typed Exception in CertificateStoreProcessor as TemporaryExceptions instead of PermanentExceptions to allow Webcert to retry storing the certificate in Intygstjansten.

--- notification-sender/src/test/java/se/inera/intyg/webcert/notification_sender/certificatesender/services/CertificateStoreProcessorTest.java
+++ notification-sender/src/test/java/se/inera/intyg/webcert/notification_sender/certificatesender/services/CertificateStoreProcessorTest.java
@@ -135,7 +135,7 @@ public class CertificateStoreProcessorTest {
         certificateStoreProcessor.process(BODY, "fk7263", LOGICAL_ADDRESS1);
     }
 
-    @Test(expected = PermanentException.class)
+    @Test(expected = TemporaryException.class)
     public void testStoreCertificateThrowsPermanentOnException() throws Exception {
         // Given
         doThrow(new RuntimeException()).when(moduleApi).registerCertificate(anyString(), anyString());
