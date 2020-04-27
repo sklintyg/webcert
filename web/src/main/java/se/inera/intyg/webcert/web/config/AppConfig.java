fef903a5b794532e0f342f2a90489f558cbf36f9
Daniel Petersson <45845951+peterssond@users.noreply.github.com>
2020-04-27 09:09:52 +0200

    INTYGFV-13060: SAML bug due to samesite

--- web/src/main/java/se/inera/intyg/webcert/web/config/AppConfig.java
+++ web/src/main/java/se/inera/intyg/webcert/web/config/AppConfig.java
@@ -19,10 +19,13 @@
 package se.inera.intyg.webcert.web.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
 import org.springframework.context.annotation.Import;
 import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.session.web.http.CookieSerializer;
+import org.springframework.session.web.http.DefaultCookieSerializer;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.transaction.annotation.TransactionManagementConfigurer;
@@ -41,6 +44,18 @@ public class AppConfig implements TransactionManagementConfigurer {
     @Override
     public PlatformTransactionManager annotationDrivenTransactionManager() {
         return transactionManager;
+    }
+
+    @Bean
+    public CookieSerializer cookieSerializer() {
+        DefaultCookieSerializer serializer = new DefaultCookieSerializer();
+        /*
+        This is needed to make IdP functionality work.
+        This will not satisfy all browsers, but it works for IE, Chrome and Edge.
+        Reference: https://auth0.com/blog/browser-behavior-changes-what-developers-need-to-know/
+         */
+        serializer.setSameSite("none");
+        return serializer;
     }
 
 }
