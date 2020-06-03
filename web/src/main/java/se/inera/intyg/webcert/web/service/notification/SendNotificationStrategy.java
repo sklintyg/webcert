cf9f5d3fdbdd1aa7e2b192c893f94e851258e774
Matias Wurschmidt-Wang <matias.wurschmidt@soprasteria.com>
2020-06-03 14:40:13 +0200

    INTYGFV-12692: Added logic to decide notification version based on certificate instead of only draft.

--- web/src/main/java/se/inera/intyg/webcert/web/service/notification/SendNotificationStrategy.java
+++ web/src/main/java/se/inera/intyg/webcert/web/service/notification/SendNotificationStrategy.java
@@ -19,11 +19,14 @@
 package se.inera.intyg.webcert.web.service.notification;
 
 import java.util.Optional;
+import se.inera.intyg.common.support.model.common.internal.Utlatande;
 import se.inera.intyg.common.support.modules.support.api.notification.SchemaVersion;
 import se.inera.intyg.webcert.persistence.utkast.model.Utkast;
 
 public interface SendNotificationStrategy {
 
     Optional<SchemaVersion> decideNotificationForIntyg(Utkast utkast);
+
+    Optional<SchemaVersion> decideNotificationForIntyg(Utlatande certificate);
 
 }
