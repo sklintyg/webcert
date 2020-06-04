325cdd928f6236d09fef1d4e5b1b9402d2a725ed
Daniel Petersson <daniel.petersson@soprasteria.com>
2020-06-04 16:40:03 +0200

    INTYGFV-13206: Added Xml signature Service

---
+++ web/src/main/java/se/inera/intyg/webcert/web/service/underskrift/dss/DssSignMessageService.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) 2020 Inera AB (http://www.inera.se)
+ *
+ * This file is part of sklintyg (https://github.com/sklintyg).
+ *
+ * sklintyg is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * sklintyg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package se.inera.intyg.webcert.web.service.underskrift.dss;
+
+import javax.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import se.inera.intyg.infra.xmldsig.model.ValidationResponse;
+import se.inera.intyg.infra.xmldsig.service.XMLDSigService;
+
+@Service
+public class DssSignMessageService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(DssSignMessageService.class);
+
+
+    private XMLDSigService infraXMLDSigService;
+
+
+    @Autowired
+    public DssSignMessageService(XMLDSigService infraXMLDSigService) {
+        this.infraXMLDSigService = infraXMLDSigService;
+    }
+
+    @PostConstruct
+    public void init() {
+        org.apache.xml.security.Init.init();
+    }
+
+    public void signSignRequest() {
+
+    }
+
+    public ValidationResponse validateSignResponseSignature(String signResponseXmlString) {
+        return infraXMLDSigService.validateSignatureValidity(signResponseXmlString, true);
+    }
+
+
+}
