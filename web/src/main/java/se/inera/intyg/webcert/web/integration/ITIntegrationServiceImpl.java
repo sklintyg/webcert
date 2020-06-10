bdf0d4e7a1ecba3da06cfe504395af3eed37bc30
Matias Wurschmidt-Wang <matias.wurschmidt@soprasteria.com>
2020-06-10 12:50:02 +0200

    INTYGFV-12692: Added integration service for calling Intygstjansten´s internal Rest APIs.

---
+++ web/src/main/java/se/inera/intyg/webcert/web/integration/ITIntegrationServiceImpl.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (C) 2020 Inera AB (http://www.inera.se)
+ *
+ * This file is part of sklintyg (https://github.com/sklintyg).
+ *
+ * sklintyg is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * sklintyg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package se.inera.intyg.webcert.web.integration;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
+import se.inera.intyg.infra.message.dto.MessageFromIT;
+
+@Service
+public class ITIntegrationServiceImpl implements ITIntegrationService {
+
+    @Bean("itRestTemplate")
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    @Autowired
+    @Qualifier("itRestTemplate")
+    private RestTemplate restTemplate;
+
+    @Value("${intygstjanst.base.url}")
+    private String intygstjanstenUrl;
+
+    @Override
+    public List<MessageFromIT> findMessagesByCertificateId(String certificateId) {
+        final var url = intygstjanstenUrl + "/internalapi/message/" + certificateId;
+        try {
+            return Arrays.asList(restTemplate.getForObject(url, MessageFromIT[].class));
+        } catch (HttpClientErrorException ex) {
+            if (ex.getStatusCode() != HttpStatus.NOT_FOUND) {
+                throw ex;
+            }
+            return Collections.emptyList();
+        }
+    }
+}
