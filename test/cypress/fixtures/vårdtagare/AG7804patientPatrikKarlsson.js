0c5b6f9db8be8b11c7cb5eb491f1945fdf96dbfc
Kristina Gullman <kristina.gullman@nordicmedtest.se>
2020-06-12 13:56:34 +0200

    INTYGFV-00:Nyperson för AG7804 intyg

---
+++ test/cypress/fixtures/vårdtagare/AG7804patientPatrikKarlsson.js
@@ -0,0 +1,9 @@
+patient = {
+    "förnamn": "Patrik",
+    "efternamn": "Karlsson",
+    "personnummerKompakt": "200104282397",
+    "personnummer": "20010428-2397",
+    "postadress": "Bryggaregatan 11",
+    "postnummer": "65340",
+    "postort": "Karlstad"
+}
