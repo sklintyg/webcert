b2ce407fccfe268682b88130439b9ff64a078537
Mikael Heglind <mikael.heglind@soprasteria.com>
2020-12-18 16:29:27 +0100

    INTYGFV-13590: Removed some unused imports

--- notification-sender/src/test/java/se/inera/intyg/webcert/notification_sender/notifications/helper/NotificationTestHelper.java
+++ notification-sender/src/test/java/se/inera/intyg/webcert/notification_sender/notifications/helper/NotificationTestHelper.java
@@ -18,31 +18,21 @@
  */
 package se.inera.intyg.webcert.notification_sender.notifications.helper;
 
-import java.time.Instant;
 import java.time.LocalDate;
 import java.time.Year;
 
-import se.inera.intyg.common.support.common.enumerations.HandelsekodEnum;
 import se.inera.intyg.common.support.modules.converter.InternalConverterUtil;
 import se.inera.intyg.infra.integration.pu.model.Person;
 import se.inera.intyg.schemas.contract.Personnummer;
-import se.inera.intyg.webcert.notification_sender.notifications.dto.ExceptionInfoTransporter;
-import se.inera.intyg.webcert.notification_sender.notifications.dto.NotificationWSResultMessage;
-import se.riv.clinicalprocess.healthcond.certificate.certificatestatusupdateforcareresponder.v3.CertificateStatusUpdateForCareResponseType;
-import se.riv.clinicalprocess.healthcond.certificate.certificatestatusupdateforcareresponder.v3.CertificateStatusUpdateForCareType;
 import se.riv.clinicalprocess.healthcond.certificate.types.v3.ArbetsplatsKod;
-import se.riv.clinicalprocess.healthcond.certificate.types.v3.Handelsekod;
 import se.riv.clinicalprocess.healthcond.certificate.types.v3.IntygId;
 import se.riv.clinicalprocess.healthcond.certificate.types.v3.PartialDateTypeFormatEnum;
 import se.riv.clinicalprocess.healthcond.certificate.types.v3.PersonId;
 import se.riv.clinicalprocess.healthcond.certificate.types.v3.TypAvIntyg;
 import se.riv.clinicalprocess.healthcond.certificate.v3.Enhet;
-import se.riv.clinicalprocess.healthcond.certificate.v3.ErrorIdType;
 import se.riv.clinicalprocess.healthcond.certificate.v3.HosPersonal;
 import se.riv.clinicalprocess.healthcond.certificate.v3.Intyg;
 import se.riv.clinicalprocess.healthcond.certificate.v3.Patient;
-import se.riv.clinicalprocess.healthcond.certificate.v3.ResultCodeType;
-import se.riv.clinicalprocess.healthcond.certificate.v3.ResultType;
 import se.riv.clinicalprocess.healthcond.certificate.v3.Vardgivare;
 
 /**
