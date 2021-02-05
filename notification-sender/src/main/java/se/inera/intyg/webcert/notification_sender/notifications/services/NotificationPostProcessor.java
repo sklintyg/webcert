096558fe38c6c65ecf8c9ea574451ac1f0135b1f
Matias Wurschmidt-Wang <matias.wurschmidt@soprasteria.com>
2021-02-05 11:32:49 +0100

    INTYGFV-10514: Refactoring of NotificationPostProcessor. Moved logic to NotificationRedeliveryService.

--- notification-sender/src/main/java/se/inera/intyg/webcert/notification_sender/notifications/services/NotificationPostProcessor.java
+++ notification-sender/src/main/java/se/inera/intyg/webcert/notification_sender/notifications/services/NotificationPostProcessor.java
@@ -19,13 +19,6 @@
 
 package se.inera.intyg.webcert.notification_sender.notifications.services;
 
-import static se.inera.intyg.webcert.common.enumerations.NotificationDeliveryStatusEnum.FAILURE;
-import static se.inera.intyg.webcert.common.enumerations.NotificationDeliveryStatusEnum.RESEND;
-import static se.inera.intyg.webcert.common.enumerations.NotificationDeliveryStatusEnum.SUCCESS;
-import static se.inera.intyg.webcert.notification_sender.notifications.enumerations.NotificationErrorTypeEnum.TECHNICAL_ERROR;
-import static se.inera.intyg.webcert.notification_sender.notifications.enumerations.NotificationErrorTypeEnum.WEBCERT_EXCEPTION;
-import static se.inera.intyg.webcert.notification_sender.notifications.enumerations.NotificationResultTypeEnum.ERROR;
-import static se.inera.intyg.webcert.notification_sender.notifications.enumerations.NotificationResultTypeEnum.INFO;
 import static se.inera.intyg.webcert.notification_sender.notifications.routes.NotificationRouteHeaders.CORRELATION_ID;
 import static se.inera.intyg.webcert.notification_sender.notifications.routes.NotificationRouteHeaders.HANDELSE;
 import static se.inera.intyg.webcert.notification_sender.notifications.routes.NotificationRouteHeaders.INTYGS_ID;
@@ -33,16 +26,11 @@ import static se.inera.intyg.webcert.notification_sender.notifications.routes.No
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import java.util.Objects;
 import org.apache.camel.Message;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import se.inera.intyg.webcert.common.enumerations.NotificationDeliveryStatusEnum;
 import se.inera.intyg.webcert.notification_sender.notifications.dto.NotificationResultMessage;
-import se.inera.intyg.webcert.notification_sender.notifications.dto.NotificationResultType;
-import se.inera.intyg.webcert.notification_sender.notifications.enumerations.NotificationErrorTypeEnum;
-import se.inera.intyg.webcert.notification_sender.notifications.enumerations.NotificationResultTypeEnum;
 
 public class NotificationPostProcessor {
 
@@ -54,20 +42,12 @@ public class NotificationPostProcessor {
     @Autowired
     private NotificationRedeliveryService notificationRedeliveryService;
 
-    private static final String SOAPFAULTEXCEPTION = "javax.xml.ws.soap.SOAPFaultException";
-    private static final String MARSHALLING_ERROR = "Marshalling Error";
-    private static final String UNMARSHALLING_ERROR = "Unmarshalling Error";
-
-
     public void process(Message message) {
-
         try {
             final var resultMessage = getNotificationResultMessage(message);
-            processNotificationResult(resultMessage);
-        } catch (JsonProcessingException e) {
+            notificationRedeliveryService.processNotificationResult(resultMessage);
+        } catch (Exception e) {
             LOG.error(getLogErrorMessage(message), e);
-            // TODO add monitorlog
-            //  is recovery possible? (should we try to resend, check resend table etc)
         }
     }
 
@@ -75,82 +55,9 @@ public class NotificationPostProcessor {
         return objectMapper.readValue(message.getBody(String.class), NotificationResultMessage.class);
     }
 
-    private void processNotificationResult(NotificationResultMessage resultMessage) {
-
-        NotificationDeliveryStatusEnum deliveryStatus = extractDeliveryStatusFromResult(resultMessage);
-        // TODO: Why changing the event here?
-        resultMessage.getEvent().setDeliveryStatus(deliveryStatus);
-
-        switch (deliveryStatus) {
-            case SUCCESS:
-                notificationRedeliveryService.handleNotificationSuccess(resultMessage);
-                break;
-            case RESEND:
-                notificationRedeliveryService.handleNotificationResend(resultMessage);
-                break;
-            case FAILURE:
-                notificationRedeliveryService.handleNotificationFailure(resultMessage);
-        }
-    }
-
-    private NotificationDeliveryStatusEnum extractDeliveryStatusFromResult(NotificationResultMessage resultMessage) {
-
-        if (resultMessage.getResultType().getNotificationErrorType() == WEBCERT_EXCEPTION) {
-            return getDeliveryStatusOnException(resultMessage);
-        } else {
-            return getDeliveryStatusOnResultType(resultMessage);
-        }
-    }
-
-    private NotificationDeliveryStatusEnum getDeliveryStatusOnException(NotificationResultMessage resultMessage) {
-        if (isFatalException(resultMessage.getResultType())) {
-            LOG.debug("Failure sending status update to care for {}", resultMessage);
-            return FAILURE;
-        } else {
-            LOG.debug("Failure sending status update to care for {}, Attempting redelivery...", resultMessage);
-            return RESEND;
-        }
-    }
-
-    private NotificationDeliveryStatusEnum getDeliveryStatusOnResultType(NotificationResultMessage resultMessage) {
-        NotificationResultType resultType = resultMessage.getResultType();
-        NotificationResultTypeEnum result = resultType.getNotificationResult();
-        if (ERROR == result) {
-            return getDeliveryStatusOnError(resultType, resultMessage);
-        }
-        if (INFO == result) {
-            LOG.info("Received info message from care for status update {}, info received: {}", resultMessage,
-                resultType.getNotificationResultText());
-        }
-        return SUCCESS;
-    }
-
-    private NotificationDeliveryStatusEnum getDeliveryStatusOnError(NotificationResultType resultType,
-        NotificationResultMessage resultMessage) {
-
-        NotificationErrorTypeEnum errorType = resultType.getNotificationErrorType();
-        String errorText = resultType.getNotificationResultText();
-        if (errorType == TECHNICAL_ERROR) {
-            LOG.debug("{} returned from care for status update {}, Error message: {}, Attempting redelivery...", errorType, resultMessage,
-                errorText);
-            return RESEND;
-        } else {
-            LOG.debug("{} returned from care for status update {}, Error message: {}", errorType, resultMessage, errorText);
-            return FAILURE;
-        }
-    }
-
-    private boolean isFatalException(NotificationResultType resultType) {
-        String resultText = resultType.getNotificationResultText();
-        return (SOAPFAULTEXCEPTION.equals(resultType.getException())
-            && Objects.nonNull(resultText) && (resultText.contains(MARSHALLING_ERROR)
-            || resultText.contains(UNMARSHALLING_ERROR)))
-            || resultType.getNotificationResult() == NotificationResultTypeEnum.FAILURE;
-    }
-
     private String getLogErrorMessage(Message message) {
-        return String.format("Failure mapping json to NotificationWSResultMessage [certificateId: %s, correlationId: %s, "
-            + "logicalAddress: %s, event: %s]", message.getHeader(INTYGS_ID), message.getHeader(CORRELATION_ID),
+        return String.format("Failure to process NotificationWSResultMessage [certificateId: %s, correlationId: %s, "
+                + "logicalAddress: %s, event: %s]", message.getHeader(INTYGS_ID), message.getHeader(CORRELATION_ID),
             message.getHeader(LOGISK_ADRESS), message.getHeader(HANDELSE));
     }
 }
--- notification-sender/src/test/java/se/inera/intyg/webcert/notification_sender/notifications/services/NotificationPostProcessorTest.java
+++ notification-sender/src/test/java/se/inera/intyg/webcert/notification_sender/notifications/services/NotificationPostProcessorTest.java
@@ -19,43 +19,24 @@
 
 package se.inera.intyg.webcert.notification_sender.notifications.services;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.isA;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoInteractions;
-import static org.mockito.Mockito.when;
-import static se.inera.intyg.webcert.notification_sender.notifications.enumerations.NotificationErrorTypeEnum.WEBCERT_EXCEPTION;
-import static se.riv.clinicalprocess.healthcond.certificate.v3.ErrorIdType.APPLICATION_ERROR;
-import static se.riv.clinicalprocess.healthcond.certificate.v3.ErrorIdType.REVOKED;
-import static se.riv.clinicalprocess.healthcond.certificate.v3.ErrorIdType.TECHNICAL_ERROR;
-import static se.riv.clinicalprocess.healthcond.certificate.v3.ErrorIdType.VALIDATION_ERROR;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import javax.xml.bind.MarshalException;
-import javax.xml.namespace.QName;
-import javax.xml.soap.SOAPException;
-import javax.xml.soap.SOAPFactory;
-import javax.xml.soap.SOAPFault;
-import javax.xml.ws.soap.SOAPFaultException;
 import org.apache.camel.Message;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Spy;
 import org.mockito.junit.MockitoJUnitRunner;
 import se.inera.intyg.webcert.notification_sender.notifications.dto.NotificationResultMessage;
-import se.inera.intyg.webcert.notification_sender.notifications.dto.NotificationResultType;
-import se.inera.intyg.webcert.notification_sender.notifications.enumerations.NotificationErrorTypeEnum;
-import se.inera.intyg.webcert.notification_sender.notifications.enumerations.NotificationResultTypeEnum;
-import se.inera.intyg.webcert.notification_sender.notifications.routes.NotificationRouteHeaders;
-import se.inera.intyg.webcert.persistence.handelse.model.Handelse;
-import se.riv.clinicalprocess.healthcond.certificate.certificatestatusupdateforcareresponder.v3.CertificateStatusUpdateForCareResponseType;
-import se.riv.clinicalprocess.healthcond.certificate.v3.ErrorIdType;
-import se.riv.clinicalprocess.healthcond.certificate.v3.ResultCodeType;
-import se.riv.clinicalprocess.healthcond.certificate.v3.ResultType;
 
 
 @RunWith(MockitoJUnitRunner.class)
@@ -65,200 +46,37 @@ public class NotificationPostProcessorTest {
     private NotificationRedeliveryService notificationRedeliveryService;
 
     @Mock
-    private Message message;
-
-    @Spy
     private ObjectMapper objectMapper;
 
     @InjectMocks
     private NotificationPostProcessor postProcessor;
 
-    private static final String CERTIFICATE_ID = "testCertificateId";
-    private static final String LOGICAL_ADDRESS = "testLogicalAddress";
-    private static final String CORRELATION_ID = "testCorrelationId";
-    private static final String EVENT = "testSKAPAT";
+    @Test
+    public void processNotificationResultFromMessage() throws Exception {
+        final var message = mock(Message.class);
+        final var body = "BODY";
+        final var notificationResultMessage = mock(NotificationResultMessage.class);
+        final var argumentCaptor = ArgumentCaptor.forClass(NotificationResultMessage.class);
 
-    private static final ResultCodeType OK = ResultCodeType.OK;
-    private static final ResultCodeType INFO = ResultCodeType.INFO;
-    private static final ResultCodeType ERROR = ResultCodeType.ERROR;
+        doReturn(body).when(message).getBody(String.class);
+        doReturn(notificationResultMessage).when(objectMapper).readValue(body, NotificationResultMessage.class);
 
+        postProcessor.process(message);
 
-    @Before
-    public void setup() {
-        when(message.getHeader(NotificationRouteHeaders.INTYGS_ID)).thenReturn(CERTIFICATE_ID);
-        when(message.getHeader(NotificationRouteHeaders.CORRELATION_ID)).thenReturn(CORRELATION_ID);
-        when(message.getHeader(NotificationRouteHeaders.LOGISK_ADRESS)).thenReturn(LOGICAL_ADDRESS);
-        when(message.getHeader(NotificationRouteHeaders.HANDELSE)).thenReturn(EVENT);
+        verify(notificationRedeliveryService).processNotificationResult(argumentCaptor.capture());
+        assertEquals(notificationResultMessage, argumentCaptor.getValue());
     }
 
     @Test
-    public void receiveMessageWithInvalidType() {
-        String invalidMessageJson = "{\"exceptionWrapper\":\"not a valid NotificationResultMessage\"}";
-        when(message.getBody(String.class)).thenReturn(invalidMessageJson);
+    public void dontProcessAnythingIfMessageIsCorrupt() throws Exception {
+        final var message = mock(Message.class);
+        final var body = "BODY";
+
+        doReturn(body).when(message).getBody(String.class);
+        doThrow(new RuntimeException()).when(objectMapper).readValue(anyString(), eq(NotificationResultMessage.class));
+
         postProcessor.process(message);
+
         verifyNoInteractions(notificationRedeliveryService);
-    }
-
-    @Test
-    public void receiveMessageWithStatusOk() throws JsonProcessingException {
-        NotificationResultMessage resultMessage = buildResultMessage(OK, null, "");
-        String messageJson = objectMapper.writeValueAsString(resultMessage);
-        when(message.getBody(String.class)).thenReturn(messageJson);
-        postProcessor.process(message);
-        verify(notificationRedeliveryService).handleNotificationSuccess(isA(NotificationResultMessage.class));
-    }
-
-    @Test
-    public void receiveMessageWithStatusInfo() throws JsonProcessingException {
-        NotificationResultMessage resultMessage = buildResultMessage(INFO, null, "Informative message");
-        String messageJson = objectMapper.writeValueAsString(resultMessage);
-        when(message.getBody(String.class)).thenReturn(messageJson);
-        postProcessor.process(message);
-
-        verify(notificationRedeliveryService).handleNotificationSuccess(any(NotificationResultMessage.class));
-    }
-
-    @Test
-    public void receiveMessageWithStatusTechnicalError() throws JsonProcessingException {
-        NotificationResultMessage resultMessage = buildResultMessage(ERROR, TECHNICAL_ERROR, "Technical error message");
-        String messageJson = objectMapper.writeValueAsString(resultMessage);
-        when(message.getBody(String.class)).thenReturn(messageJson);
-        postProcessor.process(message);
-
-        verify(notificationRedeliveryService).handleNotificationResend(any(NotificationResultMessage.class));
-    }
-
-    @Test
-    public void receiveMessageWithStatusValidationError() throws JsonProcessingException {
-        NotificationResultMessage resultMessage = buildResultMessage(ERROR, VALIDATION_ERROR, "Validation error message");
-        String messageJson = objectMapper.writeValueAsString(resultMessage);
-        when(message.getBody(String.class)).thenReturn(messageJson);
-        postProcessor.process(message);
-
-        verify(notificationRedeliveryService).handleNotificationFailure(any(NotificationResultMessage.class));
-    }
-
-    @Test
-    public void receiveMessageWithStatusApplicationError() throws JsonProcessingException {
-        NotificationResultMessage resultMessage = buildResultMessage(ERROR, APPLICATION_ERROR, "Application error message");
-        String messageJson = objectMapper.writeValueAsString(resultMessage);
-        when(message.getBody(String.class)).thenReturn(messageJson);
-        postProcessor.process(message);
-
-        verify(notificationRedeliveryService).handleNotificationFailure(any(NotificationResultMessage.class));
-    }
-
-    @Test
-    public void receiveMessageWithStatusRevoked() throws JsonProcessingException {
-        NotificationResultMessage resultMessage = buildResultMessage(ERROR, REVOKED, "Revoked error message");
-        String messageJson = objectMapper.writeValueAsString(resultMessage);
-        when(message.getBody(String.class)).thenReturn(messageJson);
-        postProcessor.process(message);
-
-        verify(notificationRedeliveryService).handleNotificationFailure(any(NotificationResultMessage.class));
-    }
-
-    @Test
-    public void receiveMessageWithRuntimeException() throws JsonProcessingException {
-        Exception exception = new RuntimeException("testRuntimeException");
-        NotificationResultMessage resultMessage = buildResultMessageWithException(NotificationResultTypeEnum.ERROR,
-            exception.getClass().getName(), exception.getMessage());
-        String messageJson = objectMapper.writeValueAsString(resultMessage);
-        when(message.getBody(String.class)).thenReturn(messageJson);
-        postProcessor.process(message);
-
-        verify(notificationRedeliveryService).handleNotificationResend(any(NotificationResultMessage.class));
-    }
-
-    @Test
-    public void receiveMessageWithSoapFaultExceptionAndMarshallingError() throws JsonProcessingException, SOAPException {
-        SOAPFaultException soapFaultException = generateSoapFaultException("Marshalling Error");
-        NotificationResultMessage resultMessage = buildResultMessageWithException(NotificationResultTypeEnum.ERROR,
-            soapFaultException.getClass().getName(), soapFaultException.getMessage());
-        String messageJson = objectMapper.writeValueAsString(resultMessage);
-        when(message.getBody(String.class)).thenReturn(messageJson);
-        postProcessor.process(message);
-
-        verify(notificationRedeliveryService).handleNotificationFailure(any(NotificationResultMessage.class));
-    }
-
-    @Test
-    public void receiveMessageWithSoapFaultExceptionAndUnmarshallingError() throws JsonProcessingException, SOAPException {
-        SOAPFaultException soapFaultException = generateSoapFaultException("Unmarshalling Error");
-        NotificationResultMessage resultMessage = buildResultMessageWithException(NotificationResultTypeEnum.ERROR,
-            soapFaultException.getClass().getName(), soapFaultException.getMessage());
-        String messageJson = objectMapper.writeValueAsString(resultMessage);
-        when(message.getBody(String.class)).thenReturn(messageJson);
-        postProcessor.process(message);
-
-        verify(notificationRedeliveryService).handleNotificationFailure(any(NotificationResultMessage.class));
-    }
-
-    @Test
-    public void receiveMessageWithExceptionFromTransFormer() throws JsonProcessingException {
-        Exception exception = new NullPointerException("testNullPointerException");
-        NotificationResultMessage resultMessage = buildResultMessageWithException(NotificationResultTypeEnum.FAILURE,
-            exception.getClass().getName(), exception.getMessage());
-        String messageJson = objectMapper.writeValueAsString(resultMessage);
-        when(message.getBody(String.class)).thenReturn(messageJson);
-        postProcessor.process(message);
-
-        verify(notificationRedeliveryService).handleNotificationFailure(any(NotificationResultMessage.class));
-    }
-
-    private NotificationResultMessage buildResultMessage(ResultCodeType code, ErrorIdType errorId, String resultText) {
-        CertificateStatusUpdateForCareResponseType statusUpdateResponse = buildStatusUpdateResponse(code, errorId, resultText);
-        NotificationResultMessage resultMessage = new NotificationResultMessage();
-        resultMessage.setCorrelationId(CORRELATION_ID);
-        resultMessage.setEvent(new Handelse());
-
-        final var notificationResultType = new NotificationResultType();
-        final var resultType = statusUpdateResponse.getResult();
-
-        if (resultType.getResultCode() != null) {
-            final var notificationResult = NotificationResultTypeEnum.fromValue(resultType.getResultCode().value());
-            notificationResultType.setNotificationResult(notificationResult);
-        }
-
-        if (resultType.getErrorId() != null) {
-            final var notificationErrorType = NotificationErrorTypeEnum.fromValue(resultType.getErrorId().value());
-            notificationResultType.setNotificationErrorType(notificationErrorType);
-        }
-
-        notificationResultType.setNotificationResultText(resultType.getResultText());
-
-        resultMessage.setResultType(notificationResultType);
-        return resultMessage;
-    }
-
-    private NotificationResultMessage buildResultMessageWithException(NotificationResultTypeEnum resultEnum, String exception,
-        String resultText) {
-        NotificationResultMessage resultMessage = new NotificationResultMessage();
-        resultMessage.setCorrelationId(CORRELATION_ID);
-        resultMessage.setEvent(new Handelse());
-        final var notificationResultType = new NotificationResultType();
-        notificationResultType.setNotificationResult(resultEnum);
-        notificationResultType.setException(exception);
-        notificationResultType.setNotificationResultText(resultText);
-        notificationResultType.setNotificationErrorType(WEBCERT_EXCEPTION);
-        resultMessage.setResultType(notificationResultType);
-        return resultMessage;
-    }
-
-    private CertificateStatusUpdateForCareResponseType buildStatusUpdateResponse(ResultCodeType code, ErrorIdType errorId, String resultText) {
-        CertificateStatusUpdateForCareResponseType res = new CertificateStatusUpdateForCareResponseType();
-        res.setResult(new ResultType());
-        res.getResult().setResultCode(code);
-        res.getResult().setErrorId(errorId);
-        res.getResult().setResultText(resultText);
-        return res;
-    }
-
-    private SOAPFaultException generateSoapFaultException(String message) throws SOAPException {
-        SOAPFactory soapFactory = SOAPFactory.newInstance();
-        SOAPFault soapFault = soapFactory.createFault(message, new QName(""));
-        SOAPFaultException soapFaultException = new SOAPFaultException(soapFault);
-        soapFaultException.initCause(new MarshalException(message));
-        return soapFaultException;
     }
 }
