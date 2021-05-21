b16fc71531a09458e9ad7b25d946ecc0ccf18da1
Mikael Heglind <mikael.heglind@soprasteria.com>
2021-05-21 09:32:08 +0200

    INTYGFV-10551: Change to show warning modal only once per care provider (per session).

--- web/src/main/webapp/app/views/subscription/subscription.controller.js
+++ web/src/main/webapp/app/views/subscription/subscription.controller.js
@@ -17,25 +17,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-/*
- * Copyright (C) 2021 Inera AB (http://www.inera.se)
- *
- * This file is part of sklintyg (https://github.com/sklintyg).
- *
- * sklintyg is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * sklintyg is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 angular.module('webcert').controller('webcert.SubscriptionCtrl', ['$log', '$rootScope', '$scope', '$window','$sanitize', '$state',
     '$location', 'common.UserModel', 'common.subscriptionService', 'common.dynamicLinkService', 'webcert.SubscriptionProxy',
     'common.messageService',
@@ -60,7 +41,7 @@ angular.module('webcert').controller('webcert.SubscriptionCtrl', ['$log', '$root
             {
                 name: 'subscription.sign.agreement.now',
                 clickFn: function() {
-                    acknowledgeSubscriptionWarning(UserModel.user.valdVardenhet.id);
+                    acknowledgeSubscriptionWarning(UserModel.user.valdVardgivare.id);
                     $window.open(dynamicLinkService.getLink('kundportalenGetAccount').url);
                 },
                 text: 'subscription.sign.agreement.now.label',
@@ -70,7 +51,7 @@ angular.module('webcert').controller('webcert.SubscriptionCtrl', ['$log', '$root
             {
                 name: 'subscription.sign.agreement.later',
                 clickFn: function() {
-                    acknowledgeSubscriptionWarning(UserModel.user.valdVardenhet.id);
+                    acknowledgeSubscriptionWarning(UserModel.user.valdVardgivare.id);
                 },
                 text: 'subscription.sign.agreement.later.label',
                 id: 'subscriptionSignAgreementLaterBtn',
