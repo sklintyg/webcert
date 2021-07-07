52af3106ce6e8289d022078e9c071d20c9918983
mhornfeldt <michaela.hornfeldt@soprasteria.com>
2021-07-07 10:06:35 +0200

    INTYGFV-13873: Added question controller and service with test for getting questions

---
+++ web/src/main/java/se/inera/intyg/webcert/web/web/controller/facade/QuestionController.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2021 Inera AB (http://www.inera.se)
+ *
+ * This file is part of sklintyg (https://github.com/sklintyg).
+ *
+ * sklintyg is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * sklintyg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package se.inera.intyg.webcert.web.web.controller.facade;
+
+import javax.validation.constraints.NotNull;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import se.inera.intyg.infra.monitoring.annotation.PrometheusTimeMethod;
+
+@Path("/question")
+public class QuestionController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(CertificateController.class);
+
+    private static final String UTF_8_CHARSET = ";charset=utf-8";
+
+    @GET
+    @Path("/{certificateId}")
+    @Produces(MediaType.APPLICATION_JSON + UTF_8_CHARSET)
+    @PrometheusTimeMethod
+    public Response getQuestions(@PathParam("certificateId") @NotNull String certificateId) {
+        if (LOG.isDebugEnabled()) {
+            LOG.debug("Getting questions for certificate with id: '{}'", certificateId);
+        }
+        return Response.ok().build();
+        //return Response.ok(CertificateResponseDTO.create(certificateDTO)).build();
+    }
+
+}
