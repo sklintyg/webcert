2b5c121ef9b5326e08f7f2f11bf615fa6f287f6d
Michaela Hörnfeldt <54178725+mhornfeldt@users.noreply.github.com>
2021-11-10 10:59:50 +0100

    INTYGFV-14134: Added possibility to log errors from client  (#586)

---
+++ web/src/main/java/se/inera/intyg/webcert/web/web/controller/facade/dto/ErrorLogRequestDTO.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (C) 2021 Inera AB (http://www.inera.se)
+ *
+ * This file is part of sklintyg (https://github.com/sklintyg).
+ *
+ * sklintyg is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * sklintyg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package se.inera.intyg.webcert.web.web.controller.facade.dto;
+
+public class ErrorLogRequestDTO {
+
+    private String message;
+    private String errorId;
+    private String certificateId;
+    private String errorCode;
+    private String stackTrace;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getErrorId() {
+        return errorId;
+    }
+
+    public void setErrorId(String errorId) {
+        this.errorId = errorId;
+    }
+
+    public String getCertificateId() {
+        return certificateId;
+    }
+
+    public void setCertificateId(String certificateId) {
+        this.certificateId = certificateId;
+    }
+
+    public String getErrorCode() {
+        return errorCode;
+    }
+
+    public void setErrorCode(String errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    public String getStackTrace() {
+        return stackTrace;
+    }
+
+    public void setStackTrace(String stackTrace) {
+        this.stackTrace = stackTrace;
+    }
+}
---
+++ web/src/test/java/se/inera/intyg/webcert/web/web/controller/facade/LogControllerTest.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2021 Inera AB (http://www.inera.se)
+ *
+ * This file is part of sklintyg (https://github.com/sklintyg).
+ *
+ * sklintyg is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * sklintyg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package se.inera.intyg.webcert.web.web.controller.facade;
+
+import static org.mockito.Mockito.verify;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import se.inera.intyg.webcert.web.service.facade.ErrorLogFacadeService;
+import se.inera.intyg.webcert.web.web.controller.facade.dto.ErrorLogRequestDTO;
+
+@ExtendWith(MockitoExtension.class)
+public class LogControllerTest {
+
+    @Mock
+    private ErrorLogFacadeService errorLogFacadeService;
+
+    @InjectMocks
+    private LogController logController;
+
+    @Nested
+    class LogError {
+
+        @Test
+        void shallCallErrorLogFacadeService() {
+            ErrorLogRequestDTO request = new ErrorLogRequestDTO();
+            logController.logError(request);
+            verify(errorLogFacadeService).log(request);
+        }
+    }
+}
