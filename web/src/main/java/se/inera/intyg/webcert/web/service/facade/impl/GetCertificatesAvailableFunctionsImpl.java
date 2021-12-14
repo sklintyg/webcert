8fd8e08990c3cd256d823348708a170780c51a76
Robert Danielsson <robert.danielsson@soprasteria.com>
2021-12-14 15:39:10 +0100

    INTYGFV-14317: Added break

--- web/src/main/java/se/inera/intyg/webcert/web/service/facade/impl/GetCertificatesAvailableFunctionsImpl.java
+++ web/src/main/java/se/inera/intyg/webcert/web/service/facade/impl/GetCertificatesAvailableFunctionsImpl.java
@@ -179,6 +179,7 @@ public class GetCertificatesAvailableFunctionsImpl implements GetCertificatesAva
                 availableFunctions.addAll(
                     getAvailableFunctionsForRevokedCertificate(certificate)
                 );
+                break;
             default:
         }
         return availableFunctions;
