37c76c37d272ec35efcf9135fd479ca199545a99
Per Flitig <40682617+perfli@users.noreply.github.com>
2022-02-25 09:18:12 +0100

    INTYGFV-14253: Add facade endpoint to get version (#647)

---
+++ web/src/main/java/se/inera/intyg/webcert/web/web/controller/facade/ConfigController.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright (C) 2022 Inera AB (http://www.inera.se)
+ *
+ * This file is part of sklintyg (https://github.com/sklintyg).
+ *
+ * sklintyg is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * sklintyg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package se.inera.intyg.webcert.web.web.controller.facade;
+
+import java.util.Map;
+import javax.annotation.PostConstruct;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.context.support.SpringBeanAutowiringSupport;
+import se.inera.intyg.infra.dynamiclink.model.DynamicLink;
+import se.inera.intyg.infra.dynamiclink.service.DynamicLinkService;
+import se.inera.intyg.infra.monitoring.annotation.PrometheusTimeMethod;
+import se.inera.intyg.webcert.web.web.controller.facade.dto.ConfigurationDTO;
+
+@Path("/configuration")
+public class ConfigController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(UserController.class);
+
+    private static final String UTF_8_CHARSET = ";charset=utf-8";
+
+    @Value("${project.version}")
+    private String version;
+
+    @Autowired
+    private DynamicLinkService dynamicLinkService;
+
+    @PostConstruct
+    public void init() {
+        SpringBeanAutowiringSupport.processInjectionBasedOnCurrentContext(this);
+    }
+
+    @GET
+    @Path("/")
+    @Produces(MediaType.APPLICATION_JSON + UTF_8_CHARSET)
+    @PrometheusTimeMethod
+    public Response getConfiguration() {
+        if (LOG.isDebugEnabled()) {
+            LOG.debug("Getting configuration");
+        }
+        return Response.ok(new ConfigurationDTO(version)).build();
+    }
+
+    @GET
+    @Path("/links")
+    @Produces(MediaType.APPLICATION_JSON + UTF_8_CHARSET)
+    @PrometheusTimeMethod
+    public Map<String, DynamicLink> getDynamicLinks() {
+        return dynamicLinkService.getAllAsMap();
+    }
+}
