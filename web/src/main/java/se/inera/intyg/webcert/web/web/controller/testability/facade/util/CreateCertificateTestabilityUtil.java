823bce1410e65b1a7d78f772e171ab79e4cc3b3f
Michaela Hörnfeldt <54178725+mhornfeldt@users.noreply.github.com>
2022-03-04 13:31:14 +0100

    INTYGFV-14410: Added possibility to fill out maximal certificate through welcome page (#650)

--- web/src/main/java/se/inera/intyg/webcert/web/web/controller/testability/facade/util/CreateCertificateTestabilityUtil.java
+++ web/src/main/java/se/inera/intyg/webcert/web/web/controller/testability/facade/util/CreateCertificateTestabilityUtil.java
@@ -18,42 +18,21 @@
  */
 package se.inera.intyg.webcert.web.web.controller.testability.facade.util;
 
-import static se.inera.intyg.common.af00213.v1.model.converter.RespConstants.ARBETETS_PAVERKAN_DELSVAR_ID_41;
-import static se.inera.intyg.common.af00213.v1.model.converter.RespConstants.ARBETETS_PAVERKAN_SVAR_JSON_ID_41;
-import static se.inera.intyg.common.af00213.v1.model.converter.RespConstants.FUNKTIONSNEDSATTNING_DELSVAR_ID_11;
-import static se.inera.intyg.common.af00213.v1.model.converter.RespConstants.FUNKTIONSNEDSATTNING_SVAR_JSON_ID_11;
-import static se.inera.intyg.common.af00213.v1.model.converter.RespConstants.UTREDNING_BEHANDLING_DELSVAR_ID_31;
-import static se.inera.intyg.common.af00213.v1.model.converter.RespConstants.UTREDNING_BEHANDLING_SVAR_JSON_ID_31;
-import static se.inera.intyg.common.ag7804.converter.RespConstants.AVSTANGNING_SMITTSKYDD_SVAR_ID_27;
-import static se.inera.intyg.common.ag7804.converter.RespConstants.AVSTANGNING_SMITTSKYDD_SVAR_JSON_ID_27;
-import static se.inera.intyg.common.ag7804.converter.RespConstants.BEHOV_AV_SJUKSKRIVNING_SVAR_ID_32;
-import static se.inera.intyg.common.ag7804.converter.RespConstants.NO_ID;
-import static se.inera.intyg.common.ag7804.converter.RespConstants.ONSKAR_FORMEDLA_DIAGNOS_SVAR_ID_100;
-
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.Map;
 import javax.validation.constraints.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import se.inera.intyg.common.af00213.support.Af00213EntryPoint;
 import se.inera.intyg.common.ag7804.support.Ag7804EntryPoint;
-import se.inera.intyg.common.fkparent.model.converter.RespConstants;
 import se.inera.intyg.common.lisjp.support.LisjpEntryPoint;
 import se.inera.intyg.common.services.texts.IntygTextsService;
 import se.inera.intyg.common.support.common.enumerations.SignaturTyp;
 import se.inera.intyg.common.support.facade.model.Certificate;
 import se.inera.intyg.common.support.facade.model.CertificateDataElement;
 import se.inera.intyg.common.support.facade.model.CertificateStatus;
-import se.inera.intyg.common.support.facade.model.value.CertificateDataValue;
-import se.inera.intyg.common.support.facade.model.value.CertificateDataValueBoolean;
-import se.inera.intyg.common.support.facade.model.value.CertificateDataValueCode;
-import se.inera.intyg.common.support.facade.model.value.CertificateDataValueDateRange;
-import se.inera.intyg.common.support.facade.model.value.CertificateDataValueDateRangeList;
-import se.inera.intyg.common.support.facade.model.value.CertificateDataValueDiagnosis;
-import se.inera.intyg.common.support.facade.model.value.CertificateDataValueDiagnosisList;
+import se.inera.intyg.common.support.facade.model.value.*;
 import se.inera.intyg.common.support.model.UtkastStatus;
 import se.inera.intyg.common.support.model.common.internal.HoSPersonal;
 import se.inera.intyg.common.support.model.common.internal.Patient;
@@ -91,14 +70,20 @@ public class CreateCertificateTestabilityUtil {
 
     private final IntygTextsService intygTextsService;
 
-    private static final int DEFAULT_SICK_LEAVE_LENGTH = 14;
+    private final CreateLisjpTestabilityUtil createLisjpTestabilityUtil;
+
+    private final CreateAg7804TestabilityUtil createAg7804TestabilityUtil;
+
+    private final CreateAf00213TestabilityUtil createAf00213TestabilityUtil;
 
     @Autowired
     public CreateCertificateTestabilityUtil(IntygModuleRegistry moduleRegistry,
-        WebcertUserDetailsService webcertUserDetailsService,
-        PatientDetailsResolver patientDetailsResolver, UtkastService utkastService,
-        UtkastToCertificateConverter utkastToCertificateConverter, UtkastRepository utkastRepository,
-        IntygTextsService intygTextsService) {
+                                            WebcertUserDetailsService webcertUserDetailsService,
+                                            PatientDetailsResolver patientDetailsResolver, UtkastService utkastService,
+                                            UtkastToCertificateConverter utkastToCertificateConverter, UtkastRepository utkastRepository,
+                                            IntygTextsService intygTextsService, CreateLisjpTestabilityUtil createLisjpTestabilityUtil,
+                                            CreateAg7804TestabilityUtil createAg7804TestabilityUtil,
+                                            CreateAf00213TestabilityUtil createAf00213TestabilityUtil) {
         this.moduleRegistry = moduleRegistry;
         this.webcertUserDetailsService = webcertUserDetailsService;
         this.patientDetailsResolver = patientDetailsResolver;
@@ -106,6 +91,9 @@ public class CreateCertificateTestabilityUtil {
         this.utkastToCertificateConverter = utkastToCertificateConverter;
         this.utkastRepository = utkastRepository;
         this.intygTextsService = intygTextsService;
+        this.createLisjpTestabilityUtil = createLisjpTestabilityUtil;
+        this.createAg7804TestabilityUtil = createAg7804TestabilityUtil;
+        this.createAf00213TestabilityUtil = createAf00213TestabilityUtil;
     }
 
     public String createNewCertificate(@NotNull CreateCertificateRequestDTO createCertificateRequest) {
@@ -198,112 +186,30 @@ public class CreateCertificateTestabilityUtil {
 
     private Map<String, CertificateDataValue> createValues(CreateCertificateRequestDTO createCertificateRequest) {
         if (createCertificateRequest.getCertificateType().equalsIgnoreCase(LisjpEntryPoint.MODULE_ID)) {
-            return createMinimumValuesLisjp();
+            if (createCertificateRequest.getFillType() == CreateCertificateFillType.MINIMAL) {
+                return createLisjpTestabilityUtil.createMinimumValuesLisjp();
+            } else {
+                return createLisjpTestabilityUtil.createMaximumValuesLisjp();
+            }
         }
 
         if (createCertificateRequest.getCertificateType().equalsIgnoreCase(Af00213EntryPoint.MODULE_ID)) {
-            return createMinimumValuesAf00213();
+            if (createCertificateRequest.getFillType() == CreateCertificateFillType.MINIMAL) {
+                return createAf00213TestabilityUtil.createMinimumValuesAf00213();
+            } else {
+                return createAf00213TestabilityUtil.createMaximumValuesAf00213();
+            }
         }
 
         if (createCertificateRequest.getCertificateType().equalsIgnoreCase(Ag7804EntryPoint.MODULE_ID)) {
-            return createMinimumValuesAg7804();
+            if (createCertificateRequest.getFillType() == CreateCertificateFillType.MINIMAL) {
+                return createAg7804TestabilityUtil.createMinimumValuesAg7804();
+            } else {
+                return createAg7804TestabilityUtil.createMaximumValuesAg7804();
+            }
         }
 
         return Collections.emptyMap();
-    }
-
-    private Map<String, CertificateDataValue> createMinimumValuesLisjp() {
-        final var values = new HashMap<String, CertificateDataValue>();
-
-        final CertificateDataValueBoolean avstangningSmittskydd = CertificateDataValueBoolean.builder()
-            .id(RespConstants.AVSTANGNING_SMITTSKYDD_SVAR_JSON_ID_27)
-            .selected(true)
-            .build();
-        values.put(RespConstants.AVSTANGNING_SMITTSKYDD_SVAR_ID_27, avstangningSmittskydd);
-
-        final CertificateDataValueDiagnosisList diagnos = CertificateDataValueDiagnosisList.builder()
-            .list(
-                Collections.singletonList(
-                    CertificateDataValueDiagnosis.builder()
-                        .id("1")
-                        .terminology("ICD_10_SE")
-                        .code("J09")
-                        .description("Influensa orsakad av identifierat zoonotiskt eller pandemiskt influensavirus")
-                        .build()
-                )
-            )
-            .build();
-        values.put(RespConstants.DIAGNOS_SVAR_ID_6, diagnos);
-
-        final var bedomning = CertificateDataValueDateRangeList.builder()
-            .list(
-                Collections.singletonList(
-                    CertificateDataValueDateRange.builder()
-                        .id("HELT_NEDSATT")
-                        .from(LocalDate.now())
-                        .to(LocalDate.now().plusDays(DEFAULT_SICK_LEAVE_LENGTH))
-                        .build()
-                )
-            )
-            .build();
-        values.put(RespConstants.BEHOV_AV_SJUKSKRIVNING_SVAR_ID_32, bedomning);
-
-        return values;
-    }
-
-    private Map<String, CertificateDataValue> createMinimumValuesAf00213() {
-        final var values = new HashMap<String, CertificateDataValue>();
-
-        final CertificateDataValueBoolean harFunktionsnedsattning = CertificateDataValueBoolean.builder()
-            .id(FUNKTIONSNEDSATTNING_SVAR_JSON_ID_11)
-            .selected(false)
-            .build();
-        values.put(FUNKTIONSNEDSATTNING_DELSVAR_ID_11, harFunktionsnedsattning);
-
-        final CertificateDataValueBoolean harUtredningBehandling = CertificateDataValueBoolean.builder()
-            .id(UTREDNING_BEHANDLING_SVAR_JSON_ID_31)
-            .selected(false)
-            .build();
-        values.put(UTREDNING_BEHANDLING_DELSVAR_ID_31, harUtredningBehandling);
-
-        final CertificateDataValueBoolean harArbetspaverkan = CertificateDataValueBoolean.builder()
-            .id(ARBETETS_PAVERKAN_SVAR_JSON_ID_41)
-            .selected(false)
-            .build();
-        values.put(ARBETETS_PAVERKAN_DELSVAR_ID_41, harArbetspaverkan);
-
-        return values;
-    }
-
-    private Map<String, CertificateDataValue> createMinimumValuesAg7804() {
-        final var values = new HashMap<String, CertificateDataValue>();
-
-        final CertificateDataValueBoolean avstangningSmittskydd = CertificateDataValueBoolean.builder()
-            .id(AVSTANGNING_SMITTSKYDD_SVAR_JSON_ID_27)
-            .selected(true)
-            .build();
-        values.put(AVSTANGNING_SMITTSKYDD_SVAR_ID_27, avstangningSmittskydd);
-
-        final CertificateDataValueCode shouldIncludeDiagnoses = CertificateDataValueCode.builder()
-            .code(NO_ID)
-            .id(NO_ID)
-            .build();
-        values.put(ONSKAR_FORMEDLA_DIAGNOS_SVAR_ID_100, shouldIncludeDiagnoses);
-
-        final var bedomning = CertificateDataValueDateRangeList.builder()
-            .list(
-                Collections.singletonList(
-                    CertificateDataValueDateRange.builder()
-                        .id("HELT_NEDSATT")
-                        .from(LocalDate.now())
-                        .to(LocalDate.now().plusDays(DEFAULT_SICK_LEAVE_LENGTH))
-                        .build()
-                )
-            )
-            .build();
-        values.put(BEHOV_AV_SJUKSKRIVNING_SVAR_ID_32, bedomning);
-
-        return values;
     }
 
     private void updateCertificate(Certificate certificate, Map<String, CertificateDataValue> valueMap) {
