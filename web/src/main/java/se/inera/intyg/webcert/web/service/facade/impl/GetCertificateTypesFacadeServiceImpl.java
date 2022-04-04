498e313046830ca416ddcbdcdd8d21e92d7d715d
HelenaEdblad <helena.edblad@soprasteria.com>
2022-04-04 12:05:44 +0200

    INTYGFV-14461: Create certicicate types controller and service

---
+++ web/src/main/java/se/inera/intyg/webcert/web/service/facade/impl/GetCertificateTypesFacadeServiceImpl.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright (C) 2022 Inera AB (http://www.inera.se)
+ *
+ * This file is part of sklintyg (https://github.com/sklintyg).
+ *
+ * sklintyg is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * sklintyg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package se.inera.intyg.webcert.web.service.facade.impl;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import se.inera.intyg.common.support.modules.registry.IntygModuleRegistry;
+import se.inera.intyg.webcert.web.web.controller.facade.dto.CertificateTypeInfoDTO;
+import se.inera.intyg.schemas.contract.InvalidPersonNummerException;
+import se.inera.intyg.schemas.contract.Personnummer;
+import se.inera.intyg.webcert.web.service.facade.GetCertificateTypesFacadeService;
+import se.inera.intyg.webcert.web.web.controller.api.dto.IntygModuleDTO;
+import se.inera.intyg.webcert.web.web.util.resourcelinks.ResourceLinkHelper;
+
+@Service
+public class GetCertificateTypesFacadeServiceImpl implements GetCertificateTypesFacadeService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(GetCertificateTypesFacadeServiceImpl.class);
+
+    private final IntygModuleRegistry intygModuleRegistry;
+    private final ResourceLinkHelper resourceLinkHelper;
+
+
+    @Autowired
+    public GetCertificateTypesFacadeServiceImpl(IntygModuleRegistry intygModuleRegistry, ResourceLinkHelper resourceLinkHelper) {
+        this.intygModuleRegistry = intygModuleRegistry;
+        this.resourceLinkHelper = resourceLinkHelper;
+    }
+
+    @Override
+    public List<CertificateTypeInfoDTO> get(String patientId) {
+        final var certificateModuleList = getCertificateModuleList(patientId);
+        return certificateModuleList.stream().map(this::convertModuleToTypeInfo).collect(Collectors.toList());
+    }
+
+    private CertificateTypeInfoDTO convertModuleToTypeInfo(IntygModuleDTO module) {
+        final var certificateTypeInfo = new CertificateTypeInfoDTO();
+        certificateTypeInfo.setId(module.getId());
+        certificateTypeInfo.setLabel(module.getLabel());
+        certificateTypeInfo.setDescription(module.getDescription());
+        certificateTypeInfo.setDetailedDescription(module.getDetailedDescription());
+        certificateTypeInfo.setIssuerTypeId(module.getIssuerTypeId());
+        certificateTypeInfo.setLinks(module.getLinks());
+        return certificateTypeInfo;
+    }
+
+    private List<IntygModuleDTO> getCertificateModuleList(String patientId) {
+        try {
+            final var intygModules = intygModuleRegistry.listAllModules();
+
+            final var intygModuleDTOs = intygModules.stream()
+                .map(IntygModuleDTO::new)
+                .collect(Collectors.toList());
+
+            final var personnummer = createPnr(patientId);
+            resourceLinkHelper.decorateIntygModuleWithValidActionLinks(intygModuleDTOs, personnummer);
+
+            return intygModuleDTOs;
+
+        } catch (InvalidPersonNummerException e) {
+            LOG.error(e.getMessage());
+            return Collections.emptyList();
+        }
+    }
+
+    private Personnummer createPnr(String personId) throws InvalidPersonNummerException {
+        return Personnummer.createPersonnummer(personId)
+            .orElseThrow(() -> new InvalidPersonNummerException("Could not parse personnummer: " + personId));
+    }
+}
---
+++ web/src/main/java/se/inera/intyg/webcert/web/web/controller/facade/dto/CertificateTypeInfoDTO.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright (C) 2022 Inera AB (http://www.inera.se)
+ *
+ * This file is part of sklintyg (https://github.com/sklintyg).
+ *
+ * sklintyg is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * sklintyg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package se.inera.intyg.webcert.web.web.controller.facade.dto;
+
+import java.util.List;
+import se.inera.intyg.webcert.web.web.util.resourcelinks.dto.ActionLink;
+
+public class CertificateTypeInfoDTO {
+    private String id;
+    private String label;
+    private String issuerTypeId;
+    private String description;
+    private String detailedDescription;
+    private List<ActionLink> links;
+
+    public CertificateTypeInfoDTO() { }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public String getIssuerTypeId() {
+        return issuerTypeId;
+    }
+
+    public void setIssuerTypeId(String issuerTypeId) {
+        this.issuerTypeId = issuerTypeId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDetailedDescription() {
+        return detailedDescription;
+    }
+
+    public void setDetailedDescription(String detailedDescription) {
+        this.detailedDescription = detailedDescription;
+    }
+
+    public List<ActionLink> getLinks() {
+        return links;
+    }
+
+    public void setLinks(List<ActionLink> links) {
+        this.links = links;
+    }
+}
