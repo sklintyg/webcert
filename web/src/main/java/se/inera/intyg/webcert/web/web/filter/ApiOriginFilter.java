2c57e9cc9425866c8820fbb74f144e40a9744dbc
Matias Wurschmidt-Wang <46641204+mww-sopra-steria@users.noreply.github.com>
2022-04-27 11:00:00 +0200

    DEVENV: Test to allow cross-origin with Cerner. Will not go to production. (#677)

---
+++ web/src/main/java/se/inera/intyg/webcert/web/web/filter/ApiOriginFilter.java
@@ -0,0 +1,31 @@
+package se.inera.intyg.webcert.web.web.filter;
+
+import java.io.IOException;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
+public class ApiOriginFilter implements javax.servlet.Filter {
+
+  @Override
+  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+      throws IOException, ServletException {
+    final var res = (HttpServletResponse) response;
+    res.addHeader("Access-Control-Allow-Origin", "*");
+    res.addHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
+    res.addHeader("Access-Control-Allow-Headers", "Content-Type, api_key, Authorization");
+    res.addHeader("Access-Control-Allow-Credentials", "true");
+    chain.doFilter(request, response);
+  }
+
+  @Override
+  public void destroy() {
+  }
+
+  @Override
+  public void init(FilterConfig filterConfig) throws ServletException {
+  }
+}
