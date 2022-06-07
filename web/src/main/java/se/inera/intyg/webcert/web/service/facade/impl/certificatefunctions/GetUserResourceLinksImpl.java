8b0896c45b56175a6fb89ecdf68c503167807c06
HelenaEdblad <helena.edblad@soprasteria.com>
2022-06-07 14:51:22 +0200

    INTYGFV-14454: Add resource link for changing unit

--- web/src/main/java/se/inera/intyg/webcert/web/service/facade/impl/certificatefunctions/GetUserResourceLinksImpl.java
+++ web/src/main/java/se/inera/intyg/webcert/web/service/facade/impl/certificatefunctions/GetUserResourceLinksImpl.java
@@ -18,14 +18,13 @@
  */
 package se.inera.intyg.webcert.web.service.facade.impl.certificatefunctions;
 
+import java.util.ArrayList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import se.inera.intyg.webcert.web.service.facade.GetUserResourceLinks;
 import se.inera.intyg.webcert.web.service.user.dto.WebCertUser;
 import se.inera.intyg.webcert.web.web.controller.facade.dto.ResourceLinkDTO;
 import se.inera.intyg.webcert.web.web.controller.facade.dto.ResourceLinkTypeDTO;
-
-import java.util.ArrayList;
 
 @Service
 public class GetUserResourceLinksImpl implements GetUserResourceLinks {
@@ -45,48 +44,64 @@ public class GetUserResourceLinksImpl implements GetUserResourceLinks {
 
         if (hasAccessToSearchCreatePage(user)) {
             resourceLinks.add(
-                    ResourceLinkDTO.create(
-                            ResourceLinkTypeDTO.ACCESS_SEARCH_CREATE_PAGE,
-                            "Sök / skriv intyg",
-                            "",
-                            true
-                    )
+                ResourceLinkDTO.create(
+                    ResourceLinkTypeDTO.ACCESS_SEARCH_CREATE_PAGE,
+                    "Sök / skriv intyg",
+                    "",
+                    true
+                )
             );
         }
 
         if (hasAccessToDraftList(user)) {
             resourceLinks.add(
-                    ResourceLinkDTO.create(
-                            ResourceLinkTypeDTO.ACCESS_DRAFT_LIST,
-                            "Ej signerade utkast",
-                            "",
-                            true
-                    )
+                ResourceLinkDTO.create(
+                    ResourceLinkTypeDTO.ACCESS_DRAFT_LIST,
+                    "Ej signerade utkast",
+                    "",
+                    true
+                )
             );
         }
 
         if (hasAccessToSignedCertificatesList(user)) {
             resourceLinks.add(
-                    ResourceLinkDTO.create(
-                            ResourceLinkTypeDTO.ACCESS_SIGNED_CERTIFICATES_LIST,
-                            "Signerade intyg",
-                            "",
-                            true
-                    )
+                ResourceLinkDTO.create(
+                    ResourceLinkTypeDTO.ACCESS_SIGNED_CERTIFICATES_LIST,
+                    "Signerade intyg",
+                    "",
+                    true
+                )
             );
         }
 
         if (isLogOutAvailable(user)) {
             resourceLinks.add(
-                    ResourceLinkDTO.create(
-                            ResourceLinkTypeDTO.LOG_OUT,
-                            "Logga ut",
-                            "",
-                            true
-                    )
+                ResourceLinkDTO.create(
+                    ResourceLinkTypeDTO.LOG_OUT,
+                    "Logga ut",
+                    "",
+                    true
+                )
             );
         }
+
+        if (isChangeUnitAvailable(user)) {
+            resourceLinks.add(
+                ResourceLinkDTO.create(
+                    ResourceLinkTypeDTO.CHANGE_UNIT,
+                    "Byta vårdenhet",
+                    "",
+                    true
+                )
+            );
+        }
+
         return resourceLinks;
+    }
+
+    private boolean isChangeUnitAvailable(WebCertUser user) {
+        return isOriginNormal(user.getOrigin()) || isOriginUthopp(user.getOrigin());
     }
 
     private boolean hasAccessToSearchCreatePage(WebCertUser user) {
