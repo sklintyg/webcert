08d1b072ab5ee110cfed73d78de30cefeca2d939
mhornfeldt <michaela.hornfeldt@soprasteria.com>
2022-06-14 11:08:48 +0200

    INTYGFV-14508: Fixed and added tests for when patient has no name in PU

--- web/src/main/java/se/inera/intyg/webcert/web/service/facade/patient/GetPatientFacadeServiceImpl.java
+++ web/src/main/java/se/inera/intyg/webcert/web/service/facade/patient/GetPatientFacadeServiceImpl.java
@@ -58,7 +58,7 @@ public class GetPatientFacadeServiceImpl implements GetPatientFacadeService {
             throw new PatientSearchErrorException();
         }
 
-        if (personSvar.getPerson().getFornamn() == null || personSvar.getPerson().getEfternamn() == null) {
+        if (personSvar.getPerson() != null && (personSvar.getPerson().getFornamn() == null || personSvar.getPerson().getEfternamn() == null)) {
             throw new PatientNoNameException();
         }
 
