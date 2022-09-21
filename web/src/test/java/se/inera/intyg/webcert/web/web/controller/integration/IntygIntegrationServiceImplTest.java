6ecc77392c6d0acc74a0af3d83df49f2217b1ee2
SIGERHED Christoffer <christoffer.sigerhed@soprasteria.com>
2022-09-21 10:43:43 +0200

    INTYGFV-14695: removed whitespace that caused PR-error

--- web/src/test/java/se/inera/intyg/webcert/web/web/controller/integration/IntygIntegrationServiceImplTest.java
+++ web/src/test/java/se/inera/intyg/webcert/web/web/controller/integration/IntygIntegrationServiceImplTest.java
@@ -18,6 +18,26 @@
  */
 package se.inera.intyg.webcert.web.web.controller.integration;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -26,7 +46,12 @@ import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 import se.inera.intyg.common.support.model.UtkastStatus;
 import se.inera.intyg.infra.integration.hsatk.model.legacy.SelectableVardenhet;
-import se.inera.intyg.infra.security.common.model.*;
+import se.inera.intyg.infra.security.common.model.AuthoritiesConstants;
+import se.inera.intyg.infra.security.common.model.Feature;
+import se.inera.intyg.infra.security.common.model.Privilege;
+import se.inera.intyg.infra.security.common.model.RequestOrigin;
+import se.inera.intyg.infra.security.common.model.Role;
+import se.inera.intyg.infra.security.common.model.UserOriginType;
 import se.inera.intyg.schemas.contract.Personnummer;
 import se.inera.intyg.webcert.common.model.SekretessStatus;
 import se.inera.intyg.webcert.common.service.exception.WebCertServiceException;
@@ -43,17 +68,6 @@ import se.inera.intyg.webcert.web.test.TestIntygFactory;
 import se.inera.intyg.webcert.web.web.controller.api.dto.IntygTypeInfo;
 import se.inera.intyg.webcert.web.web.controller.integration.dto.IntegrationParameters;
 import se.inera.intyg.webcert.web.web.controller.integration.dto.PrepareRedirectToIntyg;
-
-import java.time.LocalDateTime;
-import java.util.*;
-import java.util.function.Function;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
-import static org.junit.Assert.*;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.*;
 
 /**
  * @author Magnus Ekstrand on 2017-10-13.
@@ -255,7 +269,7 @@ public class IntygIntegrationServiceImplTest {
 
         IntegrationParameters parameters = new IntegrationParameters(null, null, ALTERNATE_SSN,
             "Nollan", null, "Nollansson", "Nollgatan", "000000", "Nollby",
-            true, false, false, false , null);
+            true, false, false, false, null);
 
         WebCertUser user = createDefaultUser();
         user.setParameters(parameters);
