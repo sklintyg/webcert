196650f9eff94245a8498f3d9a269b69ef06bee2
SIGERHED Christoffer <christoffer.sigerhed@soprasteria.com>
2022-10-24 13:09:41 +0200

    INTYGFV-14815: Fixed errors that reviewdog complained about.

--- web/src/test/java/se/inera/intyg/webcert/web/service/facade/impl/GetCertificateTypesFacadeServiceImplTest.java
+++ web/src/test/java/se/inera/intyg/webcert/web/service/facade/impl/GetCertificateTypesFacadeServiceImplTest.java
@@ -75,8 +75,8 @@ class GetCertificateTypesFacadeServiceImplTest {
     @InjectMocks
     private GetCertificateTypesFacadeServiceImpl serviceUnderTest;
 
-    private final static String CERTIFICATE_TYPE = "id";
-    private final static Personnummer PATIENT_ID = Personnummer.createPersonnummer("19121212-1212").get();
+    private static final String CERTIFICATE_TYPE = "id";
+    private static final Personnummer PATIENT_ID = Personnummer.createPersonnummer("19121212-1212").get();
 
     @Nested
     class CorrectCases {
