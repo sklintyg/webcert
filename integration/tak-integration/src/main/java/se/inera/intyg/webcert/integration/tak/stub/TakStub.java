91142575a929d323e9da7f4b8d16db134e185aac
mikeyOne <55154460+mikeyOne@users.noreply.github.com>
2023-01-03 11:24:40 +0100

    INTYGFV-15146: Updated year in license header to 2023. (#827)

--- integration/tak-integration/src/main/java/se/inera/intyg/webcert/integration/tak/stub/TakStub.java
+++ integration/tak-integration/src/main/java/se/inera/intyg/webcert/integration/tak/stub/TakStub.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2022 Inera AB (http://www.inera.se)
+ * Copyright (C) 2023 Inera AB (http://www.inera.se)
  *
  * This file is part of sklintyg (https://github.com/sklintyg).
  *
