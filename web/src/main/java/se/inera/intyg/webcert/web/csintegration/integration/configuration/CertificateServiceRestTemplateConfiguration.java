ecc2f2cfa7dce44d1e082f6efc0952b712c8b259
cigerhed <christoffer.sigerhed@soprasteria.com>
2024-01-29 09:50:33 +0100

    INTYGFV-16332: Renamed RestTemplateConfiguration -> CertificateServiceRestTemplateConfiguration

--- web/src/main/java/se/inera/intyg/webcert/web/csintegration/integration/configuration/CertificateServiceRestTemplateConfiguration.java
+++ web/src/main/java/se/inera/intyg/webcert/web/csintegration/integration/configuration/CertificateServiceRestTemplateConfiguration.java
@@ -24,7 +24,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
-public class RestTemplateConfiguration {
+public class CertificateServiceRestTemplateConfiguration {
 
     @Bean("csRestTemplate")
     public RestTemplate csRestTemplate() {
