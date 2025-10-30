f24f03b93d3c994945c41958773811006b968f87
Alexey Ryabov <alexey.ryabov@soprasteria.com>
2025-10-30 07:55:59 +0100

    K1J-1379: Fix

--- web/src/main/java/se/inera/intyg/webcert/web/service/srs/SrsCertificateExtensionChainService.java
+++ web/src/main/java/se/inera/intyg/webcert/web/service/srs/SrsCertificateExtensionChainService.java
@@ -4,15 +4,25 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import se.inera.intyg.infra.integration.srs.model.SrsCertificate;
+import se.inera.intyg.webcert.web.csintegration.aggregate.GetCertificateAggregator;
 
 @Service("srsCertificateExtensionChainService")
 @RequiredArgsConstructor
 public class SrsCertificateExtensionChainService {
 
+  //private final GetCertificateAggregator getCertificateAggregator;
   private final GetSrsCertificateAggregator getSrsCertificateAggregator;
 
   public List<SrsCertificate> get(String certificateId) {
-    return getSrsCertificateAggregator.getSrsCertificateList(certificateId);
+    /*
+    final var certificate = getCertificateAggregator.getCertificate(certificateId, false, true);
+    final var certificateParentId = certificate.getMetadata().getRelations().getParent() != null ?
+        certificate.getMetadata().getRelations().getParent().getCertificateId() : null;
+
+     */
+
+    final var resultList = getSrsCertificateAggregator.getSrsCertificateList(certificateId);
+    return resultList;
   }
 
 }
