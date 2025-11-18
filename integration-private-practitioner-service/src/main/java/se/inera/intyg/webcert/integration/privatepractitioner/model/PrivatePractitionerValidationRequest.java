036b28ea76a910a86b42d7ace48f491a0556285a
Matias Wurschmidt-Wang <matias.wurschmidt@soprasteria.com>
2025-11-18 06:31:33 +0100

    K1J-1594: Update PrivatePractitionerValidationRequest to match private-practitioner-service api

--- integration-private-practitioner-service/src/main/java/se/inera/intyg/webcert/integration/privatepractitioner/model/PrivatePractitionerValidationRequest.java
+++ integration-private-practitioner-service/src/main/java/se/inera/intyg/webcert/integration/privatepractitioner/model/PrivatePractitionerValidationRequest.java
@@ -9,6 +9,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class PrivatePractitionerValidationRequest {
 
-    private String personalIdentityNumber;
+    private String personId;
 
 }
