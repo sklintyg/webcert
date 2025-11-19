2354775aca272f3449ab65186d40551c78ed5dbb
Daniel Jönsson <daniel.joensson@soprasteria.com>
2025-11-19 19:56:34 +0100

    K1J-1613: Fix tests for pp update request converter.

--- web/src/main/java/se/inera/intyg/webcert/web/privatepractitioner/converter/PrivatePractitionerUpdateRequestConverter.java
+++ web/src/main/java/se/inera/intyg/webcert/web/privatepractitioner/converter/PrivatePractitionerUpdateRequestConverter.java
@@ -1,14 +1,30 @@
 package se.inera.intyg.webcert.web.privatepractitioner.converter;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import se.inera.intyg.webcert.integration.privatepractitioner.dto.PrivatePractitionerDetailsRequest;
+import se.inera.intyg.webcert.web.service.user.WebCertUserService;
+import se.inera.intyg.webcert.web.web.controller.api.dto.privatepractitioner.PrivatePractitionerDetails;
 
 @Component
+@RequiredArgsConstructor
 public class PrivatePractitionerUpdateRequestConverter {
 
-    public PrivatePractitionerDetailsRequest convert(PrivatePractitionerDetailsRequest privatePractitionerUpdateRequest) {
-        return UpdatePrivatePractitionerRequest.builder()
-            .personId(privatePractitionerUpdateRequest.getPersonId())
+    private final WebCertUserService webCertUserService;
+
+    public PrivatePractitionerDetailsRequest convert(PrivatePractitionerDetails privatePractitionerUpdateRequest) {
+        final var webCertUser = webCertUserService.getUser();
+
+        if (privatePractitionerUpdateRequest == null) {
+            throw new IllegalArgumentException("PrivatePractitionerDetails cannot be null");
+        }
+        if (webCertUser == null) {
+            throw new IllegalStateException("WebCertUser cannot be null");
+        }
+
+        return PrivatePractitionerDetailsRequest.builder()
+            .personId(webCertUser.getPersonId())
+            .name(webCertUser.getNamn())
             .position(privatePractitionerUpdateRequest.getPosition())
             .workplaceCode(privatePractitionerUpdateRequest.getWorkplaceCode())
             .careUnitName(privatePractitionerUpdateRequest.getCareUnitName())
--- web/src/test/java/se/inera/intyg/webcert/web/privatepractitioner/converter/PrivatePractitionerUpdateRequestConverterTest.java
+++ web/src/test/java/se/inera/intyg/webcert/web/privatepractitioner/converter/PrivatePractitionerUpdateRequestConverterTest.java
@@ -1,27 +1,62 @@
 package se.inera.intyg.webcert.web.privatepractitioner.converter;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
+import static se.inera.intyg.webcert.web.privatepractitioner.TestDataConstants.DR_KRANSTEGE_HSA_ID;
+import static se.inera.intyg.webcert.web.privatepractitioner.TestDataConstants.DR_KRANSTEGE_NAME;
+import static se.inera.intyg.webcert.web.privatepractitioner.TestDataConstants.DR_KRANSTEGE_PERSON_ID;
 import static se.inera.intyg.webcert.web.privatepractitioner.TestDataDTO.DR_KRANSTEGE_UPDATE_REQUEST_DTO;
 import static se.inera.intyg.webcert.web.privatepractitioner.TestDataDTO.DR_KRANSTEGE_UPDATE_REQUEST_INTEGRATION_DTO;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import se.inera.intyg.webcert.web.service.user.WebCertUserService;
+import se.inera.intyg.webcert.web.service.user.dto.WebCertUser;
 
+@ExtendWith(MockitoExtension.class)
 class PrivatePractitionerUpdateRequestConverterTest {
 
-    private final PrivatePractitionerUpdateRequestConverter converter =
-        new PrivatePractitionerUpdateRequestConverter();
+    @Mock
+    private WebCertUserService webCertUserService;
+
+    private PrivatePractitionerUpdateRequestConverter converter;
+
+    @BeforeEach
+    void setUp() {
+        converter = new PrivatePractitionerUpdateRequestConverter(webCertUserService);
+    }
+
+    private void mockUser() {
+        final var user = new WebCertUser();
+        user.setPersonId(DR_KRANSTEGE_PERSON_ID);
+        user.setNamn(DR_KRANSTEGE_NAME);
+        user.setHsaId(DR_KRANSTEGE_HSA_ID);
+        when(webCertUserService.getUser()).thenReturn(user);
+    }
 
     @Test
     void shouldConvertUpdateRequest() {
+        mockUser();
         final var result = converter.convert(DR_KRANSTEGE_UPDATE_REQUEST_DTO);
 
         assertEquals(DR_KRANSTEGE_UPDATE_REQUEST_INTEGRATION_DTO, result);
     }
 
     @Test
-    void shouldConvertUpdateRequestWithNulls() {
-        final var result = converter.convert(null);
+    void shouldThrowExceptionWhenInputIsNull() {
+        mockUser();
 
-        assertEquals(null, result);
+        assertThrows(IllegalArgumentException.class, () -> converter.convert(null));
+    }
+
+    @Test
+    void shouldThrowExceptionWhenUserIsNull() {
+        when(webCertUserService.getUser()).thenReturn(null);
+
+        assertThrows(IllegalStateException.class, () -> converter.convert(DR_KRANSTEGE_UPDATE_REQUEST_DTO));
     }
 }
