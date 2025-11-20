ae32ffad45b4dc4948923e71823e966caa28799a
Erik Ågren <erik.agren@soprasteria.com>
2025-11-20 10:28:01 +0100

    K1J-1606: Enhance HandleStaleDraftsService to delete related events and entities for stale drafts

--- web/src/main/java/se/inera/intyg/webcert/web/service/utkast/HandleStaleDraftsService.java
+++ web/src/main/java/se/inera/intyg/webcert/web/service/utkast/HandleStaleDraftsService.java
@@ -7,6 +7,10 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import se.inera.intyg.webcert.persistence.event.repository.CertificateEventFailedLoadRepository;
+import se.inera.intyg.webcert.persistence.event.repository.CertificateEventProcessedRepository;
+import se.inera.intyg.webcert.persistence.event.repository.CertificateEventRepository;
+import se.inera.intyg.webcert.persistence.handelse.repository.HandelseRepository;
 import se.inera.intyg.webcert.persistence.utkast.model.Utkast;
 import se.inera.intyg.webcert.persistence.utkast.repository.UtkastRepository;
 import se.inera.intyg.webcert.web.service.monitoring.MonitoringLogService;
@@ -19,6 +23,10 @@ public class HandleStaleDraftsService {
     private final NotificationService notificationService;
     private final UtkastRepository utkastRepository;
     private final MonitoringLogService monitoringLogService;
+    private final HandelseRepository handelseRepository;
+    private final CertificateEventRepository certificateEventRepository;
+    private final CertificateEventFailedLoadRepository certificateEventFailedLoadRepository;
+    private final CertificateEventProcessedRepository certificateEventProcessedRepository;
 
     @Transactional
     public void deleteAndNotify(List<Utkast> drafts, LocalDateTime staleDraftsPeriod) {
@@ -26,7 +34,12 @@ public class HandleStaleDraftsService {
             .map(Utkast::getIntygsId)
             .toList();
 
+        handelseRepository.eraseHandelseByCertificateIds(certificateIds);
+        certificateEventProcessedRepository.eraseEventsProcessedByCertificateIds(certificateIds);
+        certificateEventFailedLoadRepository.eraseEventsFailedByCertificateIds(certificateIds);
+        certificateEventRepository.eraseCertificateEventsByCertificateIds(certificateIds);
         utkastRepository.deleteAllById(certificateIds);
+
         drafts.forEach(notificationService::sendNotificationForDraftDeleted);
 
         final var period = ChronoUnit.DAYS.between(staleDraftsPeriod.toLocalDate(), LocalDate.now());
--- web/src/test/java/se/inera/intyg/webcert/web/service/utkast/HandleStaleDraftsServiceTest.java
+++ web/src/test/java/se/inera/intyg/webcert/web/service/utkast/HandleStaleDraftsServiceTest.java
@@ -13,6 +13,10 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import se.inera.intyg.webcert.persistence.event.repository.CertificateEventFailedLoadRepository;
+import se.inera.intyg.webcert.persistence.event.repository.CertificateEventProcessedRepository;
+import se.inera.intyg.webcert.persistence.event.repository.CertificateEventRepository;
+import se.inera.intyg.webcert.persistence.handelse.repository.HandelseRepository;
 import se.inera.intyg.webcert.persistence.utkast.model.Utkast;
 import se.inera.intyg.webcert.persistence.utkast.repository.UtkastRepository;
 import se.inera.intyg.webcert.web.service.monitoring.MonitoringLogService;
@@ -23,7 +27,7 @@ class HandleStaleDraftsServiceTest {
 
     private static final String CERTIFICATE_TYPE = "certificateType";
     private static final LocalDateTime STALE_DRAFTS_PERIOD = LocalDateTime.now().minusMonths(3);
-    private static final long PERIOD = ChronoUnit.DAYS.between(LocalDate.now(), STALE_DRAFTS_PERIOD.toLocalDate());
+    private static final long PERIOD = ChronoUnit.DAYS.between(STALE_DRAFTS_PERIOD.toLocalDate(), LocalDate.now());
     @Mock
     private MonitoringLogService monitoringLogService;
     @Mock
@@ -31,6 +35,18 @@ class HandleStaleDraftsServiceTest {
 
     @Mock
     private UtkastRepository utkastRepository;
+
+    @Mock
+    private HandelseRepository handelseRepository;
+
+    @Mock
+    private CertificateEventRepository certificateEventRepository;
+
+    @Mock
+    private CertificateEventFailedLoadRepository certificateEventFailedLoadRepository;
+
+    @Mock
+    private CertificateEventProcessedRepository certificateEventProcessedRepository;
 
     @InjectMocks
     private HandleStaleDraftsService handleStaleDraftsService;
@@ -94,6 +110,57 @@ class HandleStaleDraftsServiceTest {
 
         verify(monitoringLogService).logUtkastPruned(certificateId1, CERTIFICATE_TYPE, PERIOD);
         verify(monitoringLogService).logUtkastPruned(certificateId2, CERTIFICATE_TYPE, PERIOD);
+    }
+
+    @Test
+    void shouldDeleteHandelse(){
+        final var certificateId1 = "cert-id-1";
+        final var certificateId2 = "cert-id-2";
+        final var draft1 = createUtkast(certificateId1);
+        final var draft2 = createUtkast(certificateId2);
+        final var drafts = List.of(draft1, draft2);
+
+        handleStaleDraftsService.deleteAndNotify(drafts, STALE_DRAFTS_PERIOD);
+
+        verify(handelseRepository).eraseHandelseByCertificateIds(List.of(certificateId1, certificateId2));
+    }
+
+    @Test
+    void shouldDeleteCertificateProcessedEvents(){
+        final var certificateId1 = "cert-id-1";
+        final var certificateId2 = "cert-id-2";
+        final var draft1 = createUtkast(certificateId1);
+        final var draft2 = createUtkast(certificateId2);
+        final var drafts = List.of(draft1, draft2);
+
+        handleStaleDraftsService.deleteAndNotify(drafts, STALE_DRAFTS_PERIOD);
+
+        verify(certificateEventProcessedRepository).eraseEventsProcessedByCertificateIds(List.of(certificateId1, certificateId2));
+    }
+
+    @Test
+    void shouldDeleteCertificateFailedLoadEvents(){
+        final var certificateId1 = "cert-id-1";
+        final var certificateId2 = "cert-id-2";
+        final var draft1 = createUtkast(certificateId1);
+        final var draft2 = createUtkast(certificateId2);
+        final var drafts = List.of(draft1, draft2);
+
+        handleStaleDraftsService.deleteAndNotify(drafts, STALE_DRAFTS_PERIOD);
+
+        verify(certificateEventFailedLoadRepository).eraseEventsFailedByCertificateIds(List.of(certificateId1, certificateId2));
+    }
+    @Test
+    void shouldDeleteCertificateEvents(){
+        final var certificateId1 = "cert-id-1";
+        final var certificateId2 = "cert-id-2";
+        final var draft1 = createUtkast(certificateId1);
+        final var draft2 = createUtkast(certificateId2);
+        final var drafts = List.of(draft1, draft2);
+
+        handleStaleDraftsService.deleteAndNotify(drafts, STALE_DRAFTS_PERIOD);
+
+        verify(certificateEventRepository).eraseCertificateEventsByCertificateIds(List.of(certificateId1, certificateId2));
     }
 
     private Utkast createUtkast(String certificateId) {
