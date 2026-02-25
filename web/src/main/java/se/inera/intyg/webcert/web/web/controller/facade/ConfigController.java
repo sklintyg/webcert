554f3aadc99b521fa572069c937a3e78659afc6f
Matias Wurschmidt-Wang <46641204+mww-sopra-steria@users.noreply.github.com>
2026-02-25 15:23:07 +0100

    K1J-1884: Return idpConnectUrls for frontend configuration api. No idp.connect.urls should lead to empty list of urls. (#1295)
    (cherry picked from commit 0489d0fd6f89776f65642e7762e8b9d9bc90e1f9)

--- web/src/main/java/se/inera/intyg/webcert/web/web/controller/facade/ConfigController.java
+++ web/src/main/java/se/inera/intyg/webcert/web/web/controller/facade/ConfigController.java
@@ -24,6 +24,8 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -59,6 +61,9 @@ public class ConfigController {
     @Value("${forward.draft.or.question.url}")
     private String forwardDraftOrQuestionUrl;
 
+    @Value("${idp.connect.urls:}")
+    private String idpConnectUrls;
+
     @Autowired
     private DynamicLinkService dynamicLinkService;
 
@@ -79,13 +84,27 @@ public class ConfigController {
         if (log.isDebugEnabled()) {
             log.debug("Getting configuration");
         }
-        final var banners = iaBannerService.getCurrentBanners()
-            .stream()
+
+        final var banners = iaBannerService.getCurrentBanners().stream()
             .filter((banner -> banner.getApplication() == Application.WEBCERT))
             .toList();
 
-        return Response.ok(new ConfigurationDTO(version, banners, ppHost, sakerhetstjanstIdpUrl, cgiFunktionstjansterIdpUrl,
-            forwardDraftOrQuestionUrl)).build();
+        return Response.ok(
+            ConfigurationDTO.builder()
+                .version(version)
+                .banners(banners)
+                .ppHost(ppHost)
+                .sakerhetstjanstIdpUrl(sakerhetstjanstIdpUrl)
+                .cgiFunktionstjansterIdpUrl(cgiFunktionstjansterIdpUrl)
+                .forwardDraftOrQuestionUrl(forwardDraftOrQuestionUrl)
+                .idpConnectUrls(
+                    idpConnectUrls == null ? List.of() :
+                        Arrays.stream(idpConnectUrls.split(","))
+                            .filter(url -> !url.isBlank())
+                            .toList()
+                )
+                .build()
+        ).build();
     }
 
     @GET
