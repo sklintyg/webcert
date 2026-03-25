31983d77bf18b5c469887fa9565b82d6e10639bd
Daniel Jönsson <daniel.joensson@soprasteria.com>
2026-03-25 14:02:29 +0100

    K1J-1524: Add missing resources and test classes.

---
+++ infra/src/test/java/se/inera/intyg/webcert/infra/dynamiclink/service/LinksServiceTest.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2025 Inera AB (http://www.inera.se)
+ *
+ * This file is part of sklintyg (https://github.com/sklintyg).
+ *
+ * sklintyg is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * sklintyg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package se.inera.intyg.webcert.infra.dynamiclink.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import se.inera.intyg.webcert.infra.dynamiclink.model.DynamicLink;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = TestConfig.class)
+class LinksServiceTest {
+
+  @Autowired private DynamicLinkService dynamicLinkService;
+
+  @Test
+  void testLoadAndGetLink() {
+    List<DynamicLink> allAsList = dynamicLinkService.getAllAsList();
+    assertEquals(1, allAsList.size());
+  }
+}
