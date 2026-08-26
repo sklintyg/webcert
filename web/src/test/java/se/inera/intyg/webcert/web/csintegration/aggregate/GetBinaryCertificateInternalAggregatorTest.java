3234e7a10dac73fbd0529c70a6e0fc72469cd62a
Christoffer Sigerhed <christoffer.sigerhed@gmail.com>
2026-08-26 13:12:17 +0200

    K1J-2315: Implement aggregator to verify where certificate exists. (#1346)

---
+++ web/src/test/java/se/inera/intyg/webcert/web/csintegration/aggregate/GetBinaryCertificateInternalAggregatorTest.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright (C) 2026 Inera AB (http://www.inera.se)
+ *
+ * This file is part of sklintyg (https://github.com/sklintyg).
+ *
+ * sklintyg is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * sklintyg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package se.inera.intyg.webcert.web.csintegration.aggregate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verifyNoInteractions;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import se.inera.intyg.webcert.web.web.controller.internalapi.GetBinaryCertificate;
+import se.inera.intyg.webcert.web.web.controller.internalapi.dto.GetBinaryCertificateResponseDTO;
+
+@ExtendWith(MockitoExtension.class)
+class GetBinaryCertificateInternalAggregatorTest {
+
+  private static final String CERTIFICATE_ID = "certificateId";
+
+  @Mock GetBinaryCertificate getBinaryCertificateFromCS;
+
+  @Mock GetBinaryCertificate getBinaryCertificateFromWC;
+
+  private GetBinaryCertificateInternalAggregator aggregator;
+
+  @BeforeEach
+  void setUp() {
+    aggregator =
+        new GetBinaryCertificateInternalAggregator(
+            getBinaryCertificateFromCS, getBinaryCertificateFromWC);
+  }
+
+  @Test
+  void shouldReturnResponseFromCS() {
+    final var expectedResponse = GetBinaryCertificateResponseDTO.builder().build();
+    when(getBinaryCertificateFromCS.get(CERTIFICATE_ID)).thenReturn(expectedResponse);
+
+    final var actualResponse = aggregator.get(CERTIFICATE_ID);
+
+    assertEquals(expectedResponse, actualResponse);
+    verifyNoInteractions(getBinaryCertificateFromWC);
+  }
+
+  @Test
+  void shouldReturnResponseFromWC() {
+    final var expectedResponse = GetBinaryCertificateResponseDTO.builder().build();
+    when(getBinaryCertificateFromCS.get(CERTIFICATE_ID)).thenReturn(null);
+    when(getBinaryCertificateFromWC.get(CERTIFICATE_ID)).thenReturn(expectedResponse);
+
+    final var actualResponse = aggregator.get(CERTIFICATE_ID);
+
+    assertEquals(expectedResponse, actualResponse);
+  }
+}
