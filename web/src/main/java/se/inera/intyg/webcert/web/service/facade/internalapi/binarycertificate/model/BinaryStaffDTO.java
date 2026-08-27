b9cdf3e9517237bbe18cc6b09d60964e73867875
ChrilleSigerhed <christoffer.sigerhed@gmail.com>
2026-08-27 21:27:55 +0200

    K1J-2316: Staff should only use fullName and not first, middle and last name.

--- web/src/main/java/se/inera/intyg/webcert/web/service/facade/internalapi/binarycertificate/model/BinaryStaffDTO.java
+++ web/src/main/java/se/inera/intyg/webcert/web/service/facade/internalapi/binarycertificate/model/BinaryStaffDTO.java
@@ -31,9 +31,7 @@ import tools.jackson.databind.annotation.JsonPOJOBuilder;
 public class BinaryStaffDTO {
 
   String personId;
-  String firstName;
-  String middleName;
-  String lastName;
+  String fullName;
   List<BinaryCertificateTypeDTO> titles;
   List<String> specialities;
   List<BinaryCertificateTypeDTO> licences;
