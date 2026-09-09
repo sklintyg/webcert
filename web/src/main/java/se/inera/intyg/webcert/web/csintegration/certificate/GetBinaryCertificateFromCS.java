b5a91acdf22f0ac5f7a5a4cda26b4cffce32f985
Jacob Almqvist <jacob.almqvist@soprasteria.com>
2026-09-09 14:32:53 +0200

    Run spotless

--- web/src/main/java/se/inera/intyg/webcert/web/csintegration/certificate/GetBinaryCertificateFromCS.java
+++ web/src/main/java/se/inera/intyg/webcert/web/csintegration/certificate/GetBinaryCertificateFromCS.java
@@ -21,7 +21,6 @@ package se.inera.intyg.webcert.web.csintegration.certificate;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Service;
 import se.inera.intyg.webcert.common.service.exception.WebCertServiceErrorCodeEnum;
 import se.inera.intyg.webcert.common.service.exception.WebCertServiceException;
